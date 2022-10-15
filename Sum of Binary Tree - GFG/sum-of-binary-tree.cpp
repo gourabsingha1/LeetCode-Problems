379e031d4a3e35787de6dc0eee084cf416d87ba1
gourabsingha1 <110812339+gourabsingha1@users.noreply.github.com>
2022-10-15 18:47:08 +0530

    Added solution - LeetHub

---
+++ Sum of Binary Tree - GFG/sum-of-binary-tree.cpp
@@ -0,0 +1,107 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Node
+{
+	int key;
+	struct Node *left;
+	struct Node *right;
+	
+	Node(int x){
+	    key = x;
+	    left = NULL;
+	    right = NULL;
+	}
+};
+
+Node* make_tree()
+{
+	int n;
+	cin>>n;
+    
+	Node* head=NULL;
+	queue <Node*> q;
+
+	for( ; n>0 ; n-=2 )
+	{
+		int a,b;
+		char c;
+		cin>> a >> b >> c;
+
+        if(!head)
+		{
+			head = new Node(a);
+			q.push(head);
+		}
+
+        Node* pick = q.front();
+		q.pop();
+
+		if(c == 'L')
+		{
+			pick->left = new Node(b);
+			q.push( pick->left );
+		}
+		
+		
+		cin>> a >> b >> c;
+		
+		if(c == 'R')
+		{
+			pick->right = new Node(b);
+			q.push( pick->right );
+		}
+	}
+	return head;
+}
+
+long int sumBT(Node* root);
+
+
+int main()
+{
+    int t;cin>>t;while(t--){
+    	
+    	Node* head = make_tree();
+    
+        cout<<sumBT(head)<<endl;
+    }
+	return 1;
+}
+
+// } Driver Code Ends
+
+
+/*Structure of the node of the binary tree
+struct Node
+{
+	int key;
+	Node* left, *right;
+};
+*/
+// Function should return the sum of all the elements
+// of the binary tree
+long int preorderTraversal(Node *root){
+    long int res = 0;
+    if(root == NULL){
+        return -1;
+    }
+    stack<Node*> st;
+    st.push(root);
+    while(!st.empty()){
+        root = st.top();
+        res += st.top()->key;
+        st.pop();
+        if(root->right){
+            st.push(root->right);
+        }
+        if(root->left){
+            st.push(root->left);
+        }
+    }
+    return res;
+}
+long int sumBT(Node* root){
+    return preorderTraversal(root);
+}
