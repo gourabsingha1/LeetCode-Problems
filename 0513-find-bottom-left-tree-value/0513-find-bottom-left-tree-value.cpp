4ecf389350a97d83f714011d307fdcfa8b9f5334
gourabsingha1 <110812339+gourabsingha1@users.noreply.github.com>
2022-10-18 14:43:54 +0530

    Time: 34 ms (11.77%), Space: 21.9 MB (39.12%) - LeetHub

--- 0513-find-bottom-left-tree-value/0513-find-bottom-left-tree-value.cpp
+++ 0513-find-bottom-left-tree-value/0513-find-bottom-left-tree-value.cpp
@@ -1,30 +1,16 @@
 class Solution {
 public:
-    int levelorderTraversal(TreeNode *root){
-        int res;
-        if(!root){
-            return res;
-        }
+    int findBottomLeftValue(TreeNode* root) {
 
         queue<TreeNode*> q;
         q.push(root);
-        while (q.size()) {
-            int n = q.size();
-            for (int i = 0; i < n; i++) {
-                if(!i){
-                    res = q.front()->val;
-                }
-                TreeNode* node = q.front();
-                q.pop();
-                
-                // check for left and right subtree
-                if (node->left) q.push(node->left);
-                if (node->right) q.push(node->right);
-            }
+        TreeNode* curr;
+        while(!q.empty()){
+            curr = q.front();
+            q.pop();
+            if(curr->right) q.push(curr->right);
+            if(curr->left) q.push(curr->left);
         }
-        return res;
-    }
-    int findBottomLeftValue(TreeNode* root) {
-        return levelorderTraversal(root);
+        return curr->val;
     }
 };
