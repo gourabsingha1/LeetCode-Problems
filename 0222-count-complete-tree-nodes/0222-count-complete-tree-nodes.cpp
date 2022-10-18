114bccc2704c09a7c27e2d1de5ad4fedd1997917
gourabsingha1 <110812339+gourabsingha1@users.noreply.github.com>
2022-10-18 14:23:22 +0530

    Time: 69 ms (34.17%), Space: 31 MB (20.51%) - LeetHub

--- 0222-count-complete-tree-nodes/0222-count-complete-tree-nodes.cpp
+++ 0222-count-complete-tree-nodes/0222-count-complete-tree-nodes.cpp
@@ -1,16 +1,36 @@
 class Solution {
 public:
-    int cnt = 0;
-    void dfs(TreeNode *root){
+    int dfs(TreeNode *root){
         if(!root){
-            return;
+            return 0;
         }
-        cnt++;
-        dfs(root->left);
-        dfs(root->right);
+        
+        TreeNode *l = root->left;
+        int lDepth = 1;
+        while(l){
+            lDepth++;
+            l = l->left;
+        }
+        TreeNode *r = root->right;
+        int rDepth = 1;
+        while(r){
+            rDepth++;
+            r = r->right;
+        }
+
+        if(lDepth == rDepth){
+            return pow(2, lDepth) - 1;
+        }
+
+        return 1 + dfs(root->left) + dfs(root->right);
     }
     int countNodes(TreeNode* root) {
-        dfs(root);
-        return cnt;
+        if(!root){
+            return 0;
+        }
+        if(!root->left && !root->right){
+            return 1;
+        }
+        return dfs(root);
     }
 };
