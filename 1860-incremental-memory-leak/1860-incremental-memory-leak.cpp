5ac8294406b445d9319421003c29b7b20cfeed66
gourabsingha1 <110812339+gourabsingha1@users.noreply.github.com>
2022-10-21 13:34:01 +0530

    Time: 7 ms (63.96%), Space: 6.1 MB (82.23%) - LeetHub

---
+++ 1860-incremental-memory-leak/1860-incremental-memory-leak.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    vector<int> memLeak(int memory1, int memory2) {
+        int i = 1;
+        while(i <= max(memory1, memory2)){
+            if(memory1 < memory2){
+                memory2 -= i;
+            }
+            else{
+                memory1 -= i;
+            }
+            i++;
+        }
+        return {i, memory1, memory2};
+    }
+};
