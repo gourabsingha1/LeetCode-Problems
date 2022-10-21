3131307eee579631188e25d256c873f0e30acdfd
gourabsingha1 <110812339+gourabsingha1@users.noreply.github.com>
2022-10-21 13:50:26 +0530

    Time: 16 ms (78.46%), Space: 21.8 MB (30.09%) - LeetHub

--- 0098-validate-binary-search-tree/0098-validate-binary-search-tree.cpp
+++ 0098-validate-binary-search-tree/0098-validate-binary-search-tree.cpp
@@ -1,21 +1,22 @@
 class Solution {
 public:
-    bool res = 1;
-    void dfs(TreeNode* root, long long int left, long long int right){
+    bool checkBST(TreeNode* root, long long int left, long long int right){
+        // base condition
         if(!root){
-            return;
+            return 1;
         }
         if(!(left < (long long int)root->val && (long long int)root->val < right)){
-            res = 0;
-            return;
+            return 0;
         }
 
-        dfs(root->left, left, root->val);
-        dfs(root->right, root->val, right);
+        // hypothesis
+        bool checkLeft = checkBST(root->left, left, root->val); // check -inf < 1 < 5
+        bool checkRight = checkBST(root->right, root->val, right); // check 5 < 7 < inf
+
+        return checkLeft && checkRight; // if one of them is false, ans is false
     }
 
     bool isValidBST(TreeNode* root) {
-        dfs(root, LONG_LONG_MIN, LONG_LONG_MAX);
-        return res;
+        return checkBST(root, LONG_LONG_MIN, LONG_LONG_MAX);
     }
 };
