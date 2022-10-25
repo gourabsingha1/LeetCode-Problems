d5c32eeff023d68294193f6d1694ca9135838628
gourabsingha1 <110812339+gourabsingha1@users.noreply.github.com>
2022-10-25 19:17:12 +0530

    Time: 151 ms (62.82%), Space: 20 MB (63.28%) - LeetHub

--- 0015-3sum/0015-3sum.cpp
+++ 0015-3sum/0015-3sum.cpp
@@ -8,19 +8,16 @@ public:
             if(i>0 && nums[i] == nums[i-1]){
                 continue;
             }
+
             int low = i + 1, high = nums.size() - 1;
             while(low < high){
                 int x = nums[i] + nums[low] + nums[high];
                 if(!x){
                     res.push_back({nums[i], nums[low], nums[high]});
-                    while(low < nums.size()-1 && nums[low]==nums[low+1]){
+                    low++;
+                    while(low < high && nums[low]==nums[low-1]){
                         low++;
                     }
-                    while(high > 0 && nums[high]==nums[high-1]){
-                        high--;
-                    }
-                    low++;
-                    high--;
                 }
                 else if(x > 0){
                     high--;
