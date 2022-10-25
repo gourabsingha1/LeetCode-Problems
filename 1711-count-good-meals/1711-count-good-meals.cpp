009fba3d91243c3d50575f1857b702e7aa6ea83f
gourabsingha1 <110812339+gourabsingha1@users.noreply.github.com>
2022-10-25 15:58:24 +0530

    Time: 1289 ms (22.14%), Space: 116.7 MB (21.12%) - LeetHub

--- 1711-count-good-meals/1711-count-good-meals.cpp
+++ 1711-count-good-meals/1711-count-good-meals.cpp
@@ -3,16 +3,16 @@ public:
     int countPairs(vector<int>& deliciousness) {
         int res = 0, M = 1e9 + 7;
         unordered_map<int,int> m;
-        for (int i = 0; i < deliciousness.size(); i++)
+        for (int &x : deliciousness)
         {
             for (int j = 1; j <= (1<<22); j*=2)
             {
-                int target = j - deliciousness[i];
-                if(m[target]){
+                int target = j - x;
+                if(target >= 0 && m[target]){
                     res = (res + m[target])%M;
                 }
             }
-            m[deliciousness[i]]++;
+            m[x]++;
         }
         return res;
     }
