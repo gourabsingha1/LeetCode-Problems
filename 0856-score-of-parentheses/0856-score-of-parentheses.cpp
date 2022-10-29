dfa8da2c53458ee92da03a78d902a54d113a5d68
gourabsingha1 <110812339+gourabsingha1@users.noreply.github.com>
2022-10-29 20:07:26 +0530

    Time: 0 ms (100.00%), Space: 6 MB (82.95%) - LeetHub

--- 0856-score-of-parentheses/0856-score-of-parentheses.cpp
+++ 0856-score-of-parentheses/0856-score-of-parentheses.cpp
@@ -2,18 +2,47 @@ class Solution {
 public:
     int scoreOfParentheses(string s) {
         stack<int>st;
-        int res = 0;
-        for(char &c : s){
-            if(c == '('){
-                st.push(res);
-                res = 0;
+        int sum = 0;
+        for (int i = 0; i < s.size(); i++)
+        {
+            if(i < s.size()-1 && s.substr(i,2) == "()"){
+                if(st.size() && st.top() != '('){
+                    st.top()++;
+                    sum = 0;
+                }
+                else{
+                    sum++;
+                }
+                i++;
             }
-            else{
-                int x = st.top();
-                st.pop();
-                res = x + max(2*res, 1);
+            else if(s[i] == '('){
+                if(sum){
+                    st.push(sum);
+                    sum = 0;
+                }
+                st.push('(');
+            }
+            else if(s[i] == ')'){
+                while(st.size() && st.top() != '('){
+                    sum += st.top();
+                    st.pop();
+                }
+                if(sum){
+                    st.push(sum);
+                }
+                sum = 0;
+                if(st.size() > 1){
+                    int x = st.top()*2;
+                    st.pop();
+                    st.pop();
+                    st.push(x);
+                }
             }
         }
-        return res;
+        while(st.size()){
+            sum += st.top();
+            st.pop();
+        }
+        return sum;
     }
 };
