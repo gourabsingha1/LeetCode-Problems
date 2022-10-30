7fcbf968b709a532c2d61377e6976363b93eaf8a
gourabsingha1 <110812339+gourabsingha1@users.noreply.github.com>
2022-10-30 13:40:08 +0530

    Time: 332 ms (34.84%), Space: 86.6 MB (32.33%) - LeetHub

--- 1305-all-elements-in-two-binary-search-trees/1305-all-elements-in-two-binary-search-trees.cpp
+++ 1305-all-elements-in-two-binary-search-trees/1305-all-elements-in-two-binary-search-trees.cpp
@@ -1,17 +1,31 @@
 class Solution {
 public:
-    vector<int> res;
-    void Inorder(TreeNode *root){
+    void Inorder(TreeNode *root, vector<int> &list){
         if(root == NULL){
             return;
         }
-        Inorder(root->left);
-        res.push_back(root->val);
-        Inorder(root->right);
+        Inorder(root->left, list);
+        list.push_back(root->val);
+        Inorder(root->right, list);
     }
     vector<int> getAllElements(TreeNode* root1, TreeNode* root2) {
-        Inorder(root1), Inorder(root2);
-        sort(res.begin(), res.end());
+        vector<int> res, list1, list2;
+        Inorder(root1, list1), Inorder(root2, list2);
+        int i = 0, j = 0;
+        while(i < list1.size() && j < list2.size()){
+            if(list1[i] < list2[j]){
+                res.push_back(list1[i++]);
+            }
+            else{
+                res.push_back(list2[j++]);
+            }
+        }
+        while(i != list1.size()){
+            res.push_back(list1[i++]);
+        }
+        while(j != list2.size()){
+            res.push_back(list2[j++]);
+        }
         return res;
     }
 };
