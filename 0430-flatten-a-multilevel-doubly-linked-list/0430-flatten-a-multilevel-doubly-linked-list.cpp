7bab073b15231300823c09211e61f89f4fa1078d
gourabsingha1 <110812339+gourabsingha1@users.noreply.github.com>
2022-11-12 20:05:04 +0530

    Time: 9 ms (38.19%), Space: 7.4 MB (20.14%) - LeetHub

--- 0430-flatten-a-multilevel-doubly-linked-list/0430-flatten-a-multilevel-doubly-linked-list.cpp
+++ 0430-flatten-a-multilevel-doubly-linked-list/0430-flatten-a-multilevel-doubly-linked-list.cpp
@@ -2,20 +2,20 @@ class Solution {
 public:
     Node* flatten(Node* head) {
         Node* dummy = head;
-        stack<Node*> st;
         while(head){
             if(head->child){
-                if(head->next){
-                    st.push(head->next);
-                }
+                Node* nex = head->next;
                 head->child->prev = head;
                 head->next = head->child;
                 head->child = NULL;
-            }
-            if(!head->next && st.size()){
-                head->next = st.top();
-                st.top()->prev = head;
-                st.pop();
+                Node* temp = head->next;
+                while(temp->next){
+                    temp = temp->next;
+                }
+                temp->next = nex;
+                if(nex){
+                    nex->prev = temp;
+                }
             }
             head = head->next;
         }
