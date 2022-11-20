fa77fcc5597d877eaea450877ea56bb540d6991a
gourabsingha1 <110812339+gourabsingha1@users.noreply.github.com>
2022-11-20 17:51:17 +0530

    Time: 163 ms (89.70%), Space: 89.1 MB (13.97%) - LeetHub

--- 2420-find-all-good-indices/2420-find-all-good-indices.cpp
+++ 2420-find-all-good-indices/2420-find-all-good-indices.cpp
@@ -2,41 +2,23 @@ class Solution {
 public:
     vector<int> goodIndices(vector<int>& nums, int k) {
         vector<int> res;
-        int n = nums.size(), c = 1, d = 1;
-        vector<int> pre(n, 0);
-        if(k == 1){
-            for (int i = 1; i < n-1; i++)
-            {
-                res.push_back(i);
-            }
-            return res;
-        }
-
+        int n = nums.size();
+        vector<int> pre(n, 1), suff(n, 1);
         for (int i = 1; i < n; i++)
         {
             if(nums[i-1] >= nums[i]){
-                c++;
-                if(c >= k && i+1<n){
-                    pre[i+1]++;
-                }
-            }
-            else{
-                c = 1;
-            }
-
-            if(nums[n-i] >= nums[n-1-i]){
-                d++;
-                if(d >= k && n-2-i>=0){
-                    pre[n-2-i]++;
-                }
-            }
-            else{
-                d = 1;
+                pre[i] = pre[i-1] + 1;
             }
         }
-        for (int i = 0; i < n; i++)
+        for (int i = n-2; i > 0; i--)
         {
-            if(pre[i] == 2){
+            if(nums[i+1] >= nums[i]){
+                suff[i] = suff[i+1] + 1;
+            }
+        }
+        for (int i = k; i < n-k; i++)
+        {
+            if(pre[i-1] >= k && suff[i+1] >= k){
                 res.push_back(i);
             }
         }
