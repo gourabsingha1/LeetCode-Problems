e1f4b6f67d757b23efe5f44b24eae7c3393f2df7
gourabsingha1 <110812339+gourabsingha1@users.noreply.github.com>
2022-12-03 21:50:35 +0530

    Time: 428 ms (96.94%), Space: 93.3 MB (44.39%) - LeetHub

--- 1665-minimum-initial-energy-to-finish-tasks/1665-minimum-initial-energy-to-finish-tasks.cpp
+++ 1665-minimum-initial-energy-to-finish-tasks/1665-minimum-initial-energy-to-finish-tasks.cpp
@@ -2,17 +2,16 @@ class Solution {
 public:
     int minimumEffort(vector<vector<int>>& tasks) {
         int res = 0, cur = 0;
-        vector<pair<int, pair<int, int>>> v;
         for(auto &x : tasks){
-            v.push_back({x[1]-x[0], {x[0], x[1]}});
+            x[0] = x[1] - x[0];
         }
-        sort(v.rbegin(), v.rend());
-        for(auto &[x,y] : v){
-            if(cur < y.second){
-                res += (y.second - cur);
-                cur = y.second;
+        sort(tasks.rbegin(), tasks.rend());
+        for(auto &x : tasks){
+            if(cur < x[1]){
+                res += (x[1] - cur);
+                cur = x[1];
             }
-            cur -= y.first;
+            cur -= (x[1]-x[0]);
         }
         return res;
     }
