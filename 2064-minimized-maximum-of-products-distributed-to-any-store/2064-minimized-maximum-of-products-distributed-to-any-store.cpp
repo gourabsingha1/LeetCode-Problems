6e806305d14e3a092b341a88cd846206a41b46c1
gourabsingha1 <110812339+gourabsingha1@users.noreply.github.com>
2022-12-04 16:49:34 +0530

    Time: 186 ms (99.37%), Space: 84.5 MB (47.36%) - LeetHub

--- 2064-minimized-maximum-of-products-distributed-to-any-store/2064-minimized-maximum-of-products-distributed-to-any-store.cpp
+++ 2064-minimized-maximum-of-products-distributed-to-any-store/2064-minimized-maximum-of-products-distributed-to-any-store.cpp
@@ -1,20 +1,19 @@
 class Solution {
 public:
     int minimizedMaximum(int n, vector<int>& quantities) {
-        int l = 1, h = *max_element(quantities.begin(), quantities.end()), res = INT_MAX;
+        int l = 1, h = 1e5;
         while(l <= h){
-            int m = h + (l-h)/2, sum = 0;
+            int m = (l+h)/2, sum = 0;
             for(int &x : quantities){
                 sum += ((x+m-1)/m);
             }
             if(sum <= n){
-                res = min(res, m);
                 h = m - 1;
             }
             else{
                 l = m + 1;
             }
         }
-        return res;
+        return l;
     }
 };
