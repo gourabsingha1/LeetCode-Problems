0a728ec9a773f93cfd88f90795662d686705b308
gourabsingha1 <110812339+gourabsingha1@users.noreply.github.com>
2022-12-05 03:13:13 +0530

    Time: 277 ms (69.98%), Space: 65.2 MB (37.21%) - LeetHub

--- 0912-sort-an-array/0912-sort-an-array.cpp
+++ 0912-sort-an-array/0912-sort-an-array.cpp
@@ -1,13 +1,15 @@
 class Solution {
 public:
     vector<int> sortArray(vector<int>& nums) {
-        priority_queue<int, vector<int>, greater<int>> pq;
-        int i = 0;
-        for ( ; i < nums.size(); i++) pq.push(nums[i]);
-        i = 0;
-        while(pq.size()){
-            nums[i++] = pq.top();
-            pq.pop();
+        vector<int> bucket(1e5+1, 0);
+        int d = 5*(1e4);
+        for (int &x : nums){
+            bucket[x+d]++;
+        }
+        for (int i = 0, j = 0; i < 1e5+1; i++){
+            while(bucket[i]--){
+                nums[j++] = i - d;
+            }
         }
         return nums;
     }
