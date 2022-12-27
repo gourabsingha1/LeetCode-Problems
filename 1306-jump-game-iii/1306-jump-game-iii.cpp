f4cfcd8a024148c8f50810856a8761570f8545dc
Gourab Singha <110812339+gourabsingha1@users.noreply.github.com>
2022-12-27 16:01:57 +0530

    Time: 48 ms (89.53%), Space: 35.7 MB (27.95%) - LeetHub

--- 1306-jump-game-iii/1306-jump-game-iii.cpp
+++ 1306-jump-game-iii/1306-jump-game-iii.cpp
@@ -1,20 +1,16 @@
 class Solution {
 public:
-    bool res = 0;
-    void dfs(int i, int n, vector<int> &arr, vector<bool> &vis){
-        if(i < 0 || i >= n || vis[i] == 1) return;
+    bool dfs(int i, int n, vector<int> &arr, vector<bool> &vis){
+        if(i < 0 || i >= n || vis[i] == 1) return 0;
         if(arr[i] == 0){
-            res = 1;
-            return;
+            return 1;
         }
         vis[i] = 1;
-        dfs(i + arr[i], n, arr, vis);
-        dfs(i - arr[i], n, arr, vis);
+        return dfs(i + arr[i], n, arr, vis) || dfs(i - arr[i], n, arr, vis);
     }
     bool canReach(vector<int>& arr, int start) {
         int n = arr.size();
         vector<bool> vis(n, 0);
-        dfs(start, n, arr, vis);
-        return res;
+        return dfs(start, n, arr, vis);
     }
 };
