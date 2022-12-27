71ae2d428799ad253d57dd245a4ca647cd9c3ef4
Gourab Singha <110812339+gourabsingha1@users.noreply.github.com>
2022-12-27 13:25:47 +0530

    Time: 0 ms (100.00%), Space: 7.4 MB (59.21%) - LeetHub

--- 0410-split-array-largest-sum/0410-split-array-largest-sum.cpp
+++ 0410-split-array-largest-sum/0410-split-array-largest-sum.cpp
@@ -3,16 +3,16 @@ public:
     int splitArray(vector<int>& nums, int k) {
         int l = *max_element(nums.begin(), nums.end()), h = accumulate(nums.begin(), nums.end(), 0);
         while(l <= h){
-            int mid = h + (l-h)/2, sum = 0, currDays = 1;
+            int mid = h + (l-h)/2, sum = 0, curr = 1;
             for (int i = 0; i < nums.size(); i++)
             {
                 sum += nums[i];
                 if(sum > mid){
-                    currDays++;
+                    curr++;
                     sum = nums[i];
                 }
             }
-            if(currDays > k){
+            if(curr > k){
                 l = mid + 1;
             }
             else{
