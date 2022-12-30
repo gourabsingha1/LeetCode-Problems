721a93a7ebc4e58858e71bf9ec9c125d7f0dec5e
Gourab Singha <110812339+gourabsingha1@users.noreply.github.com>
2022-12-31 03:05:43 +0530

    Time: 0 ms (100.00%), Space: 6 MB (55.89%) - LeetHub

--- 1922-count-good-numbers/1922-count-good-numbers.cpp
+++ 1922-count-good-numbers/1922-count-good-numbers.cpp
@@ -3,13 +3,13 @@ ll M = 1e9+7;
 class Solution {
 public:
     ll Pow(ll x, ll n, ll m = M){
-        ll res = 1;
-        while(n){
-            if(n & 1) res = (res * x) % m;
-            x = (x * x) % m;
-            n >>= 1;
+        if(n == 0) return 1;
+        if(n%2 == 0){
+            return Pow(x*x%m, n/2)%m;
         }
-        return res;
+        else{
+            return x*(Pow(x*x%m, n/2)%m)%m;
+        }
     }
     int countGoodNumbers(long long n) {
         return (Pow(5, (n+1)/2)*Pow(4, n/2)) % M;
