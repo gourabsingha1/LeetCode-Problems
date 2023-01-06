4cab9b8abaf79864249442f9ce37dfc85c719cbc
Gourab Singha <110812339+gourabsingha1@users.noreply.github.com>
2023-01-06 23:31:52 +0530

    Time: 39 ms (84.96%), Space: 15.2 MB (80.97%) - LeetHub

--- 1392-longest-happy-prefix/1392-longest-happy-prefix.cpp
+++ 1392-longest-happy-prefix/1392-longest-happy-prefix.cpp
@@ -1,19 +1,20 @@
-#define ll long long int
-ll M = 1e9 + 7;
 class Solution {
 public:
     string longestPrefix(string s) {
-        ll n = s.size(), j = n, pre = 0, suf = 0, mul = 1;
-        for (int i = 0; i < n-1; i++)
-        {
-            pre = ((26*pre) + s[i] - 'a') % M;
-            suf = (mul*(s[n - 1 - i] - 'a') + suf) % M;
-            mul = (mul*26) % M;
-
-            if(pre == suf){
-                j = n - 1 - i;
+        int n = s.size(), LPS[n], i = 1, prevLPS = 0;
+        LPS[0] = 0;
+        while(i < n){
+            if(s[i] == s[prevLPS]){
+                LPS[i] = prevLPS + 1;
+                prevLPS++, i++;
+            }
+            else if(prevLPS == 0){
+                LPS[i] = 0, i++;
+            }
+            else{
+                prevLPS = LPS[prevLPS - 1];
             }
         }
-        return s.substr(j);
+        return s.substr(0, prevLPS);
     }
 };
