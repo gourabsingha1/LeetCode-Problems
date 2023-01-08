a913332cd3f65231d78f430c3a1ecb7b9c4e6d7b
Gourab Singha <110812339+gourabsingha1@users.noreply.github.com>
2023-01-08 22:42:24 +0530

    Time: 416 ms (68.90%), Space: 8.1 MB (48.40%) - LeetHub

--- 0079-word-search/0079-word-search.cpp
+++ 0079-word-search/0079-word-search.cpp
@@ -9,22 +9,19 @@ public:
         if(i < 0 || j < 0 || i == board.size() || j == board[0].size() || board[i][j] != word[k]){
             return;
         }
-        char c = board[i][j];
-        board[i][j] = '#';
+        board[i][j] ^= 257;
         dfs(i - 1, j, k + 1, board, word);
         dfs(i, j - 1, k + 1, board, word);
         dfs(i + 1, j, k + 1, board, word);
         dfs(i, j + 1, k + 1, board, word);
-        board[i][j] = c;
+        board[i][j] ^= 257;
     }
     bool exist(vector<vector<char>>& board, string word) {
         for (int i = 0; i < board.size(); i++)
         {
             for (int j = 0; j < board[0].size(); j++)
             {
-                if(board[i][j] == word[0]){
-                    dfs(i, j, 0, board, word);
-                }
+                dfs(i, j, 0, board, word);
             }
         }
         return res;
