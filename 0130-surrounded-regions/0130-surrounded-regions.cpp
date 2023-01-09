328a3627c78d2ee88bfe7fb4dfbb9fb69a6bfa37
Gourab Singha <110812339+gourabsingha1@users.noreply.github.com>
2023-01-09 16:23:24 +0530

    Time: 19 ms (62.57%), Space: 10 MB (68.15%) - LeetHub

---
+++ 0130-surrounded-regions/0130-surrounded-regions.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    void dfs(int i, int j, vector<vector<char>>& board){
+        if(i < 0 || j < 0 || i == board.size() || j == board[0].size() || board[i][j] != 'O'){
+            return;
+        }
+        board[i][j] = '#';
+        dfs(i-1, j, board);
+        dfs(i, j-1, board);
+        dfs(i+1, j, board);
+        dfs(i, j+1, board);
+    }
+    void solve(vector<vector<char>>& board) {
+        int n = board.size(), m = board[0].size();
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = 0; j < m; j++)
+            {
+                if(i == 0 || j == 0 || i == n - 1 || j == m - 1){
+                    dfs(i, j, board);
+                }
+            }
+        }
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = 0; j < m; j++)
+            {
+                if(board[i][j] == 'O') board[i][j] = 'X';
+            }
+        }
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = 0; j < m; j++)
+            {
+                if(board[i][j] == '#') board[i][j] = 'O';
+            }
+        }
+    }
+};
