f3b130c06cddac8597586c259f16421355ea411c
Gourab Singha <110812339+gourabsingha1@users.noreply.github.com>
2023-01-15 13:04:06 +0530

    Time: 18 ms (66.09%), Space: 10.2 MB (86.15%) - LeetHub

--- 1094-car-pooling/1094-car-pooling.cpp
+++ 1094-car-pooling/1094-car-pooling.cpp
@@ -1,23 +1,18 @@
+// Scanline Algorithm
 class Solution {
 public:
-    static bool sortbyvector(const vector<int> &a, const vector<int> &b){
-        return a[1] < b[1];
-    }
     bool carPooling(vector<vector<int>>& trips, int capacity) {
-        sort(trips.begin(), trips.end(), sortbyvector);
-        int curr = trips[0][0], ma = curr, n = trips.size();
-        priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>> > pq;
-        pq.push({trips[0][2], trips[0][0]});
-        for (int i = 1; i < n; i++)
-        {
-            curr += trips[i][0];
-            pq.push({trips[i][2], trips[i][0]});
-            while(pq.size() && pq.top().first <= trips[i][1]){
-                curr -= pq.top().second;
-                pq.pop();
-            }
-            ma = max(ma, curr);
+        vector<int> pre(1001, 0);
+        for(auto &v : trips){
+            int x = v[0], l = v[1], r = v[2];
+            pre[l] += x;
+            pre[r] -= x;
         }
-        return ma <= capacity;
+        for (int i = 0; i < 1000; i++)
+        {
+            if(pre[i] > capacity) return 0;
+            pre[i + 1] += pre[i];
+        }
+        return 1;
     }
 };
