4196192d01fa2aa0043d462546cb1b4e4da8a16e
Gourab Singha <110812339+gourabsingha1@users.noreply.github.com>
2023-01-15 17:04:55 +0530

    Time: 252 ms (100.00%), Space: 81.9 MB (40.00%) - LeetHub

--- 2536-increment-submatrices-by-one/2536-increment-submatrices-by-one.cpp
+++ 2536-increment-submatrices-by-one/2536-increment-submatrices-by-one.cpp
@@ -1,33 +1,30 @@
+// 2D Scanline Algorithm
 class Solution {
 public:
     vector<vector<int>> rangeAddQueries(int n, vector<vector<int>>& queries) {
-        vector<vector<int>> res(n, vector<int> (n, 0));
-        int mat[n][n];
-        for (int i = 0; i < n; i++)
-        {
-            for (int j = 0; j < n; j++)
-            {
-                mat[i][j] = 0;
-            }
-        }
+        vector<vector<int>> pre(n, vector<int> (n, 0));
         for (int i = 0; i < queries.size(); i++)
         {
             int r1 = queries[i][0], c1 = queries[i][1], r2 = queries[i][2], c2 = queries[i][3];
-            for (int j = r1; j <= r2; j++)
+            pre[r1][c1]++;
+            if(c2 + 1 < n) pre[r1][c2 + 1]--;
+            if(r2 + 1 < n) pre[r2 + 1][c1]--;
+            if(r2 + 1 < n && c2 + 1 < n) pre[r2 + 1][c2 + 1]++;
+        }
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = 1; j < n; j++)
             {
-                for (int k = c1; k <= c2; k++)
-                {
-                    mat[j][k]++;
-                }
+                pre[i][j] += pre[i][j - 1];
             }
         }
         for (int i = 0; i < n; i++)
         {
-            for (int j = 0; j < n; j++)
+            for (int j = 1; j < n; j++)
             {
-                res[i][j] = mat[i][j];
+                pre[j][i] += pre[j - 1][i];
             }
         }
-        return res;
+        return pre;
     }
 };
