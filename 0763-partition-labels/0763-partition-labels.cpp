58ad0f348f400a748ea933d31e1e7a143a77d594
Gourab Singha <110812339+gourabsingha1@users.noreply.github.com>
2023-01-17 09:59:16 +0530

    Time: 0 ms (100.00%), Space: 6.7 MB (71.24%) - LeetHub

--- 0763-partition-labels/0763-partition-labels.cpp
+++ 0763-partition-labels/0763-partition-labels.cpp
@@ -1,25 +1,21 @@
 class Solution {
 public:
     vector<int> partitionLabels(string s) {
-        vector<int> res, ml(26, 0), mr(26, 0);
-        int n = s.size(), k = 0;
-        for (int i = 0; i < n; i++) mr[s[i] - 'a']++;
+        vector<int> res, m(26, 0);
+        int n = s.size(), currMax = 0;
+        for (int i = 0; i < n; i++) m[s[i] - 'a'] = i;
         for (int i = 0; i < n; i++)
         {
-            ml[s[i] - 'a']++;
-            mr[s[i] - 'a']--;
-            bool flag = 1;
-            for (int j = 0; j < 26; j++)
-            {
-                if(ml[j] && mr[j]){
-                    flag = 0;
-                    break;
+            int size = 1;
+            currMax = m[s[i] - 'a'];
+            while(i < currMax){
+                size++;
+                i++;
+                if(currMax < m[s[i] - 'a']){
+                    currMax = m[s[i] - 'a'];
                 }
             }
-            if(flag){
-                res.push_back(i - k + 1);
-                k = i + 1;
-            }
+            res.push_back(size);
         }
         return res;
     }
