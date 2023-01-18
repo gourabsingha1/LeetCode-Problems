e7e3b853de7ced245970fe73ff09e8973bf4248e
Gourab Singha <110812339+gourabsingha1@users.noreply.github.com>
2023-01-18 13:46:55 +0530

    Time: 80 ms (55.19%), Space: 40 MB (38.25%) - LeetHub

--- 0918-maximum-sum-circular-subarray/0918-maximum-sum-circular-subarray.cpp
+++ 0918-maximum-sum-circular-subarray/0918-maximum-sum-circular-subarray.cpp
@@ -1,21 +1,26 @@
 class Solution {
 public:
     int maxSubarraySumCircular(vector<int>& nums) {
-        priority_queue<int> pq;
-        int n = nums.size(), sum = accumulate(nums.begin(), nums.end(), 0), res = INT_MIN, pre = 0, kadanes = 0;
+        int n = nums.size(), sum = 0, maxSum = nums[0], minSum = nums[0], currMax = 0, currMin = 0;
         for (int i = 0; i < n; i++)
         {
-            if(nums[i] + kadanes > nums[i]){
-                kadanes += nums[i];
+            sum += nums[i];
+            if(nums[i] + currMax > nums[i]){
+                currMax += nums[i];
             }
             else{
-                kadanes = nums[i];
+                currMax = nums[i];
             }
-            pre += nums[i];
-            sum -= nums[i];
-            pq.push(pre);
-            res = max({res, pq.top() + sum, kadanes});
+            if(nums[i] + currMin < nums[i]){
+                currMin += nums[i];
+            }
+            else{
+                currMin = nums[i];
+            }
+            maxSum = max(maxSum, currMax);
+            minSum = min(minSum, currMin);
         }
-        return res;
+        if(maxSum < 0) return maxSum;
+        return max(maxSum, sum - minSum);
     }
 };
