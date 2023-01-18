be4870883c4532e155c95f20c1101a204b065145
Gourab Singha <110812339+gourabsingha1@users.noreply.github.com>
2023-01-18 13:18:33 +0530

    Time: 102 ms (38.13%), Space: 43.3 MB (5.55%) - LeetHub

---
+++ 0918-maximum-sum-circular-subarray/0918-maximum-sum-circular-subarray.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int maxSubarraySumCircular(vector<int>& nums) {
+        priority_queue<int> pq;
+        int n = nums.size(), sum = accumulate(nums.begin(), nums.end(), 0), res = INT_MIN, pre = 0, kadanes = 0;
+        for (int i = 0; i < n; i++)
+        {
+            if(nums[i] + kadanes > nums[i]){
+                kadanes += nums[i];
+            }
+            else{
+                kadanes = nums[i];
+            }
+            pre += nums[i];
+            sum -= nums[i];
+            pq.push(pre);
+            if(!sum && i == n - 1){
+                sum = -3*1e4;
+            }
+            res = max({res, pq.top() + sum, sum, pq.top(), kadanes});
+        }
+        return res;
+    }
+};
