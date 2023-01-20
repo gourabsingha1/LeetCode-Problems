419ab60dbed1bf9dad80ed07171f08c4370aa427
Gourab Singha <110812339+gourabsingha1@users.noreply.github.com>
2023-01-20 20:10:16 +0530

    Time: 30 ms (52.94%), Space: 13.4 MB (75.70%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.cpp
+++ 0207-course-schedule/0207-course-schedule.cpp
@@ -1,40 +1,40 @@
 class Solution {
 public:
-    bool dfs(int i, vector<bool> &vis, vector<bool> &dfsVis, vector<vector<int>> &adj){
-        vis[i] = 1;
-        dfsVis[i] = 1;
-        for(auto &it : adj[i]){
-            if(!vis[it]){
-                if(dfs(it, vis, dfsVis, adj)){
-                    return 1;
-                }
-            }
-            else if(dfsVis[it]){
-                return 1;
-            }
-        }
-        dfsVis[i] = 0;
-        return 0;
-    }
     bool isCyclic(int n, vector<vector<int>> &adj){
-        vector<bool> vis(n, 0); // to mark all time visited paths
-        vector<bool> dfsVis(n, 0); // to mark the current visited path
+        int cnt = 0;
+        vector<int> indegree(n, 0); // indegree = no. of arrows directed towards
+        queue<int> q;
         for (int i = 0; i < n; i++)
         {
-            if(!vis[i]){
-                if(dfs(i, vis, dfsVis, adj)){
-                    return 1;
-                }
+            for(auto &it : adj[i]){
+                indegree[it]++;
             }
         }
-        return 0;
+        for (int i = 0; i < n; i++)
+        {
+            if(indegree[i] == 0){
+                q.push(i);
+            }
+        }
+        while(!q.empty()){
+            int i = q.front();
+            q.pop();
+            for(auto &it : adj[i]){
+                indegree[it]--;
+                if(indegree[it] == 0){
+                    q.push(it);
+                }
+            }
+            cnt++;
+        }
+        return cnt != n;
     }
     bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
         vector<vector<int>> adj(numCourses);
         for (int i = 0; i < prerequisites.size(); i++)
         {
             int u = prerequisites[i][0], v = prerequisites[i][1];
-            adj[v].push_back(u);
+            adj[u].push_back(v);
         }
         return !isCyclic(numCourses, adj);
     }
