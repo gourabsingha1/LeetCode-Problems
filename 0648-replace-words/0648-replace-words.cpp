6039f965a997ed09504eedce30e48f49997882d2
Gourab Singha <110812339+gourabsingha1@users.noreply.github.com>
2023-01-24 20:20:18 +0530

    Time: 2319 ms (5.07%), Space: 532.4 MB (5.07%) - LeetHub

--- 0648-replace-words/0648-replace-words.cpp
+++ 0648-replace-words/0648-replace-words.cpp
@@ -2,26 +2,18 @@ class Solution {
 public:
     string replaceWords(vector<string>& dictionary, string sentence) {
         string res;
-        int n = sentence.size();
+        int i = 0, j = 0, n = sentence.size();
         unordered_map<string, bool> m;
         for (int i = 0; i < dictionary.size(); i++) m[dictionary[i]] = 1;
-        for (int i = 0; i < n; i++){
-            string s;
-            bool flag = 1;
-            while(i < n && sentence[i] != ' '){
-                s += sentence[i++];
-                if(m[s]){
-                    res += s + " ";
-                    flag = 0;
-                    while(i < n && sentence[i] != ' ') i++;
-                    break;
-                }
+        while(i < n){
+            res += sentence[i++];
+            if(m[sentence.substr(j, i - j)]){
+                while(i < n && sentence[i] != ' ') i++;
             }
-            if(flag){
-                res.append(s + " ");
+            if(i < n && sentence[i] == ' '){
+                j = i + 1;
             }
         }
-        res.pop_back();
         return res;
     }
 };
