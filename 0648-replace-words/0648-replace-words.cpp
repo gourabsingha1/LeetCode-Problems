4687bc85e1491128421935bc973131d39b0bb182
Gourab Singha <110812339+gourabsingha1@users.noreply.github.com>
2023-01-24 19:59:21 +0530

    Time: 1635 ms (7.76%), Space: 391.4 MB (8.55%) - LeetHub

---
+++ 0648-replace-words/0648-replace-words.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    string replaceWords(vector<string>& dictionary, string sentence) {
+        string res;
+        int n = sentence.size();
+        unordered_map<string, bool> m;
+        for (int i = 0; i < dictionary.size(); i++) m[dictionary[i]] = 1;
+        for (int i = 0; i < n; i++){
+            string s;
+            bool flag = 1;
+            while(i < n && sentence[i] != ' '){
+                s += sentence[i++];
+                if(m[s]){
+                    res += s + " ";
+                    flag = 0;
+                    while(i < n && sentence[i] != ' ') i++;
+                    break;
+                }
+            }
+            if(flag){
+                res += s + " ";
+            }
+        }
+        res.pop_back();
+        return res;
+    }
+};
