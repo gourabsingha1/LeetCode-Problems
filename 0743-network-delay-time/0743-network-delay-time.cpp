2cc7f32ffa314710813e80c7bfe96e0c53b50887
Gourab Singha <110812339+gourabsingha1@users.noreply.github.com>
2023-01-27 01:00:15 +0530

    Time: 292 ms (21.63%), Space: 37 MB (96.83%) - LeetHub

--- 0743-network-delay-time/0743-network-delay-time.cpp
+++ 0743-network-delay-time/0743-network-delay-time.cpp
@@ -1,33 +1,21 @@
 class Solution {
 public:
-    int res = 0;
-    vector<int> Dijkstras(int src, int n, vector<pair<int,int>> adj[]){
-        vector<int> distTo(n, 1e9);
-        priority_queue <pair<int,int>, vector<pair<int,int>>, greater<pair<int,int>>> pq; // ascending order
-        distTo[src] = 0;
-        pq.push({0, src});
-        while(pq.size()){
-            int dist = pq.top().first, prev = pq.top().second;
-            pq.pop();
-            for(auto &it : adj[prev]){
-                int next = it.first;
-                int nextDist = it.second;
-                if(distTo[next] > dist + nextDist){
-                    distTo[next] = dist + nextDist;
-                    pq.push({distTo[next], next});
-                }
+    vector<int> bellmanFord(int src, int n, vector<vector<int>> &edges){
+        vector<int> dist(n, 1e9);
+        dist[src] = 0;
+        for (int i = 0; i < n - 1; i++)
+        {
+            vector<int> temp(dist);
+            for(auto &it : edges){
+                int u = it[0], v = it[1], wt = it[2];
+                dist[v] = min(dist[v], dist[u] + wt);
             }
         }
-        return distTo;
+        return dist;
     }
     int networkDelayTime(vector<vector<int>>& times, int n, int k) {
-        vector<pair<int,int>> adj[n + 1];
-        for (int i = 0; i < times.size(); i++)
-        {
-            int u = times[i][0], v = times[i][1], wt = times[i][2];
-            adj[u].push_back({v, wt});
-        }
-        vector<int> path = Dijkstras(k, n + 1, adj);
+        int res = 0;
+        vector<int> path = bellmanFord(k, n + 1, times);
         for (int i = 1; i < n + 1; i++)
         {
             res = max(res, path[i]);
