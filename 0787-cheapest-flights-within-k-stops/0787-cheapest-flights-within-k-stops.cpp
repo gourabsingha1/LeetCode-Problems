251f3a8fd233be46aedf474ff1c49b167074f124
Gourab Singha <110812339+gourabsingha1@users.noreply.github.com>
2023-01-26 23:29:01 +0530

    Time: 42 ms (45.39%), Space: 12.6 MB (92.85%) - LeetHub

--- 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.cpp
+++ 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.cpp
@@ -1,30 +1,22 @@
 class Solution {
 public:
-    int res = INT_MAX;
-    unordered_map<string, int> m;
-    void dfs(int i, int curr, int dst, int k, vector<pair<int, int>> adj[]){
-        if(i == dst){
-            res = min(res, curr);
-            return;
+    int bellmanFord(int src, int dst, int k, int n, vector<vector<int>> &edges){
+        vector<int> dist(n, 1e9);
+        dist[src] = 0;
+        for (int i = 0; i < k + 1; i++)
+        {
+            vector<int> temp(dist);
+            for(auto &it : edges){
+                int u = it[0], v = it[1], wt = it[2];
+                temp[v] = min(temp[v], dist[u] + wt);
+            }
+            dist = temp;
         }
-        if(k < 0 || curr >= res) return;
-        string key = to_string(i) + "*" + to_string(curr) + "*" + to_string(k);
-        if(m[key]) return;
-        m[key] = 1;
-        for(auto &it : adj[i]){
-            int j = it.first, cost = it.second;
-            dfs(j, curr + cost, dst, k - 1, adj);
-        }
+        return dist[dst];
     }
     int findCheapestPrice(int n, vector<vector<int>>& flights, int src, int dst, int k) {
-        vector<pair<int, int>> adj[n];
-        for (int i = 0; i < flights.size(); i++)
-        {
-            int u = flights[i][0], v = flights[i][1], cost = flights[i][2];
-            adj[u].push_back({v, cost});
-        }
-        dfs(src, 0, dst, k, adj);
-        if(res == INT_MAX) return -1;
+        int res = bellmanFord(src, dst, k, n, flights);
+        if(res == 1e9) return -1;
         return res;
     }
 };
