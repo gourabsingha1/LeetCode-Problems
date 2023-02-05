7f80b0dc8aa0d19458dd2322e76b48d44bd08246
Gourab Singha <110812339+gourabsingha1@users.noreply.github.com>
2023-02-06 00:00:27 +0530

    Added solution - LeetHub

--- Minimum Spanning Tree - GFG/minimum-spanning-tree.cpp
+++ Minimum Spanning Tree - GFG/minimum-spanning-tree.cpp
@@ -8,24 +8,22 @@ class Solution
 	public:
     int spanningTree(int n, vector<vector<int>> adj[]){
         int res = 0;
-        vector<int> dist(n, 1e9), parent(n); // update with minimum distance
+        vector<int> parent(n);
         vector<bool> vis(n);
         priority_queue <pair<int, int>, vector<pair<int, int>>, greater<>> pq;
-        dist[0] = 0;
         parent[0] = -1;
         pq.push({0, 0}); // dist, node
         while(pq.size()){
             int prev = pq.top().first, u = pq.top().second;
             pq.pop();
             if(vis[u]) continue;
-            res += prev;
             vis[u] = 1;
+            res += prev;
             for(auto &it : adj[u]){
                 int v = it[0], wt = it[1];
-                if(!vis[v] && dist[v] > wt){
-                    dist[v] = wt;
+                if(!vis[v]){
                     parent[v] = u;
-                    pq.push({dist[v], v});
+                    pq.push({wt, v});
                 }
             }
         }
