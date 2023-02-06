adad98bb48e7ca5b1482aa4880ada83dc12737c2
Gourab Singha <110812339+gourabsingha1@users.noreply.github.com>
2023-02-06 09:44:19 +0530

    Added solution - LeetHub

---
+++ Disjoint set (Union-Find) - GFG/disjoint-set-union-find.cpp
@@ -0,0 +1,50 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+int find(int a[],int x);
+void unionSet(int a[],int x ,int z);
+int main(){
+	int t;
+	cin>>t;
+	while(t--){
+		int n;
+		cin>>n;
+		int a[n+1];
+		for(int i=1;i<=n;i++)
+			a[i]=i;
+		int k;
+		cin>>k;
+		for(int i=0;i<k;i++){
+			string s;
+			cin>>s;
+			if(s=="UNION"){
+				int z,x;
+				cin>>x>>z;
+				unionSet(a,x,z);
+			}
+			else{
+				int x;
+				cin>>x;
+				int parent=find(a,x);
+				cout<<parent<<" ";
+			}
+		}
+		cout<<endl;
+	}
+}
+// } Driver Code Ends
+
+
+    int find(int parent[], int node)
+    {
+        if(node == parent[node]){
+            return node;
+        }
+        return parent[node] = find(parent, parent[node]); // path compression
+    }
+    void unionSet(int parent[], int u, int v)
+    {
+        u = find(parent, u);
+        v = find(parent, v);
+        parent[u] = v;
+    }
