ec7dd85f62a01a3301e13ef4cf9f0bc277a67d77
Gourab Singha <110812339+gourabsingha1@users.noreply.github.com>
2023-02-08 11:22:51 +0530

    Time: 10 ms (96.46%), Space: 16.6 MB (84.26%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.cpp
+++ 0045-jump-game-ii/0045-jump-game-ii.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     int jump(vector<int>& nums) {
         int res = 0, curr = 0, ma = 0;
-        for (int i = 0; i < nums.size()-1; i++)
+        for (int i = 0; i < nums.size() - 1; i++)
         {
             ma = max(ma, nums[i] + i);
             if(i == curr){
