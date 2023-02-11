90ff63912d95878f30eb14eafdcf33f3e3ea94cb
Gourab Singha <110812339+gourabsingha1@users.noreply.github.com>
2023-02-11 12:43:51 +0530

    Time: 70 ms (94.78%), Space: 27.3 MB (96.88%) - LeetHub

--- 0542-01-matrix/0542-01-matrix.cpp
+++ 0542-01-matrix/0542-01-matrix.cpp
@@ -1,42 +1,33 @@
 class Solution {
 public:
-    void bfs(vector<vector<int>>& mat){
-        int n = mat.size(), m = mat[0].size();
-        queue<pair<int, int>> q;
-        for (int i = 0; i < n; i++)
-        {
-            for (int j = 0; j < m; j++)
-            {
-                if(!mat[i][j]){
-                    q.push({i, j});
+    vector<vector<int>> updateMatrix(vector<vector<int>> &mat) {
+        int m = mat.size(), n = mat[0].size(), inf = m + n;
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (!mat[i][j]) continue;
+                int top = inf, left = inf;
+                if (i - 1 >= 0){
+                    top = mat[i - 1][j];
                 }
-                else{
-                    mat[i][j] = -1;
+                if (j - 1 >= 0){
+                    left = mat[i][j - 1];
                 }
+                mat[i][j] = min(top, left) + 1;
             }
         }
-        vector<int> dir = {0, 1, 0, -1, 0};
-        while(q.size()){
-            int t = q.size();
-            while(t--){
-                auto [x, y] = q.front();
-                q.pop();
-                for (int i = 0; i < 4; i++)
-                {
-                    int dx = x + dir[i], dy = y + dir[i + 1];
-                    if(isValid(dx, dy, n, m) && mat[dx][dy] == -1){
-                        q.push({dx, dy});
-                        mat[dx][dy] = mat[x][y] + 1;
-                    }
+        for (int i = m - 1; i >= 0; i--) {
+            for (int j = n - 1; j >= 0; j--) {
+                if (!mat[i][j]) continue;
+                int bottom = inf, right = inf;
+                if (i + 1 < m){
+                    bottom = mat[i + 1][j];
                 }
+                if (j + 1 < n){
+                    right = mat[i][j + 1];
+                }
+                mat[i][j] = min(mat[i][j], min(bottom, right) + 1);
             }
         }
-    }
-    bool isValid(int &i, int &j, int &n, int &m){
-        return i >= 0 && j >= 0 && i < n && j < m;
-    }
-    vector<vector<int>> updateMatrix(vector<vector<int>>& mat) {
-        bfs(mat);
         return mat;
     }
 };
