a46930b9fe955a657ba42763d3d7860e4b33117f
Gourab Singha <110812339+gourabsingha1@users.noreply.github.com>
2023-02-12 01:13:49 +0530

    Time: 259 ms (56.15%), Space: 65.9 MB (32.75%) - LeetHub

--- 0815-bus-routes/0815-bus-routes.cpp
+++ 0815-bus-routes/0815-bus-routes.cpp
@@ -3,7 +3,7 @@ public:
     int numBusesToDestination(vector<vector<int>>& routes, int source, int target) {
         queue<pair<int, int>> q; // stop, bus
         unordered_map<int, vector<int>> adj;
-        unordered_map<int, bool> vis, mp;
+        unordered_map<int, bool> visStop, visBus;
         for (int i = 0; i < routes.size(); i++)
         {
             for (int j = 0; j < routes[i].size(); j++)
@@ -16,20 +16,19 @@ public:
             int t = q.size();
             while(t--){
                 auto [stop, bus] = q.front();
-                vis[stop] = 1;
                 if(stop == target){
                     return bus;
                 }
+                visStop[stop] = 1;
                 q.pop();
                 for(auto &it : adj[stop]){
-                    if(mp[it]) continue;
+                    if(visBus[it]) continue;
                     for (int i = 0; i < routes[it].size(); i++)
                     {
-                        if(!vis[routes[it][i]]){
-                            q.push({routes[it][i], bus + 1});
-                        }
+                        if(visStop[routes[it][i]]) continue;
+                        q.push({routes[it][i], bus + 1});
                     }
-                    mp[it] = 1;
+                    visBus[it] = 1;
                 }
             }
         }
