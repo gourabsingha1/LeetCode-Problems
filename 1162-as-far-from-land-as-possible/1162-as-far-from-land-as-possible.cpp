002e8048f65d527c03db1d3876dd76af87673780
Gourab Singha <110812339+gourabsingha1@users.noreply.github.com>
2023-02-11 12:09:07 +0530

    Time: 68 ms (77.28%), Space: 21.5 MB (43.13%) - LeetHub

--- 1162-as-far-from-land-as-possible/1162-as-far-from-land-as-possible.cpp
+++ 1162-as-far-from-land-as-possible/1162-as-far-from-land-as-possible.cpp
@@ -1,42 +1,43 @@
-class Solution{
+class Solution {
 public:
-    int maxDistance(vector<vector<int>> &grid){
-        int n = grid.size(), ans = 0;
+    int bfs(vector<vector<int>> grid){
+        int levels = -1, n = grid.size();
         queue<pair<int, int>> q;
-
         for (int i = 0; i < n; i++)
         {
             for (int j = 0; j < n; j++)
             {
-                if (grid[i][j] == 1)
+                if(grid[i][j]){
                     q.push({i, j});
+                }
             }
         }
-
-        if (q.size() == 0 || q.size() == n * n)
+        if(!q.size() || q.size() == n * n){
             return -1;
-
-        vector<pair<int, int>> dirc = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
-
-        while (!q.empty())
-        {
-            int size = q.size();
-            while (size--)
-            {
-                auto [i, j] = q.front();
+        }
+        vector<int> dir = {0, 1, 0, -1, 0};
+        while(q.size()){
+            levels++;
+            int t = q.size();
+            while(t--){
+                auto [x, y] = q.front();
                 q.pop();
-                for (auto [dx, dy] : dirc)
+                for (int i = 0; i < 4; i++)
                 {
-                    int x = i + dx, y = j + dy;
-                    if (x >= 0 && x < n && y >= 0 && y < n && grid[x][y] == 0)
-                    {
-                        grid[x][y] = 1;
-                        q.push({x, y});
+                    int dx = x + dir[i], dy = y + dir[i + 1];
+                    if(isValid(dx, dy, n, n) && !grid[dx][dy]){
+                        q.push({dx, dy});
+                        grid[dx][dy] = 1;
                     }
                 }
             }
-            ans++;
         }
-        return ans - 1;
+        return levels;
+    }
+    bool isValid(int &i, int &j, int &n, int &m){
+        return i >= 0 && j >= 0 && i < n && j < m;
+    }
+    int maxDistance(vector<vector<int>>& grid) {
+        return bfs(grid);
     }
 };
