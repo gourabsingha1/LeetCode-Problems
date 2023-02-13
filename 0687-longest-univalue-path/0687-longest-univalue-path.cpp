f732251aa3375550063335227da9e5efd5682788
Gourab Singha <110812339+gourabsingha1@users.noreply.github.com>
2023-02-13 15:40:04 +0530

    Time: 138 ms (88.63%), Space: 71.9 MB (20.13%) - LeetHub

---
+++ 0687-longest-univalue-path/0687-longest-univalue-path.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int res = 0;
+    int dfs(TreeNode* root){
+        if(!root) return 0;
+        int left = dfs(root->left);
+        int right = dfs(root->right);
+        if(!(root->left && root->left->val == root->val)){
+            left = 0;
+        }
+        if(!(root->right && root->right->val == root->val)){
+            right = 0;
+        }
+        int temp = max(left, right) + 1;
+        res = max({res, left + right});
+        return temp;
+    }
+    int longestUnivaluePath(TreeNode* root) {
+        dfs(root);
+        return res;
+    }
+};
