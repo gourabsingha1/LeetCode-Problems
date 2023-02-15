4da7b28e080955132a913469f9ff3ca73e4bff5f
Gourab Singha <110812339+gourabsingha1@users.noreply.github.com>
2023-02-15 17:40:29 +0530

    Time: 192 ms (98.65%), Space: 40.1 MB (88.51%) - LeetHub

--- 1761-minimum-degree-of-a-connected-trio-in-a-graph/1761-minimum-degree-of-a-connected-trio-in-a-graph.cpp
+++ 1761-minimum-degree-of-a-connected-trio-in-a-graph/1761-minimum-degree-of-a-connected-trio-in-a-graph.cpp
@@ -2,21 +2,23 @@ class Solution {
 public:
     int minTrioDegree(int n, vector<vector<int>>& edges) {
         int res = INT_MAX;
-        unordered_set<int> adj[n + 1];
+        int adj[401][401] = {}, deg[401] = {};
         for (int i = 0; i < edges.size(); i++)
         {
-            adj[edges[i][0]].insert(edges[i][1]);
-            adj[edges[i][1]].insert(edges[i][0]);
+            int u = edges[i][0], v = edges[i][1];
+            adj[u][v] = 1;
+            adj[v][u] = 1;
+            deg[u]++, deg[v]++;
         }
         for (int i = 1; i <= n; i++)
         {
             for (int j = i + 1; j <= n; j++)
             {
+                if(!adj[i][j]) continue;
                 for (int k = j + 1; k <= n; k++)
                 {
-                    if(adj[i].count(j) && adj[i].count(k) && adj[j].count(k)){
-                        int size = adj[i].size() + adj[j].size() + adj[k].size() - 6;
-                        res = min(res, size);
+                    if(adj[i][k] && adj[j][k]){
+                        res = min(res, deg[i] + deg[j] + deg[k] - 6);
                     }
                 }
             }
