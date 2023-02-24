652e90aba8248377105945111750fba049d17202
Gourab Singha <110812339+gourabsingha1@users.noreply.github.com>
2023-02-24 15:50:20 +0530

    Time: 7 ms (90.78%), Space: 10.4 MB (64.16%) - LeetHub

--- 0841-keys-and-rooms/0841-keys-and-rooms.cpp
+++ 0841-keys-and-rooms/0841-keys-and-rooms.cpp
@@ -1,29 +1,25 @@
 class Solution {
 public:
-    bool dfsOfGraph(int V, vector<vector<int>> adj){
-        vector<int> vis(V, 0);
-        vis[0] = 1;
-        for(int i = 0; i < V; i++){
-            if(vis[i]){
-                dfs(i, vis, adj);
+    void dfs(int u, vector<bool> &vis, vector<vector<int>>& adj){
+        vis[u] = 1;
+        for(auto &v : adj[u]){
+            if(!vis[v]){
+                dfs(v, vis, adj);
             }
         }
-        int c = 0;
-        for(int &b : vis){
-            if(b) c++;
-        }
-        return c == V;
-    }
-    void dfs(int node, vector<int> &vis, vector<vector<int>> adj){
-        vis[node] = 1;
-        for(auto &it : adj[node]){
-            if(!vis[it]){
-                dfs(it, vis, adj);
-            }
-        }
-        return;
     }
     bool canVisitAllRooms(vector<vector<int>>& rooms) {
-        return dfsOfGraph(rooms.size(), rooms);
+        int n = rooms.size();
+        vector<bool> vis(n);
+        vis[0] = 1;
+        for(int i = 0; i < n; i++){
+            if(vis[i]){
+                dfs(i, vis, rooms);
+            }
+        }
+        for(auto x : vis){
+            if(!x) return 0;
+        }
+        return 1;
     }
 };
