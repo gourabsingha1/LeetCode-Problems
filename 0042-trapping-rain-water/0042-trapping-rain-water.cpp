338cc0ded19ddde4bc762556f9396da8e876543b
Gourab Singha <110812339+gourabsingha1@users.noreply.github.com>
2023-02-26 00:38:35 +0530

    Time: 33 ms (11.35%), Space: 20.3 MB (42.53%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.cpp
+++ 0042-trapping-rain-water/0042-trapping-rain-water.cpp
@@ -5,9 +5,8 @@ public:
         stack<int> decStack;
         for (int i = 0; i < n; i++)
         {
-            int last = -1;
             while (decStack.size() && height[decStack.top()] <= height[i]) {
-                last = height[decStack.top()];
+                int last = height[decStack.top()];
                 decStack.pop();
                 if(decStack.size()){
                     res += (min(height[decStack.top()], height[i]) - last) * (i - decStack.top() - 1);
