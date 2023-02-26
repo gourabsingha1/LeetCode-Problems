bc9e3e8c092ce61c8e8b1748162a70c779d09993
Gourab Singha <110812339+gourabsingha1@users.noreply.github.com>
2023-02-26 16:04:20 +0530

    Time: 338 ms (96.94%), Space: 101.3 MB (91.06%) - LeetHub

--- 1466-reorder-routes-to-make-all-paths-lead-to-the-city-zero/1466-reorder-routes-to-make-all-paths-lead-to-the-city-zero.cpp
+++ 1466-reorder-routes-to-make-all-paths-lead-to-the-city-zero/1466-reorder-routes-to-make-all-paths-lead-to-the-city-zero.cpp
@@ -1,13 +1,12 @@
 class Solution {
 public:
     int res = 0;
-    map<pair<int, int>, bool> m;
     void dfs(int u, vector<bool>& vis, vector<int> adj[]){
         vis[u] = 1;
         for (auto &v : adj[u]) {
-            if(!vis[v]){
-                res += m[{u, v}];
-                dfs(v, vis, adj);
+            if(!vis[abs(v)]){
+                res += v >= 0;
+                dfs(abs(v), vis, adj);
             }
         }
     }
@@ -17,8 +16,7 @@ public:
         {
             int u = connections[i][0], v = connections[i][1];
             adj[u].push_back(v);
-            adj[v].push_back(u);
-            m[{u, v}] = 1;
+            adj[v].push_back(-u);
         }
         vector<bool> vis(n);
         dfs(0, vis, adj);
