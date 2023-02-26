94d3c923613a0e12fda14cae519ae69f7832d6cc
Gourab Singha <110812339+gourabsingha1@users.noreply.github.com>
2023-02-26 10:02:27 +0530

    Time: 0 ms (100.00%), Space: 9.4 MB (96.89%) - LeetHub

--- 0784-letter-case-permutation/0784-letter-case-permutation.cpp
+++ 0784-letter-case-permutation/0784-letter-case-permutation.cpp
@@ -1,31 +1,24 @@
 class Solution {
 public:
-    void helper(int currInd, string& nums, set<string> &st){
+    void helper(int currInd, string& nums, vector<string> &res){
         int n = nums.size();
         if(currInd == n){
-            st.insert(nums);
+            res.push_back(nums);
             return;
         }
-        if(islower(nums[currInd])){
+        if(isdigit(nums[currInd])){
+            helper(currInd + 1, nums, res);
+        }
+        else{
             nums[currInd] = toupper(nums[currInd]);
-        }
-        else if(isupper(nums[currInd])){
+            helper(currInd + 1, nums, res);
             nums[currInd] = tolower(nums[currInd]);
+            helper(currInd + 1, nums, res);
         }
-        helper(currInd + 1, nums, st);
-        if(islower(nums[currInd])){
-            nums[currInd] = toupper(nums[currInd]);
-        }
-        else if(isupper(nums[currInd])){
-            nums[currInd] = tolower(nums[currInd]);
-        }
-        helper(currInd + 1, nums, st);
     }
     vector<string> letterCasePermutation(string s) {
-        set<string> st;
-        helper(0, s, st);
         vector<string> res;
-        res.assign(st.begin(), st.end());
+        helper(0, s, res);
         return res;
     }
 };
