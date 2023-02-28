6afead21e600841f15cdf46cb33ad0aabec80514
Gourab Singha <110812339+gourabsingha1@users.noreply.github.com>
2023-02-28 22:04:21 +0530

    Time: 55 ms (56.12%), Space: 37.4 MB (26.29%) - LeetHub

---
+++ 0297-serialize-and-deserialize-binary-tree/0297-serialize-and-deserialize-binary-tree.cpp
@@ -0,0 +1,31 @@
+class Codec {
+public:
+    string serialize(TreeNode* root) {
+        if(!root) {
+            return "n";
+        }
+        string s = to_string(root->val) + "," + serialize(root->left) + "," + serialize(root->right);
+        return s;
+    }
+    TreeNode* Deserialize(int& i, string& data) {
+        int n = data.size();
+        if(i < n && data[i] == ',') i++;
+        if(i >= n || data[i] == 'n') {
+            i++;
+            return NULL;
+        }
+        int j = i;
+        if(data[i] == '-') i++;
+        while(i < n && isdigit(data[i])) {
+            i++;
+        }
+        TreeNode* root = new TreeNode(stoi(data.substr(j, i - j)));
+        root->left = Deserialize(i, data);
+        root->right = Deserialize(i, data);
+        return root;
+    }
+    TreeNode* deserialize(string data) {
+        int i = 0;
+        return Deserialize(i, data);
+    }
+};
