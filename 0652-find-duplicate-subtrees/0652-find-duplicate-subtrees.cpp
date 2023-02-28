2514c61aa4ae8c6a1ddf33641c2a69a2b9a30c3d
Gourab Singha <110812339+gourabsingha1@users.noreply.github.com>
2023-02-28 16:53:10 +0530

    Time: 418 ms (5.01%), Space: 51 MB (28.47%) - LeetHub

--- 0652-find-duplicate-subtrees/0652-find-duplicate-subtrees.cpp
+++ 0652-find-duplicate-subtrees/0652-find-duplicate-subtrees.cpp
@@ -1,24 +1,24 @@
 class Solution {
 public:
     vector<TreeNode*> res;
-    unordered_map<string, int> m, seen;
-    void hashSubTree(string& hash, TreeNode* root, bool left) {
+    unordered_map<string, int> m;
+    void hashSubTree(string& hash, TreeNode* root) {
         if(!root){
             hash += "#";
             return;
         }
-        hash += to_string(root->val) + "*" + to_string(left);
-        hashSubTree(hash, root->left, 1);
-        hashSubTree(hash, root->right, 0);
+        hash += to_string(root->val) + "*";
+        hashSubTree(hash, root->left);
+        hashSubTree(hash, root->right);
     }
     void dfs(TreeNode* root) {
         if(!root) return;
         string hash;
-        hashSubTree(hash, root, 0);
+        hashSubTree(hash, root);
         m[hash]++;
-        if(m[hash] > 1 && !seen[hash]) {
+        if(m[hash] > 1) {
             res.push_back(root);
-            seen[hash] = 1;
+            m[hash] = -1e9;
         }
         dfs(root->left);
         dfs(root->right);
