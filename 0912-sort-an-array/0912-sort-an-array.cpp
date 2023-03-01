25f903648b0829afe50b76d70d7ab539f0fefd76
Gourab Singha <110812339+gourabsingha1@users.noreply.github.com>
2023-03-01 22:25:30 +0530

    Time: 128 ms (90.19%), Space: 69.8 MB (33.42%) - LeetHub

--- 0912-sort-an-array/0912-sort-an-array.cpp
+++ 0912-sort-an-array/0912-sort-an-array.cpp
@@ -1,12 +1,12 @@
 class Solution {
 public:
     vector<int> sortArray(vector<int>& nums) {
-        vector<int> bucket(1e5+1, 0);
-        int d = 5*(1e4);
-        for (int &x : nums){
-            bucket[x+d]++;
+        vector<int> bucket(1e5 + 1, 0);
+        int d = 5 * (1e4);
+        for (int& x : nums){
+            bucket[x + d]++;
         }
-        for (int i = 0, j = 0; i < 1e5+1; i++){
+        for (int i = 0, j = 0; i < 1e5 + 1; i++){
             while(bucket[i]--){
                 nums[j++] = i - d;
             }
