c032839dbad979cb2a34ca25ff9e65914698f73c
Gourab Singha <110812339+gourabsingha1@users.noreply.github.com>
2023-03-02 14:30:21 +0530

    Time: 0 ms (100.00%), Space: 6.1 MB (12.53%) - LeetHub

---
+++ 2429-minimize-xor/2429-minimize-xor.cpp
@@ -0,0 +1,48 @@
+class Solution {
+public:
+    string decToBin(int n){
+        string res;
+        while(n){
+            if(n & 1) res = '1' + res;
+            else res = '0' + res;
+            n /= 2;
+        }
+        return res;
+    }
+    int binToDec(string& s){
+        int n = s.size(), cnt = 0, res = 0;
+        while(n--){
+            res += (s[n] == '1') * (1 << cnt);
+            cnt++;
+        }
+        return res;
+    }
+    int minimizeXor(int num1, int num2) {
+        string s = decToBin(num1);
+        int cnt = __builtin_popcount(num2), n = s.size();
+        string res(n, '0');
+        for (int i = 0; i < n && cnt; i++)
+        {
+            if(s[i] == '1') {
+                res[i] = '1';
+                cnt--;
+            }
+        }
+        if(cnt) {
+            while(n-- && cnt) {
+                if(s[n] == '0') {
+                    res[n] = '1';
+                    cnt--;
+                }
+            }
+            if(cnt) {
+                reverse(res.begin(), res.end());
+                while(cnt--) {
+                    res += '1';
+                }
+                reverse(res.begin(), res.end());
+            }
+        }
+        return binToDec(res);
+    }
+};
