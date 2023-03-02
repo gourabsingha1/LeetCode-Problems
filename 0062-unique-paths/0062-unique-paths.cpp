7638e175e2238bd025d315ad31e6173e4b088d5a
Gourab Singha <110812339+gourabsingha1@users.noreply.github.com>
2023-03-02 11:32:56 +0530

    Time: 0 ms (100.00%), Space: 6 MB (89.07%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -3,16 +3,16 @@
 class Solution {
 public:
     ll nCr(ll n, ll r){
-        r = min(r, n-r);
         ll res = 1;
+        r = min(r, n - r);
         for (int i = 1; i <= r; i++)
         {
-            res = res*(n-r+i)/i;
+            res = res * (n - r + i) / i;
         }
         return res;
     }
     int uniquePaths(int m, int n) {
         if(m > n) swap(n, m);
-        return nCr(n+m-2, m-1);
+        return nCr(n + m - 2, m - 1);
     }
 };
