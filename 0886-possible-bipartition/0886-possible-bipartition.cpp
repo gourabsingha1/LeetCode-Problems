939b6c5cd466b046e2d1fefc541f684a60c7912e
Gourab Singha <110812339+gourabsingha1@users.noreply.github.com>
2023-03-03 11:34:27 +0530

    Time: 196 ms (89.39%), Space: 64.7 MB (65.34%) - LeetHub

--- 0886-possible-bipartition/0886-possible-bipartition.cpp
+++ 0886-possible-bipartition/0886-possible-bipartition.cpp
@@ -1,46 +1,44 @@
-class Solution
-{
+class Solution {
 public:
-    bool ans;
-    void dfs(vector<int> adj[], int u, vector<int> &color, int c)
-    {
-        color[u] = c;
-        for (auto v : adj[u])
-        {
-            if (color[v] == -1)
-            {
-                dfs(adj, v, color, c ^ 1);
-            }
-            else if (color[v] == color[u])
-            {
-                ans = false;
-                return;
+    bool bfs(int i, vector<int>& color, vector<vector<int>>& adj){
+        queue<int> q;
+        q.push(i);
+        while(q.size()){
+            int u = q.front();
+            q.pop();
+            for(auto& v : adj[u]){
+                if(color[v] == -1){
+                    color[v] = 1 - color[u];
+                    q.push(v); 
+                }
+                else if(color[v] == color[u]){
+                    return 0;
+                }
             }
         }
+        return 1;
     }
-    bool possibleBipartition(int n, vector<vector<int>> &d)
-    {
-        vector<int> adj[n + 1];
-        for (int i = 0; i < d.size(); i++)
+    bool isBipartite(vector<vector<int>>& graph) {
+        int n = graph.size(), res = 1;
+        vector<int> color(n, -1);
+        for(int i = 1; i < n; i++){
+            if(color[i] == -1){
+                color[i] = 0;
+                if(!bfs(i, color, graph)) {
+                    return 0;
+                }
+            }
+        }
+        return 1;
+    }
+    bool possibleBipartition(int n, vector<vector<int>>& dislikes) {
+        vector<vector<int>> adj(n + 1);
+        for (int i = 0; i < dislikes.size(); i++)
         {
-            int u = d[i][0];
-            int v = d[i][1];
-
+            int u = dislikes[i][0], v = dislikes[i][1];
             adj[u].push_back(v);
             adj[v].push_back(u);
         }
-
-        ans = true;
-        vector<int> color(n + 1, -1);
-
-        for (int i = 1; i <= n; i++)
-        {
-            if (color[i] == -1)
-            {
-                dfs(adj, i, color, 0);
-            }
-        }
-
-        return ans;
+        return isBipartite(adj);
     }
 };
