fbfcb7872a39feca73d35f273e9e3a4d9baaa201
Gourab Singha <110812339+gourabsingha1@users.noreply.github.com>
2023-03-03 12:04:49 +0530

    Time: 352 ms (18.95%), Space: 78.2 MB (16.52%) - LeetHub

---
+++ 0365-water-and-jug-problem/0365-water-and-jug-problem.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    bool canMeasureWater(int jug1Capacity, int jug2Capacity, int targetCapacity) {
+        unordered_map<int, bool> seen;
+        vector<int> dir = {jug1Capacity, -jug1Capacity, jug2Capacity, -jug2Capacity};
+        queue<int> q;
+        q.push(0);
+        seen[0] = 1;
+        while(q.size()) {
+            int x = q.front();
+            q.pop();
+            if(x == targetCapacity) {
+                return 1;
+            }
+            for (int i = 0; i < 4; i++)
+            {
+                int dx = x + dir[i];
+                if(dx > 0 && dx <= jug1Capacity + jug2Capacity && !seen[dx]) {
+                    q.push(dx);
+                    seen[dx] = 1;
+                }
+            }
+        }
+        return 0;
+    }
+};
