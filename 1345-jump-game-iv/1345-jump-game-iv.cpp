fcd1f707dc33be9ca9038c48fe9e66e297bb4c86
Gourab Singha <110812339+gourabsingha1@users.noreply.github.com>
2023-03-05 19:31:56 +0530

    Time: 230 ms (69.57%), Space: 75.9 MB (61.21%) - LeetHub

--- 1345-jump-game-iv/1345-jump-game-iv.cpp
+++ 1345-jump-game-iv/1345-jump-game-iv.cpp
@@ -3,7 +3,7 @@ public:
     int minJumps(vector<int>& arr) {
         int res = 0, n = arr.size();
         unordered_map<int, vector<int>> adj;
-        vector<bool> vis(n, 0);
+        vector<bool> vis(n);
         for (int i = 0; i < n; i++)
         {
             adj[arr[i]].push_back(i);
@@ -11,26 +11,25 @@ public:
         queue<int> q;
         q.push(0), vis[0] = 1;
         while(q.size()){
-            int size = q.size();
-            while(size--){
-                int i = q.front();
+            int t = q.size();
+            while(t--){
+                int i = q.front(), u = arr[i];
                 q.pop();
-                if(i == n-1){
+                if(i == n - 1){
                     return res;
                 }
-                if(i > 0) adj[arr[i]].push_back(i-1);
-                if(i+1 < n) adj[arr[i]].push_back(i+1);
-                for(int &x : adj[arr[i]]){
-                    if(!vis[x]){
-                        q.push(x);
-                        vis[x] = 1;
+                if(i + 1 < n) adj[u].push_back(i + 1);
+                if(i > 0) adj[u].push_back(i - 1);
+                for(int &v : adj[u]){
+                    if(!vis[v]){
+                        q.push(v);
+                        vis[v] = 1;
                     }
                 }
-                adj[arr[i]].clear();
+                adj[u].clear();
             }
             res++;
         }
-
         return -1;
     }
 };
