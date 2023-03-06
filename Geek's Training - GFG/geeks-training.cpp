d18bb89cef20ee2ac13ff8dd32095d545a3ca506
Gourab Singha <110812339+gourabsingha1@users.noreply.github.com>
2023-03-06 18:41:10 +0530

    Added solution - LeetHub

--- Geek's Training - GFG/geeks-training.cpp
+++ Geek's Training - GFG/geeks-training.cpp
@@ -5,25 +5,27 @@ using namespace std;
 // } Driver Code Ends
 class Solution {
   public:
-    // tabulation
+    // space optimization
     int maximumPoints(vector<vector<int>>& points, int n) {
-        vector<vector<int>> dp(n, vector<int> (3));
-        dp[0][0] = max(points[0][1], points[0][2]);
-        dp[0][1] = max(points[0][0], points[0][2]);
-        dp[0][2] = max(points[0][0], points[0][1]);
+        vector<int> prev(3);
+        prev[0] = max(points[0][1], points[0][2]);
+        prev[1] = max(points[0][0], points[0][2]);
+        prev[2] = max(points[0][0], points[0][1]);
         for (int day = 1; day < n; day++)
         {
+            vector<int> temp(3);
             for (int last = 0; last < 3; last++)
             {
                 for (int task = 0; task < 3; task++)
                 {
                     if(last != task) {
-                        dp[day][last] = max(dp[day][last], points[day][task] + dp[day - 1][task]);
+                        temp[last] = max(temp[last], points[day][task] + prev[task]);
                     }
                 }
             }
+            prev = temp;
         }
-        return max({dp[n - 1][0], dp[n - 1][1], dp[n - 1][2]});
+        return max({prev[0], prev[1], prev[2]});
     }
 };
 
