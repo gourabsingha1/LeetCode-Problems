c5e1494e79ac03e942692a01264fd28a77bf1cc0
Gourab Singha <110812339+gourabsingha1@users.noreply.github.com>
2023-03-07 00:46:57 +0530

    Added solution - LeetHub

--- Number of Unique Paths - GFG/number-of-unique-paths.cpp
+++ Number of Unique Paths - GFG/number-of-unique-paths.cpp
@@ -9,23 +9,30 @@ using namespace std;
 class Solution
 {
     public:
-    // memoization
-    int helper(int n, int m, vector<vector<int>>& dp) {
-        if(n == 0 && m == 0) {
-            return 1;
-        }
-        if(n < 0 || m < 0) {
-            return 0;
-        }
-        if(dp[n][m] != -1) {
-            return dp[n][m];
-        }
-        return dp[n][m] = helper(n - 1, m, dp) + helper(n, m - 1, dp);
-    }
+    // tabulation
     int NumberOfPath(int n, int m)
     {
-        vector<vector<int>> dp(n, vector<int> (m, -1));
-        return helper(n - 1, m - 1, dp);
+        vector<vector<int>> dp(n, vector<int> (m));
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = 0; j < m; j++)
+            {
+                int up = 0, left = 0;
+                if(i == 0 && j == 0) {
+                    dp[i][j] = 1;
+                }
+                else {
+                    if(i > 0) {
+                        up = dp[i - 1][j];
+                    }
+                    if(j > 0) {
+                        left = dp[i][j - 1];
+                    }
+                    dp[i][j] = up + left;
+                }
+            }
+        }
+        return dp[n - 1][m - 1];
     }
 };
 
