fd43295ea3d71361a168fde206ba4b50073a814c
Gourab Singha <110812339+gourabsingha1@users.noreply.github.com>
2023-03-07 18:24:47 +0530

    Time: 7 ms (77.51%), Space: 11.7 MB (55.51%) - LeetHub

--- 0139-word-break/0139-word-break.cpp
+++ 0139-word-break/0139-word-break.cpp
@@ -1,28 +1,24 @@
 class Solution {
 public:
-    // memoization
-    bool helper(int n, string& s, unordered_map<string, bool>& dict, vector<int>& dp) {
-        if(n < 0) {
-            return 1;
-        }
-        if(dp[n] != -1) {
-            return dp[n];
-        }
-        bool res = 0;
-        for (int i = n; i >= 0; i--)
-        {
-            if(dict[s.substr(i, n + 1 - i)]) {
-                res |= helper(i - 1, s, dict, dp);
-            }
-        }
-        return dp[n] = res;
-    }
+    // tabulation
     bool wordBreak(string s, vector<string>& wordDict) {
+        int n = s.size();
         unordered_map<string, bool> dict;
-        vector<int> dp(s.size(), -1);
+        vector<int> dp(n + 1, 0);
         for(auto& s : wordDict) {
             dict[s] = 1;
         }
-        return helper(s.size() - 1, s, dict, dp);
+        dp[0] = 1;
+        for (int i = 1; i <= n; i++)
+        {
+            for (int j = i - 1; j >= 0; j--)
+            {
+                if(dp[j] && dict[s.substr(j, i - j)]) {
+                    dp[i] = 1;
+                    break;
+                }
+            }
+        }
+        return dp[n];
     }
 };
