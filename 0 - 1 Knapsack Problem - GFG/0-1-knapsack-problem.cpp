cb3048d5d4bc082fef95bbb42ad50190186863a0
Gourab Singha <110812339+gourabsingha1@users.noreply.github.com>
2023-03-10 23:24:27 +0530

    Added solution - LeetHub

--- 0 - 1 Knapsack Problem - GFG/0-1-knapsack-problem.cpp
+++ 0 - 1 Knapsack Problem - GFG/0-1-knapsack-problem.cpp
@@ -7,25 +7,27 @@ using namespace std;
 class Solution
 {
     public:
-    // memoization
-    int helper(int W, int wt[], int val[], int n, vector<vector<int>>& dp) {
-        if(n == 0 || W == 0) {
-            return 0;
-        }
-        if(dp[n][W]) {
-            return dp[n][W];
-        }
-        int take = INT_MIN;
-        if(W - wt[n - 1] >= 0) {
-            take = helper(W - wt[n - 1], wt, val, n - 1, dp) + val[n - 1];
-        }
-        int notTake = helper(W, wt, val, n - 1, dp);
-        return dp[n][W] = max(take, notTake);
-    }
+    // tabulation
     int knapSack(int W, int wt[], int val[], int n) 
     {
-        vector<vector<int>> dp(n + 1, vector<int> (W + 1, 0));
-        return helper(W, wt, val, n, dp);
+        vector<vector<int>> dp(n, vector<int> (W + 1, 0));
+        for (int j = wt[0]; j <= W; j++)
+        {
+            dp[0][j] = val[0];
+        }
+        for (int i = 1; i < n; i++)
+        {
+            for (int j = 0; j <= W; j++)
+            {
+                int take = INT_MIN;
+                if(j - wt[i] >= 0) {
+                    take = dp[i - 1][j - wt[i]] + val[i];
+                }
+                int notTake = dp[i - 1][j];
+                dp[i][j] = max(take, notTake);
+            }
+        }
+        return dp[n - 1][W];
     }
 };
 
