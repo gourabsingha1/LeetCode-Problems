dc6041a8939a0b313079be7930bbca66ceaebda7
Gourab Singha <110812339+gourabsingha1@users.noreply.github.com>
2023-03-10 13:49:38 +0530

    Time: 0 ms (100.00%), Space: 7.8 MB (50.90%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.cpp
+++ 0063-unique-paths-ii/0063-unique-paths-ii.cpp
@@ -1,40 +1,21 @@
 class Solution {
 public:
+    // memoization
+    int helper(int n, int m, vector<vector<int>>& obstacleGrid, vector<vector<int>>& dp) {
+        if(n < 0 || m < 0 || obstacleGrid[n][m]) {
+            return 0;
+        }
+        if(n == 0 && m == 0) {
+            return 1;
+        }
+        if(dp[n][m] != -1) {
+            return dp[n][m];
+        }
+        return dp[n][m] = helper(n - 1, m, obstacleGrid, dp) + helper(n, m - 1, obstacleGrid, dp);
+    }
     int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid) {
-        if(obstacleGrid[0][0]) return 0;
         int n = obstacleGrid.size(), m = obstacleGrid[0].size();
-        for (int i = 0; i < n; i++){
-            if(obstacleGrid[i][0]){
-                for (int j = i; j < n; j++) obstacleGrid[j][0] = 0;
-                break;
-            }
-            obstacleGrid[i][0] = 1;
-        }
-        for (int i = 1; i < m; i++){
-            if(obstacleGrid[0][i]){
-                for (int j = i; j < m; j++) obstacleGrid[0][j] = 0;
-                break;
-            }
-            obstacleGrid[0][i] = 1;
-        }
-        for (int i = 1; i < n; i++)
-        {
-            for (int j = 1; j < m; j++)
-            {
-                obstacleGrid[i][j] = !obstacleGrid[i][j];
-            }
-        }
-
-        for (int i = 1; i < n; i++)
-        {
-            for (int j = 1; j < m; j++)
-            {
-                if(obstacleGrid[i][j]){
-                    obstacleGrid[i][j] = obstacleGrid[i-1][j] + obstacleGrid[i][j-1];
-                }
-            }
-        }
-        
-        return obstacleGrid[n-1][m-1];
+        vector<vector<int>> dp(n, vector<int> (m, -1));
+        return helper(n - 1, m - 1, obstacleGrid, dp);
     }
 };
