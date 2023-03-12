e2fa31abc82ee517ae4d13bea46e6c3e9360c5ac
Gourab Singha <110812339+gourabsingha1@users.noreply.github.com>
2023-03-12 21:15:07 +0530

    Time: 545 ms (20.92%), Space: 144.9 MB (19.13%) - LeetHub

--- 0912-sort-an-array/0912-sort-an-array.cpp
+++ 0912-sort-an-array/0912-sort-an-array.cpp
@@ -1,16 +1,34 @@
 class Solution {
 public:
+    // merge sort
+    void mergeSort(int low, int high, vector<int>& nums) {
+        if(low >= high) {
+            return;
+        }
+
+        // divide
+        int mid = (low + high) / 2;
+        mergeSort(low, mid, nums);
+        mergeSort(mid + 1, high, nums);
+
+        // merge
+        merge(low, mid, high, nums);
+    }
+    void merge(int low, int mid, int high, vector<int>& nums) {
+        if (low >= high) return;
+        int l = low, r = mid + 1, k = 0, size = high - low + 1;
+        vector<int> sorted(size, 0);
+        while (l <= mid and r <= high)
+            sorted[k++] = nums[l] < nums[r] ? nums[l++] : nums[r++];
+        while (l <= mid) 
+            sorted[k++] = nums[l++];
+        while (r <= high) 
+            sorted[k++] = nums[r++];
+        for (k = 0; k < size; k++)
+            nums[k + low] = sorted[k];
+    }
     vector<int> sortArray(vector<int>& nums) {
-        vector<int> bucket(1e5 + 1, 0);
-        int d = 5 * (1e4);
-        for (int& x : nums){
-            bucket[x + d]++;
-        }
-        for (int i = 0, j = 0; i < 1e5 + 1; i++){
-            while(bucket[i]--){
-                nums[j++] = i - d;
-            }
-        }
+        mergeSort(0, nums.size() - 1, nums);
         return nums;
     }
 };
