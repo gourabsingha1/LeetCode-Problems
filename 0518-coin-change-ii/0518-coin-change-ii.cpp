51ea16ccf8743d93b7976034b466848bbfe147b8
Gourab Singha <110812339+gourabsingha1@users.noreply.github.com>
2023-03-13 17:23:17 +0530

    Time: 50 ms (34.50%), Space: 18.5 MB (49.72%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.cpp
+++ 0518-coin-change-ii/0518-coin-change-ii.cpp
@@ -1,22 +1,26 @@
 class Solution {
 public:
-    // memoization
-    int helper(int n, vector<int>& coins, int amount, vector<vector<int>>& dp) {
-        if(n == 0 || amount == 0) {
-            return amount % coins[n] == 0;
-        }
-        if(dp[n][amount] != -1) {
-            return dp[n][amount];
-        }
-        int take = 0;
-        if(amount - coins[n] >= 0) {
-            take = helper(n, coins, amount - coins[n], dp);
-        }
-        int notTake = helper(n - 1, coins, amount, dp);
-        return dp[n][amount] = take + notTake;
-    }
+    // tabulation
     int change(int amount, vector<int>& coins) {
-        vector<vector<int>> dp(coins.size(), vector<int> (amount + 1, -1));
-        return helper(coins.size() - 1, coins, amount, dp);
+        int n = coins.size();
+        vector<vector<int>> dp(n, vector<int> (amount + 1, -1));
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = 0; j <= amount; j++)
+            {
+                if(i == 0) {
+                    dp[i][j] = j % coins[i] == 0;
+                }
+                else {
+                    int take = 0;
+                    if(j - coins[i] >= 0) {
+                        take = dp[i][j - coins[i]];
+                    }
+                    int notTake = dp[i - 1][j];
+                    dp[i][j] = take + notTake;
+                }
+            }
+        }
+        return dp[n - 1][amount];
     }
 };
