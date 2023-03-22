e9eebf8e98ccd550751c10018c690d5b5f6a612b
Gourab Singha <110812339+gourabsingha1@users.noreply.github.com>
2023-03-22 17:22:56 +0530

    Time: 313 ms (8.83%), Space: 5.9 MB (83.68%) - LeetHub

--- 0060-permutation-sequence/0060-permutation-sequence.cpp
+++ 0060-permutation-sequence/0060-permutation-sequence.cpp
@@ -1,5 +1,15 @@
 class Solution {
 public:
+    void nextPermutation(string& nums) {
+        int i = nums.size() - 1, j = i;
+        while(i - 1 >= 0 && nums[i - 1] >= nums[i]) i--;
+        i--;
+        if(i >= 0) {
+            while(nums[i] >= nums[j]) j--;
+            swap(nums[i], nums[j]);
+        }
+        sort(nums.begin() + i + 1, nums.end());
+    }
     string getPermutation(int n, int k) {
         string num;
         for (int i = 1; i <= n; i++)
@@ -7,7 +17,7 @@ public:
             num += '0' + i;
         }
         while(--k) {
-            next_permutation(num.begin(), num.end());
+            nextPermutation(num);
         }
         return num;
     }
