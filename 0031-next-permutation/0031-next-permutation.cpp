37faaecbeaf992742ce56a361daa94ab35a4ef88
Gourab Singha <110812339+gourabsingha1@users.noreply.github.com>
2023-03-22 17:16:20 +0530

    Time: 0 ms (100.00%), Space: 12.2 MB (42.09%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.cpp
+++ 0031-next-permutation/0031-next-permutation.cpp
@@ -1,6 +1,13 @@
 class Solution {
 public:
     void nextPermutation(vector<int>& nums) {
-        next_permutation(nums.begin(), nums.end());
+        int i = nums.size() - 1, j = i;
+        while(i - 1 >= 0 && nums[i - 1] >= nums[i]) i--;
+        i--;
+        if(i >= 0) {
+            while(nums[i] >= nums[j]) j--;
+            swap(nums[i], nums[j]);
+        }
+        sort(nums.begin() + i + 1, nums.end());
     }
 };
