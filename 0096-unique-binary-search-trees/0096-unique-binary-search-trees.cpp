2d201ed27562f6b8387cc9f3256baa1d6a932213
Gourab Singha <110812339+gourabsingha1@users.noreply.github.com>
2023-03-23 20:24:37 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (97.08%) - LeetHub

--- 0096-unique-binary-search-trees/0096-unique-binary-search-trees.cpp
+++ 0096-unique-binary-search-trees/0096-unique-binary-search-trees.cpp
@@ -1,17 +1,15 @@
-#define ll long long int
-ll M = 1e11 + 7;
 class Solution {
 public:
-    ll nCr(ll n, ll r, ll m = M){
-        r = min(r, n-r);
-        ll res = 1;
+    long nCr(int n, int r){
+        r = min(r, n - r);
+        long res = 1;
         for (int i = 1; i <= r; i++)
         {
-            res = (res*(n-r+i) / i) % m;
+            res = (res * (n - r + i) / i);
         }
         return res;
     }
     int numTrees(int n) {
-        return nCr(2*n, n)/(n+1);
+        return nCr(2 * n, n) / (n + 1);
     }
 };
