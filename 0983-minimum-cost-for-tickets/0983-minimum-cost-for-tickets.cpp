f73c06913ff27e12416a74ca62f6a280754f9e3e
Gourab Singha <110812339+gourabsingha1@users.noreply.github.com>
2023-03-28 20:51:33 +0530

    Time: 10 ms (15.16%), Space: 9.7 MB (30.82%) - LeetHub

--- 0983-minimum-cost-for-tickets/0983-minimum-cost-for-tickets.cpp
+++ 0983-minimum-cost-for-tickets/0983-minimum-cost-for-tickets.cpp
@@ -2,15 +2,15 @@ class Solution {
 public:
     // memoization
     vector<int> pass = {1, 7, 30};
-    int helper(int i, int costInd, vector<int>& days, vector<int>& costs, vector<vector<int>>& dp) {
+    int helper(int i, int costInd, vector<int>& days, vector<int>& costs, vector<int>& dp) {
         if(costInd == 3 || i == days.size()) {
             if(i < days.size()) {
                 return 1e9;
             }
             return 0;
         }
-        if(dp[i][costInd] != -1) {
-            return dp[i][costInd];
+        if(dp[i] != -1) {
+            return dp[i];
         }
         int j = i;
         while(i < days.size() && days[i] < days[j] + pass[costInd]) {
@@ -18,10 +18,10 @@ public:
         }
         int take = costs[costInd] + helper(i, 0, days, costs, dp);
         int notTake = helper(j, costInd + 1, days, costs, dp);
-        return dp[j][costInd] = min(take, notTake);
+        return dp[j] = min(take, notTake);
     }
     int mincostTickets(vector<int>& days, vector<int>& costs) {
-        vector<vector<int>> dp(days.size(), vector<int> (3, -1));
+        vector<int> dp(days.size(), -1);
         return helper(0, 0, days, costs, dp);
     }
 };
