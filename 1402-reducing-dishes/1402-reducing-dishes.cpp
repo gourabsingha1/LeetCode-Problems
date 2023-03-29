238bf513b6596aee44d0c6d0c9bf62f79dec8e92
Gourab Singha <110812339+gourabsingha1@users.noreply.github.com>
2023-03-29 11:07:11 +0530

    Time: 60 ms (17.33%), Space: 20.9 MB (49.34%) - LeetHub

--- 1402-reducing-dishes/1402-reducing-dishes.cpp
+++ 1402-reducing-dishes/1402-reducing-dishes.cpp
@@ -1,20 +1,21 @@
 class Solution {
 public:
     // memoization
-    int helper(int i, int j, vector<int>& satisfaction, vector<vector<int>>& dp) {
-        if(i == satisfaction.size()) {
+    int helper(int n, int j, vector<int>& satisfaction, vector<vector<int>>& dp) {
+        if(n < 0) {
             return 0;
         }
-        if(dp[i][j] != -1) {
-            return dp[i][j];
+        if(dp[n][j] != -1) {
+            return dp[n][j];
         }
-        int take = satisfaction[i] * j + helper(i + 1, j + 1, satisfaction, dp);
-        int notTake = helper(i + 1, j, satisfaction, dp);
-        return dp[i][j] = max(take, notTake);
+        int take = satisfaction[n] * j + helper(n - 1, j + 1, satisfaction, dp);
+        int notTake = helper(n - 1, j, satisfaction, dp);
+        return dp[n][j] = max(take, notTake);
     }
     int maxSatisfaction(vector<int>& satisfaction) {
-        sort(satisfaction.begin(), satisfaction.end());
-        vector<vector<int>> dp(satisfaction.size(), vector<int> (satisfaction.size() + 1, -1));
-        return helper(0, 1, satisfaction, dp);
+        int n = satisfaction.size();
+        sort(satisfaction.rbegin(), satisfaction.rend());
+        vector<vector<int>> dp(n, vector<int> (n + 1, -1));
+        return helper(n - 1, 1, satisfaction, dp);
     }
 };
