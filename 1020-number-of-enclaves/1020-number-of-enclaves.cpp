911651165dcb116ef7692e1835bf9a9cf86f170d
Gourab Singha <110812339+gourabsingha1@users.noreply.github.com>
2023-04-07 10:37:29 +0530

    Time: 81 ms (51.75%), Space: 31.1 MB (42.61%) - LeetHub

--- 1020-number-of-enclaves/1020-number-of-enclaves.cpp
+++ 1020-number-of-enclaves/1020-number-of-enclaves.cpp
@@ -1,14 +1,14 @@
 class Solution {
 public:
     bool dfs(int i, int j, vector<vector<int>>& grid, int &sum) {
-        if(i < 0 || j < 0 || i == grid.size() || j == grid[0].size() || grid[i][j] == -1){
+        if(i < 0 || j < 0 || i == grid.size() || j == grid[0].size()){
             return 0;
         }
         if(!grid[i][j]) return 1;
         grid[i][j] = 0;
         sum++;
 
-        return dfs(i-1, j, grid, sum) & dfs(i, j-1, grid, sum) & dfs(i+1, j, grid, sum) & dfs(i, j+1, grid, sum);
+        return dfs(i - 1, j, grid, sum) & dfs(i, j - 1, grid, sum) & dfs(i + 1, j, grid, sum) & dfs(i, j + 1, grid, sum);
     }
     int numEnclaves(vector<vector<int>>& grid) {
         int res = 0;
