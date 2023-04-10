519c50525607ed8b9dbe6b201acbfcea87e9980d
Gourab Singha <110812339+gourabsingha1@users.noreply.github.com>
2023-04-10 07:40:54 +0530

    Time: 0 ms (100.00%), Space: 6.3 MB (81.54%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char> st;
+        for(char &c : s){
+            if(!st.empty()){
+                if(c == ')' && st.top() == '(' || c == '}' && st.top() == '{' || c == ']' && st.top() == '['){
+                    st.pop();
+                }
+                else{
+                    st.push(c);
+                }
+            }
+            else{
+                st.push(c);
+            }
+        }
+        return st.empty();
+    }
+};
