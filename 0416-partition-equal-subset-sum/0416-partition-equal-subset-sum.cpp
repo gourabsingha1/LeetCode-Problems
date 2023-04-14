8dde55b83b79fbe7593599ee0e66b9a12427f204
Gourab Singha <110812339+gourabsingha1@users.noreply.github.com>
2023-04-14 11:08:11 +0530

    Time: 404 ms (33.36%), Space: 95.1 MB (29.30%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
@@ -1,27 +1,31 @@
 class Solution {
 public:
-    // memoization
-    bool helper(int n, int sum, vector<int>& nums, vector<vector<int>>& dp) {
-        if(!sum) {
-            return 1;
-        }
-        if(n < 0 || sum < 0) {
-            return 0;
-        }
-        if(dp[n][sum] != -1) {
-            return dp[n][sum];
-        }
-        bool take = helper(n - 1, sum - nums[n], nums, dp);
-        bool notTake = helper(n - 1, sum, nums, dp);
-        return dp[n][sum] = take || notTake;
-    }
+    // tabulation
     bool canPartition(vector<int>& nums) {
         int n = nums.size(), sum = accumulate(nums.begin(), nums.end(), 0);
         if(sum & 1) {
             return 0;
         }
         sum /= 2;
-        vector<vector<int>> dp(n, vector<int> (sum + 1, -1));
-        return helper(n - 1, sum, nums, dp);
+        vector<vector<int>> dp(n, vector<int> (sum + 1, 0));
+        dp[0][0] = 1;
+        for (int i = 1; i < n; i++)
+        {
+            for (int j = 0; j <= sum; j++)
+            {
+                if(j == 0) {
+                    dp[i][j] = 1;
+                }
+                else {
+                    bool take = 0;
+                    if(j - nums[i] >= 0) {
+                        take = dp[i - 1][j - nums[i]];
+                    }
+                    bool notTake = dp[i - 1][j];
+                    dp[i][j] = take || notTake;
+                }
+            }
+        }
+        return dp[n - 1][sum];
     }
 };
