9730c08aa14ab016dfef998624f450234fc8df3a
Gourab Singha <110812339+gourabsingha1@users.noreply.github.com>
2023-04-21 20:17:35 +0530

    Time: 349 ms (19.34%), Space: 53.2 MB (33.49%) - LeetHub

--- 0879-profitable-schemes/0879-profitable-schemes.cpp
+++ 0879-profitable-schemes/0879-profitable-schemes.cpp
@@ -1,23 +1,23 @@
 class Solution {
 public:
     int M = 1e9 + 7;
-    int helper(int pSum, int i, int n, int minProfit, vector<int>& group, vector<int>& profit, vector<vector<vector<int>>>& dp) {
+    int helper(int i, int n, int minProfit, vector<int>& group, vector<int>& profit, vector<vector<vector<int>>>& dp) {
         if(n < 0) {
             return 0;
         }
         if(i == profit.size()) {
-            return minProfit <= pSum;
+            return minProfit == 0;
         }
-        if(dp[i][n][pSum] != -1) {
-            return dp[i][n][pSum];
+        if(dp[i][n][minProfit] != -1) {
+            return dp[i][n][minProfit];
         }
 
-        int take = helper(min(pSum + profit[i], minProfit), i + 1, n - group[i], minProfit, group, profit, dp);
-        int notTake = helper(pSum, i + 1, n, minProfit, group, profit, dp);
-        return dp[i][n][pSum] = (take + notTake) % M;
+        int take = helper(i + 1, n - group[i], max(0, minProfit - profit[i]), group, profit, dp);
+        int notTake = helper(i + 1, n, minProfit, group, profit, dp);
+        return dp[i][n][minProfit] = (take + notTake) % M;
     }
     int profitableSchemes(int n, int minProfit, vector<int>& group, vector<int>& profit) {
         vector<vector<vector<int>>> dp(profit.size(), vector<vector<int>> (n + 1, vector<int> (minProfit + 1, -1)));
-        return helper(0, 0, n, minProfit, group, profit, dp);
+        return helper(0, n, minProfit, group, profit, dp);
     }
 };
