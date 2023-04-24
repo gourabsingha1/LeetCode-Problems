17266302a5a6e8238425e59d8062f8b8b227ff35
Gourab Singha <110812339+gourabsingha1@users.noreply.github.com>
2023-04-24 18:06:36 +0530

    Time: 603 ms (28.81%), Space: 89.2 MB (5.04%) - LeetHub

--- 0494-target-sum/0494-target-sum.cpp
+++ 0494-target-sum/0494-target-sum.cpp
@@ -1,14 +1,21 @@
 class Solution {
 public:
-    int helper(int n, int cur, vector<int>& nums, int target) {
+    // memoization
+    int helper(int n, vector<int>& nums, int target, unordered_map<string, int>& dp) {
         if(n < 0) {
-            return cur == target;
+            return !target;
         }
-        int neg = helper(n - 1, cur - nums[n], nums, target);
-        int pos = helper(n - 1, cur + nums[n], nums, target);
-        return neg + pos;
+        string memo = to_string(n) + "*" + to_string(target);
+        if(dp.find(memo) != dp.end()) {
+            return dp[memo];
+        }
+
+        int neg = neg = helper(n - 1, nums, target - nums[n], dp);
+        int pos = helper(n - 1, nums, target + nums[n], dp);
+        return dp[memo] = neg + pos;
     }
     int findTargetSumWays(vector<int>& nums, int target) {
-        return helper(nums.size() - 1, 0, nums, target);
+        unordered_map<string, int> dp;
+        return helper(nums.size() - 1, nums, target, dp);
     }
 };
