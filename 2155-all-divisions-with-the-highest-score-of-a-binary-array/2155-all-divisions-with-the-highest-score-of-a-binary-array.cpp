f0324c1b6ab3abd75bf86f5068de8a4f9f54f750
Gourab Singha <110812339+gourabsingha1@users.noreply.github.com>
2023-04-24 17:26:49 +0530

    Time: 363 ms (98.21%), Space: 286.7 MB (61.79%) - LeetHub

--- 2155-all-divisions-with-the-highest-score-of-a-binary-array/2155-all-divisions-with-the-highest-score-of-a-binary-array.cpp
+++ 2155-all-divisions-with-the-highest-score-of-a-binary-array/2155-all-divisions-with-the-highest-score-of-a-binary-array.cpp
@@ -1,23 +1,21 @@
 class Solution {
 public:
     vector<int> maxScoreIndices(vector<int>& nums) {
-        unordered_map<int, vector<int>> mp;
-        int n = nums.size(), pre[n];
-        pre[0] = nums[0];
-        for (int i = 1; i < n; i++)
-        {
-            pre[i] = nums[i] + pre[i - 1];
-        }
-        int ma = pre[n - 1];
-        mp[ma] = {0};
+        vector<int> res = {0};
+        int n = nums.size(), left = 0, right = accumulate(nums.begin(), nums.end(), 0), maxScore = right;
         for (int i = 0; i < n; i++)
         {
-            int left = i + 1 - pre[i], right = pre[n - 1] - pre[i];
-            if(ma <= left + right) {
-                ma = left + right;
-                mp[ma].push_back(i + 1);
+            left += (nums[i] == 0);
+            right -= (nums[i] == 1);
+            int curScore = left + right;
+            if(maxScore < curScore) {
+                maxScore = curScore;
+                res.clear();
+            }
+            if(maxScore == curScore) {
+                res.push_back(i + 1);
             }
         }
-        return mp[ma];
+        return res;
     }
 };
