9fa38ecb00f0a18d5b0f6a1e2cf2492fa3647298
Gourab Singha <110812339+gourabsingha1@users.noreply.github.com>
2023-05-23 11:01:01 +0530

    Time: 24 ms (98.72%), Space: 19.9 MB (34.32%) - LeetHub

--- 0703-kth-largest-element-in-a-stream/0703-kth-largest-element-in-a-stream.cpp
+++ 0703-kth-largest-element-in-a-stream/0703-kth-largest-element-in-a-stream.cpp
@@ -4,17 +4,23 @@ public:
     int k;
     KthLargest(int k, vector<int>& nums) {
         this->k = k;
-        for(int &x : nums){
-            pq.push(x);
-            if(pq.size() > k){
+        for(int& num : nums){
+            if(pq.size() < k){
+                pq.push(num);
+            }
+            else if(pq.size() == k && pq.top() < num) {
+                pq.push(num);
                 pq.pop();
             }
         }
     }
     
     int add(int val) {
-        pq.push(val);
-        if(pq.size() > k){
+        if(pq.size() < k){
+            pq.push(val);
+        }
+        else if(pq.size() == k && pq.top() < val) {
+            pq.push(val);
             pq.pop();
         }
         return pq.top();
