137c760ee6e13c9fd8d326d178a0925e201de9dd
Gourab Singha <110812339+gourabsingha1@users.noreply.github.com>
2023-05-24 11:53:34 +0530

    Time: 300 ms (47.78%), Space: 92 MB (60.84%) - LeetHub

--- 2542-maximum-subsequence-score/2542-maximum-subsequence-score.cpp
+++ 2542-maximum-subsequence-score/2542-maximum-subsequence-score.cpp
@@ -1,3 +1,6 @@
+// minHeap will give cur minimum of nums1 which should be logically omitted when size >= k
+// sorting nums2 wise will give current minimum
+// dry run karle bhai
 class Solution {
 public:
     long long maxScore(vector<int>& nums1, vector<int>& nums2, int k) {
@@ -8,12 +11,13 @@ public:
             v.push_back({nums2[i], nums1[i]});
         }
         sort(v.rbegin(), v.rend());
+        
         priority_queue<int, vector<int>, greater<>> minHeap;
         for (int i = 0; i < n; i++)
         {
             curr = (curr + v[i].second);
             minHeap.push(v[i].second);
-            if(i >= k - 1){
+            if(i + 1 >= k){
                 res = max(res, curr * v[i].first);
                 curr -= minHeap.top();
                 minHeap.pop();
