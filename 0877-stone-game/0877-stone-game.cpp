12d4bbdb57d66f9956a2ac0de6bc1e70bff3e460
Gourab Singha <110812339+gourabsingha1@users.noreply.github.com>
2023-05-26 11:01:44 +0530

    Time: 5 ms (60.24%), Space: 7.4 MB (97.09%) - LeetHub

--- 0877-stone-game/0877-stone-game.cpp
+++ 0877-stone-game/0877-stone-game.cpp
@@ -1,22 +1,6 @@
 class Solution {
 public:
-    bool helper(int alice, int bob, int i, int n, vector<int>& piles, vector<vector<int>>& dp) {
-        if(i >= n) {
-            return alice > bob;
-        }
-        if(dp[i][n] != -1) {
-            return dp[i][n];
-        }
-        bool res = 0;
-        // beginning
-        res |= helper(alice + piles[i], bob + piles[i + 1], i + 2, n, piles, dp) || helper(alice + piles[i], bob + piles[n], i + 1, n - 1, piles, dp);
-        // ending
-        res |= helper(alice + piles[n], bob + piles[n - 1], i, n - 2, piles, dp) || helper(alice + piles[n], bob + piles[i], i + 1, n - 1, piles, dp);
-        return dp[i][n] = res;
-    }
     bool stoneGame(vector<int>& piles) {
-        int n = piles.size();
-        vector<vector<int>> dp(n, vector<int> (n, -1));
-        return helper(0, 0, 0, n - 1, piles, dp);
+        return 1;
     }
 };
