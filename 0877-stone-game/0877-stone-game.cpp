135916eb7cda55394987f5258599b161eb0aa165
Gourab Singha <110812339+gourabsingha1@users.noreply.github.com>
2023-05-26 12:48:30 +0530

    Time: 54 ms (9.21%), Space: 16.5 MB (21.82%) - LeetHub

--- 0877-stone-game/0877-stone-game.cpp
+++ 0877-stone-game/0877-stone-game.cpp
@@ -1,6 +1,22 @@
+// Correct Solution
 class Solution {
 public:
+    int helper(int i, int n, bool turn, vector<int>& piles, vector<vector<int>>& dp) {
+        if(i >= n) {
+            return 0;
+        }
+        if(dp[i][n] != -1) {
+            return dp[i][n];
+        }
+        int left = piles[i] + helper(i + 1, n, !turn, piles, dp), right = piles[n] + helper(i, n - 1, !turn, piles, dp);
+        if(!turn) {
+            left -= piles[i], right -= piles[n];
+        }
+        return dp[i][n] = max(left, right);
+    }
     bool stoneGame(vector<int>& piles) {
-        return 1;
+        int n = piles.size(), sum = accumulate(piles.begin(), piles.end(), 0);
+        vector<vector<int>> dp(n, vector<int> (n, -1));
+        return helper(0, n - 1, 1, piles, dp) > sum / 2;
     }
 };
