3715b071d785ccaac31417a8bc28256d3f9cc8b1
Gourab Singha <110812339+gourabsingha1@users.noreply.github.com>
2023-06-01 09:59:25 +0530

    Time: 78 ms (57.49%), Space: 19.3 MB (80.62%) - LeetHub

--- 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.cpp
+++ 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.cpp
@@ -1,21 +1,23 @@
 class Solution {
 public:
+    vector<vector<int>> dir = {{0, -1}, {-1, 0}, {0, 1}, {1, 0}, {1, 1}, {-1, 1}, {1, -1}, {-1, -1}};
     int bfs(vector<vector<int>>& grid){
-        if(grid[0][0]) return -1;
+        if(grid[0][0]) {
+            return -1;
+        }
         int n = grid.size(), res = 1;
-        queue<vector<int>> q;
+        queue<pair<int, int>> q;
         q.push({0, 0});
         grid[0][0] = 1;
-        vector<vector<int>> dir = {{0, -1}, {-1, 0}, {0, 1}, {1, 0}, {1, 1}, {-1, 1}, {1, -1}, {-1, -1}};
         while(q.size()){
             int t = q.size();
             while(t--){
-                int x = q.front()[0], y = q.front()[1];
+                auto [x, y] = q.front();
                 q.pop();
                 if(x == n - 1 && y == n - 1){
                     return res;
                 }
-                for (int i = 0; i < dir.size(); i++)
+                for (int i = 0; i < 8; i++)
                 {
                     int dx = x + dir[i][0], dy = y + dir[i][1];
                     if(dx >= 0 && dy >= 0 && dx < n && dy < n && !grid[dx][dy]){
