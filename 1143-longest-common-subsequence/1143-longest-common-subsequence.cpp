db3ce85c409a238ab9021890ca35e78b0cc1b104
Gourab Singha <110812339+gourabsingha1@users.noreply.github.com>
2023-06-04 16:25:27 +0530

    Time: 84 ms (37.56%), Space: 24.6 MB (24.75%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
@@ -1,28 +1,31 @@
+// in lcs, abc = a + bc = c + r
+// if(c1 == c2) ans = lcs(r1, r2) + 1
+// else ans = max(lcs(c1r1, r2), lcs(r1, c2r2))
+
+// memoization
 class Solution {
 public:
-    // in lcs, abc = a + bc
-    // if(c1 == c2) ans = lcs(r1, r2) + 1
-    // else ans = max(lcs(c1r1, r2), lcs(r1, c2r2))
-
-    int longestCommonSubsequence(string text1, string text2) {
-        int res = 0;
-        int n = text1.size(), m = text2.size();
-        int dp[n+1][m+1];
-        for (int i = 0; i < n+1; i++) dp[i][0] = 0;
-        for (int i = 0; i < m+1; i++) dp[0][i] = 0;
-        for (int i = 1; i < n+1; i++)
-        {
-            for (int j = 1; j < m+1; j++)
-            {
-                if(text1[i-1] == text2[j-1]){
-                    dp[i][j] = dp[i-1][j-1] + 1;
-                }
-                else{
-                    dp[i][j] = max(dp[i-1][j], dp[i][j-1]);
-                }
-                res = max(res, dp[i][j]);
-            }
+    int helper(int n, int m, string& text1, string& text2, vector<vector<int>>& dp) {
+        if(n < 0 || m < 0) {
+            return 0;
         }
-        return res;
+        if(dp[n][m] != -1) {
+            return dp[n][m];
+        }
+        int res = 0;
+        if(text1[n] == text2[m]) {
+            res = 1 + helper(n - 1, m - 1, text1, text2, dp);
+        }
+        else {
+            int first = helper(n - 1, m, text1, text2, dp);
+            int second = helper(n, m - 1, text1, text2, dp);
+            res = max(first, second);
+        }
+        return dp[n][m] = res;
+    }
+    int longestCommonSubsequence(string text1, string text2) {
+        int n = text1.size(), m = text2.size();
+        vector<vector<int>> dp(n, vector<int> (m, -1));
+        return helper(n - 1, m - 1, text1, text2, dp);
     }
 };
