e2982903fe5e97f228b0a26b375c9704bc8e7e27
Gourab Singha <110812339+gourabsingha1@users.noreply.github.com>
2023-06-12 13:56:53 +0530

    Time: 235 ms (84.50%), Space: 96.9 MB (69.88%) - LeetHub

---
+++ 0380-insert-delete-getrandom-o1/0380-insert-delete-getrandom-o1.cpp
@@ -0,0 +1,39 @@
+class RandomizedSet {
+public:
+    vector<int> nums;
+    unordered_map<int, int> mp; // num, index (1 based)
+
+    RandomizedSet() {
+        
+    }
+    
+    bool insert(int val) {
+        if(mp[val]) {
+            return 0;
+        }
+
+        nums.push_back(val);
+        mp[val] = nums.size();
+
+        return 1;
+    }
+    
+    bool remove(int val) {
+        if(!mp[val]) {
+            return 0;
+        }
+
+        int last = nums.back();
+        int ind = mp[val] - 1;
+        nums[ind] = last;
+        mp[last] = ind + 1;
+        mp[val] = 0;
+        nums.pop_back();
+
+        return 1;
+    }
+    
+    int getRandom() {
+        return nums[rand() % nums.size()];
+    }
+};
