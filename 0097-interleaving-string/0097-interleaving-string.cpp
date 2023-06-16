a0a00f5010f08a48168d1508d222201a343473fc
Gourab Singha <110812339+gourabsingha1@users.noreply.github.com>
2023-06-16 10:48:04 +0530

    Time: 5 ms (63.44%), Space: 6.3 MB (89.19%) - LeetHub

--- 0097-interleaving-string/0097-interleaving-string.cpp
+++ 0097-interleaving-string/0097-interleaving-string.cpp
@@ -1,30 +1,33 @@
 class Solution {
 public:
-    unordered_map<string, bool> dp;
+    int dp[101][101];
     bool helper(int n, int m, int o, string& s1, string& s2, string& s3) {
         if(o < 0) {
             return n < 0 && m < 0;
         }
-        string memo = to_string(n) + "*" + to_string(m) + "*" + to_string(o);
-        if(dp.find(memo) != dp.end()) {
-            return dp[memo];
+        if(n >= 0 && m >= 0 && dp[n][m] != -1) {
+            return dp[n][m];
         }
 
-        bool res = 0, flag = 0;
+        bool res = 0;
         if(n >= 0 && s1[n] == s3[o]) {
             res |= helper(n - 1, m, o - 1, s1, s2, s3);
-            flag = 1;
         }
         if(m >= 0 && s2[m] == s3[o]) {
             res |= helper(n, m - 1, o - 1, s1, s2, s3);
-            flag = 1;
         }
-        return dp[memo] = res;
+        
+        if(n >= 0 && m >= 0) {
+            dp[n][m] = res;
+        }
+        return res;
     }
 
     bool isInterleave(string s1, string s2, string s3) {
         int n = s1.size(), m = s2.size(), o = s3.size();
         if(n + m != o) return 0;
+
+        memset(dp, -1, sizeof(dp));
         return helper(n - 1, m - 1, o - 1, s1, s2, s3);
     }
 };
