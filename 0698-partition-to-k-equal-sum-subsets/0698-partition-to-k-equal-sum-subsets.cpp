5a8c316689948849bac1906abe2f827189ce554f
Gourab Singha <110812339+gourabsingha1@users.noreply.github.com>
2023-06-20 14:29:28 +0530

    Time: 75 ms (65.40%), Space: 216 MB (5.03%) - LeetHub

--- 0698-partition-to-k-equal-sum-subsets/0698-partition-to-k-equal-sum-subsets.cpp
+++ 0698-partition-to-k-equal-sum-subsets/0698-partition-to-k-equal-sum-subsets.cpp
@@ -2,7 +2,7 @@
 
 class Solution {
 public:
-    bool helper(int curInd, int cur, int& target, int seen, vector<int>& nums, int k) {
+    bool helper(int curInd, int cur, int& target, int seen, vector<int>& nums, int k, vector<vector<int>>& dp) {
         if(k == 1) {
             return 1;
         }
@@ -10,7 +10,10 @@ public:
            return 0;
         }
         if(cur == target) {
-            return helper(0, 0, target, seen, nums, k - 1);
+            return helper(0, 0, target, seen, nums, k - 1, dp);
+        }
+        if(dp[curInd][seen] != -1) {
+            return dp[curInd][seen];
         }
         
         bool res = 0;
@@ -18,16 +21,18 @@ public:
         {
             int bit = 1 << i;
             if(seen & bit || cur + nums[i] > target) continue;
-            res |= helper(i + 1, cur + nums[i], target, seen | bit, nums, k);
-            if(res) return res;
+            res |= helper(i + 1, cur + nums[i], target, seen | bit, nums, k, dp);
+            if(res) break;
         }
-        return res;
+        return dp[curInd][seen] = res;
     }
     bool canPartitionKSubsets(vector<int>& nums, int k) {
         int sum = accumulate(nums.begin(), nums.end(), 0), target = sum / k;
         if(sum % k) {
             return 0;
         }
-        return helper(0, 0, target, 0, nums, k);
+        int n = nums.size();
+        vector<vector<int>> dp(n + 1, vector<int> (1 << n, -1));
+        return helper(0, 0, target, 0, nums, k, dp);
     }
 };
