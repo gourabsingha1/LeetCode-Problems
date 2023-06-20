1232571f6a5317cd2e9a5a793dacf68c66f6581e
Gourab Singha <110812339+gourabsingha1@users.noreply.github.com>
2023-06-20 14:13:54 +0530

    Time: 8 ms (94.93%), Space: 11.4 MB (17.02%) - LeetHub

--- 0526-beautiful-arrangement/0526-beautiful-arrangement.cpp
+++ 0526-beautiful-arrangement/0526-beautiful-arrangement.cpp
@@ -1,10 +1,11 @@
-// bitmask
-
 class Solution {
 public:
-    int helper(int i, int n, int seen){
+    int helper(int i, int n, int seen, vector<vector<int>>& dp){
         if(i > n){
             return 1;
+        }
+        if(dp[i][seen] != -1) {
+            return dp[i][seen];
         }
         
         int res = 0;
@@ -13,13 +14,14 @@ public:
             int bit = 1 << (j - 1);
             if(seen & bit) continue;
             if(i % j == 0 || j % i == 0) {
-                res = res + helper(i + 1, n, seen | bit);
+                res = res + helper(i + 1, n, seen | bit, dp);
             }
         }
-        return res;
+        return dp[i][seen] = res;
     }
 
     int countArrangement(int n) {
-        return helper(1, n, 0);
+        vector<vector<int>> dp(n + 1, vector<int> (1 << n, -1));
+        return helper(1, n, 0, dp);
     }
 };
