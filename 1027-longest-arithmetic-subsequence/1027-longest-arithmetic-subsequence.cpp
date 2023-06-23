56f2834849016b22dc3490fa53285d325a369a07
Gourab Singha <110812339+gourabsingha1@users.noreply.github.com>
2023-06-23 19:42:04 +0530

    Time: 322 ms (86.92%), Space: 141.4 MB (84.52%) - LeetHub

--- 1027-longest-arithmetic-subsequence/1027-longest-arithmetic-subsequence.cpp
+++ 1027-longest-arithmetic-subsequence/1027-longest-arithmetic-subsequence.cpp
@@ -8,12 +8,7 @@ public:
             for (int j = 0; j < i; j++)
             {
                 int diff = nums[i] - nums[j] + 501;
-                if(dp[i][diff] != -1) {
-                    dp[i][diff] = 1 + dp[j][diff];
-                }
-                else {
-                    dp[i][diff] = 2;
-                }
+                dp[i][diff] = 1 + dp[j][diff];
                 res = max(res, dp[i][diff]);
             }
         }
