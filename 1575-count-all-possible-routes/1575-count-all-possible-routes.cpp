73e2add6514aa8a87fc3deb81f236a1d439d1429
Gourab Singha <110812339+gourabsingha1@users.noreply.github.com>
2023-06-25 22:36:38 +0530

    Time: 468 ms (13.82%), Space: 11.8 MB (42.54%) - LeetHub

--- 1575-count-all-possible-routes/1575-count-all-possible-routes.cpp
+++ 1575-count-all-possible-routes/1575-count-all-possible-routes.cpp
@@ -1,26 +1,25 @@
 class Solution {
 public:
     int M = 1e9 + 7;
-    int helper(int i, vector<int>& locations, int& finish, int fuel, vector<vector<int>>& dp) {
-        if(fuel < 0) {
-            return 0;
-        }
-        if(dp[i][fuel] != -1) {
-            return dp[i][fuel];
-        }
-
-        int res = i == finish;
-        for (int j = 0; j < locations.size(); j++)
-        {
-            if(i == j) continue;
-            int x = abs(locations[i] - locations[j]);
-            res = (res + helper(j, locations, finish, fuel - x, dp)) % M;
-        }
-        return dp[i][fuel] = res;
-    }
-
     int countRoutes(vector<int>& locations, int start, int finish, int fuel) {
-        vector<vector<int>> dp(locations.size(), vector<int> (fuel + 1, -1));
-        return helper(start, locations, finish, fuel, dp);
+        int n = locations.size();
+        vector<vector<int>> dp(locations.size(), vector<int> (fuel + 1, 0));
+        for (int f = 0; f <= fuel; f++)
+        {
+            for (int i = 0; i < n; i++)
+            {
+                int res = i == finish;
+                for (int j = 0; j < n; j++)
+                {
+                    if(i == j) continue;
+                    int x = abs(locations[i] - locations[j]);
+                    if(f >= x) {
+                        res = (res + dp[j][f - x]) % M;
+                    }
+                }
+                dp[i][f] = res;
+            }
+        }
+        return dp[start][fuel];
     }
 };
