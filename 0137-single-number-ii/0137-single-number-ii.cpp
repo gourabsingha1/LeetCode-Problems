7da9476ebe06418215ce655f2ce6352469855d7d
Gourab Singha <110812339+gourabsingha1@users.noreply.github.com>
2023-07-04 10:04:07 +0530

    Time: 11 ms (46.18%), Space: 9.5 MB (72.62%) - LeetHub

--- 0137-single-number-ii/0137-single-number-ii.cpp
+++ 0137-single-number-ii/0137-single-number-ii.cpp
@@ -1,23 +1,19 @@
-// Check for every bit. if the sum of all elements' setbits is divisible by 3
-// if not, add that bit to result and slowly form the answer
+// Ratta material
 
 class Solution {
 public:
     int singleNumber(vector<int>& nums) {
-        int res = 0;
-        for (int i = 0; i < 32; i++)
+        int x1 = 0, x2 = 0;
+        for (int i = 0; i < nums.size(); i++)
         {
-            int mask = 1 << i, cnt = 0;
-            for (int j = 0; j < nums.size(); j++)
-            {
-                if(nums[j] & mask) {
-                    cnt++;
-                }
-            }
-            if(cnt % 3) {
-                res |= mask;
-            }
+            x2 ^= (x1 & nums[i]);
+            x1 ^= nums[i];
+            
+            int mask = ~(x1 & x2);
+            
+            x1 &= mask;
+            x2 &= mask;
         }
-        return res;
+        return x1;
     }
 };
