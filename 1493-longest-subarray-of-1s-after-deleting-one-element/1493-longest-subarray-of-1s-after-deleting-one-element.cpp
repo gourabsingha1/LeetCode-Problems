09ac3f6b93efe0bc9624b03baee1b75b31dc4fc5
Gourab Singha <110812339+gourabsingha1@users.noreply.github.com>
2023-07-05 21:45:04 +0530

    Time: 49 ms (40.22%), Space: 39.6 MB (5.07%) - LeetHub

--- 1493-longest-subarray-of-1s-after-deleting-one-element/1493-longest-subarray-of-1s-after-deleting-one-element.cpp
+++ 1493-longest-subarray-of-1s-after-deleting-one-element/1493-longest-subarray-of-1s-after-deleting-one-element.cpp
@@ -1,36 +1,45 @@
+// store count of 1s of all "1's subarrays" on the start and ending position of that subarray
+// then res = max(res, leftSubarray + rightSubarray)
+
 class Solution {
 public:
     int longestSubarray(vector<int>& nums) {
-        int res = 0, pre = 0, suf = 0, zero = 0;
-        for (int i = 0; i < nums.size(); i++)
+        int n = nums.size(), res = 0;
+        bool foundZero = 0; // case: 1, 1, 1
+        vector<pair<int, int>> mp(n); // index = [count of ones, start]
+        for (int i = 0; i < n; i++)
         {
-            if(nums[i] && !zero){
-                pre++;
+            if(nums[i] == 0) {
+                foundZero = 1;
+                continue;
             }
-            else if(pre && !nums[i]){
-                zero++;
+
+            int left = i, cnt = 0;
+            while(i < n && nums[i]) {
+                cnt++, i++;
             }
-            else if(zero == 1){
-                suf++;
+            res = max(res, cnt);
+
+            mp[left] = {cnt, 1};
+            int right = i - 1;
+            if(left == right) {
+                mp[right] = {cnt, -1};
             }
-            else if(pre){
-                res = max(res, pre + suf);
-                if((zero == 2 && nums[i-1] != nums[i-2])){
-                    pre = suf;
-                    zero = 1;
-                    suf = 1;
-                }
-                else{
-                    pre = 1;
-                    zero = 0;
-                    suf = 0;
-                }
+            else {
+                mp[right] = {cnt, 0};
             }
         }
-        res = max(res, pre + suf);
-        if(res == nums.size()){
-            res--;
+
+        if(!foundZero) res--;
+
+        for (int i = 1; i < n - 1; i++)
+        {
+
+            if(mp[i - 1].second != 1 && mp[i + 1].second != 0 ) {
+                res = max(res, mp[i - 1].first + mp[i + 1].first);
+            }
         }
+        cout<<endl;
         return res;
     }
 };
