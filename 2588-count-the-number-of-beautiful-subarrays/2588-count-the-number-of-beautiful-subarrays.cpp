d3628f03394bca5ca3b330963eda6c7639ce9fe2
Gourab Singha <110812339+gourabsingha1@users.noreply.github.com>
2023-07-07 22:30:42 +0530

    Time: 406 ms (40.64%), Space: 120.3 MB (79.14%) - LeetHub

--- 2588-count-the-number-of-beautiful-subarrays/2588-count-the-number-of-beautiful-subarrays.cpp
+++ 2588-count-the-number-of-beautiful-subarrays/2588-count-the-number-of-beautiful-subarrays.cpp
@@ -1,18 +1,18 @@
-// https://www.youtube.com/watch?v=eZr-6p0B7ME
+// Number of Subarrays with xor k, where k = 0
 
 class Solution {
 public:
     long long beautifulSubarrays(vector<int>& nums) {
         long long res = 0, XOR = 0;
         unordered_map<int, int> mp;
+        mp[XOR]++;
         for (int i = 0; i < nums.size(); i++)
         {
             XOR ^= nums[i];
-            res += mp[XOR];
-            if(XOR == 0){
-                res++;
-            }
-            mp[XOR]++;
+            int x = XOR ^ 0; // k = 0
+
+            res += mp[x];
+            mp[x]++;
         }
         return res;
     }
