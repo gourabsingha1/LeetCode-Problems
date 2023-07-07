19e85a7fc62d1d80d502c62677f5a883c9e77485
Gourab Singha <110812339+gourabsingha1@users.noreply.github.com>
2023-07-07 15:29:50 +0530

    Time: 24 ms (26.03%), Space: 7 MB (35.11%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.cpp
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.cpp
@@ -1,16 +1,11 @@
+// Max Consecutive Ones III but reverse
+
 class Solution {
 public:
     int characterReplacement(string s, int k) {
         int res = 0;
-        unordered_map<char, bool> mp;
-        for(auto& ch : s) {
-            mp[ch] = 1;
-        }
-        
         for (char ch = 'A'; ch <= 'Z'; ch++)
         {
-            if(!mp[ch]) continue;
-            
             int cnt = 0;
             for (int i = 0, j = 0; i < s.size(); i++)
             {
