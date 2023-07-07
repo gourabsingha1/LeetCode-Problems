29ee744f7c4256544675b9af65f80e4c88be6816
Gourab Singha <110812339+gourabsingha1@users.noreply.github.com>
2023-07-07 16:43:43 +0530

    Time: 147 ms (61.79%), Space: 57.2 MB (61.13%) - LeetHub

--- 2401-longest-nice-subarray/2401-longest-nice-subarray.cpp
+++ 2401-longest-nice-subarray/2401-longest-nice-subarray.cpp
@@ -4,7 +4,7 @@ public:
         int res = 0, AND = 0;
         for (int i = 0, j = 0; i < nums.size(); i++)
         {
-            while((AND & nums[i]) > 0) {
+            while(AND & nums[i]) {
                 AND ^= nums[j++];
             }
             AND |= nums[i];
