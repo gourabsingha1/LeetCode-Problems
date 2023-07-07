cd1e9a1232bf69d575fe8d384ba49a27500775d5
Gourab Singha <110812339+gourabsingha1@users.noreply.github.com>
2023-07-07 11:04:16 +0530

    Time: 37 ms (89.56%), Space: 36.6 MB (20.50%) - LeetHub

--- 1493-longest-subarray-of-1s-after-deleting-one-element/1493-longest-subarray-of-1s-after-deleting-one-element.cpp
+++ 1493-longest-subarray-of-1s-after-deleting-one-element/1493-longest-subarray-of-1s-after-deleting-one-element.cpp
@@ -1,42 +1,19 @@
-// store count of 1s of all "1's subarrays" on the start and ending position of that subarray
-// then res = max(res, leftSubarray + rightSubarray)
-
 class Solution {
 public:
     int longestSubarray(vector<int>& nums) {
-        int n = nums.size(), res = 0;
-        bool foundZero = 0; // case: 1, 1, 1
-        vector<pair<int, int>> mp(n); // index = [count of ones, start] start = -1 for index 2 of eg: 1, 0, 1, 0 when left and right are same
-        for (int i = 0; i < n; i++)
+        int res = 0, cnt = 0;
+        for (int i = 0, j = 0; i < nums.size(); i++)
         {
-            if(nums[i] == 0) {
-                foundZero = 1;
-                continue;
+            if(!nums[i]) {
+                cnt++;
             }
-
-            int left = i, cnt = 0;
-            while(i < n && nums[i]) {
-                cnt++, i++;
+            while(cnt > 1) {
+                if(!nums[j]) {
+                    cnt--;
+                }
+                j++;
             }
-            res = max(res, cnt);
-
-            mp[left] = {cnt, 1};
-            int right = i - 1;
-            if(left == right) {
-                mp[right] = {cnt, -1};
-            }
-            else {
-                mp[right] = {cnt, 0};
-            }
-        }
-
-        if(!foundZero) res--;
-
-        for (int i = 1; i < n - 1; i++)
-        {
-            if(mp[i - 1].second != 1 && mp[i + 1].second != 0 ) {
-                res = max(res, mp[i - 1].first + mp[i + 1].first);
-            }
+            res = max(res, i - j);
         }
         return res;
     }
