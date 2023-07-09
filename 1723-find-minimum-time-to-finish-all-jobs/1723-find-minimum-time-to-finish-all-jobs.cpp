cc80f3f2c8a8ff3d312f9557a19e129c553a3f44
Gourab Singha <110812339+gourabsingha1@users.noreply.github.com>
2023-07-09 23:23:18 +0530

    Time: 1430 ms (25.00%), Space: 7.5 MB (48.49%) - LeetHub

--- 1723-find-minimum-time-to-finish-all-jobs/1723-find-minimum-time-to-finish-all-jobs.cpp
+++ 1723-find-minimum-time-to-finish-all-jobs/1723-find-minimum-time-to-finish-all-jobs.cpp
@@ -1,15 +1,14 @@
 class Solution {
 public:
-    int res = INT_MAX;
-
     int helper(int n, vector<int>& jobs, int& k, vector<int>& workers){
         if(n < 0){
             return *max_element(workers.begin(), workers.end());
         }
 
+        int res = INT_MAX;
         for (int i = 0; i < k; i++)
         {
-            if(i > 0 && workers[i - 1] <= workers[i] || workers[i] + jobs[n] >= res) continue;
+            if(i > 0 && workers[i - 1] <= workers[i]) continue;
             workers[i] += jobs[n];
             res = min(res, helper(n - 1, jobs, k, workers));
             workers[i] -= jobs[n];
