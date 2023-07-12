e766fcad5146a31d1a4a68d8d44993bbd5042079
Gourab Singha <110812339+gourabsingha1@users.noreply.github.com>
2023-07-12 19:10:27 +0530

    Time: 243 ms (17.38%), Space: 61.6 MB (15.81%) - LeetHub

--- 0802-find-eventual-safe-states/0802-find-eventual-safe-states.cpp
+++ 0802-find-eventual-safe-states/0802-find-eventual-safe-states.cpp
@@ -1,43 +1,53 @@
-class Solution {
+// Reverse the graph, then return the topological sort in ascending order
+
+// **** Topological Sort (BFS) | Kahn's Algorithm - O(n + e), O(n) ****
+class TopologicalSortBFS{
 public:
     vector<int> topoSort(int n, vector<vector<int>> &adj){
         vector<int> res, indegree(n, 0); // indegree = no. of arrows directed towards
         queue<int> q;
-        for (int i = 0; i < n; i++)
+        for (int u = 0; u < n; u++)
         {
-            for(auto &it : adj[i]){
-                indegree[i]++;
+            for(auto &v : adj[u]){
+                indegree[v]++;
             }
         }
-        for (int i = 0; i < n; i++)
+        for (int u = 0; u < n; u++)
         {
-            if(indegree[i] == 0){
-                q.push(i);
-            }
-        }
-        vector<vector<int>> graph(n);
-        for (int i = 0; i < n; i++)
-        {
-            for(auto &it : adj[i]){
-                graph[it].push_back(i);
+            if(indegree[u] == 0){
+                q.push(u);
             }
         }
         while(q.size()){
-            int i = q.front();
+            int u = q.front();
             q.pop();
-            res.push_back(i);
-            int size = res.size();
-            for(auto &it : graph[i]){
-                indegree[it]--;
-                if(indegree[it] == 0){
-                    q.push(it);
+            res.push_back(u);
+            for(auto &v : adj[u]){
+                indegree[v]--;
+                if(indegree[v] == 0){
+                    q.push(v);
                 }
             }
         }
-        sort(res.begin(), res.end());
         return res;
     }
+};
+
+class Solution {
+public:
     vector<int> eventualSafeNodes(vector<vector<int>>& graph) {
-        return topoSort(graph.size(), graph);
+        int n = graph.size();
+        vector<vector<int>> newGraph(n);
+        for (int i = 0; i < n; i++)
+        {
+            for(auto &it : graph[i]){
+                newGraph[it].push_back(i);
+            }
+        }
+
+        TopologicalSortBFS TS;
+        vector<int> res = TS.topoSort(n, newGraph);
+        sort(res.begin(), res.end());
+        return res;
     }
 };
