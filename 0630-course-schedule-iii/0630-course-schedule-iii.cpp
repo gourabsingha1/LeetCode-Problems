078a2ab277fa76728f9acd5ae7066e66b3fe924c
Gourab Singha <110812339+gourabsingha1@users.noreply.github.com>
2023-07-14 01:27:59 +0530

    Time: 281 ms (65.38%), Space: 56.1 MB (79.44%) - LeetHub

---
+++ 0630-course-schedule-iii/0630-course-schedule-iii.cpp
@@ -0,0 +1,28 @@
+// Since we'll be progressing through courses as if we're progressing through time,
+// we'll want to sort C based on the courses' cutoffs (end)
+// but when we backtrack to potentially remove a course
+// we'll want to sort the courses we've accepted by their duration (dur).
+
+class Solution {
+public:
+    static bool cmp(vector<int>& a, vector<int>& b) {
+        return a[1] < b[1];
+    }
+
+    int scheduleCourse(vector<vector<int>>& courses) {
+        int cur = 0, n = courses.size();
+        sort(courses.begin(), courses.end(), cmp);
+        priority_queue<int> pq;
+        for (int i = 0, j = 0; i < n; i++)
+        {
+            int duration = courses[i][0], time = courses[i][1];
+            cur += duration;
+            pq.push(duration);
+            if(cur > time) {
+                cur -= pq.top();
+                pq.pop();
+            }
+        }
+        return pq.size();
+    }
+};
