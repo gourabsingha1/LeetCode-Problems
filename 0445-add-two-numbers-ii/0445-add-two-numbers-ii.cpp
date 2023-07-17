5bad375a62cefff8b2e88817a910dcc5f81b17cb
Gourab Singha <110812339+gourabsingha1@users.noreply.github.com>
2023-07-17 11:03:22 +0530

    Time: 37 ms (68.40%), Space: 71.1 MB (74.64%) - LeetHub

---
+++ 0445-add-two-numbers-ii/0445-add-two-numbers-ii.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    ListNode* reverse(ListNode* head) {
+        ListNode* prev = NULL, *cur = head;
+        while(cur) {
+            ListNode* next = cur->next;
+            cur->next = prev;
+            prev = cur;
+            cur = next;
+        }
+        return prev;
+    }
+
+    ListNode* addTwoNumbersI(ListNode* l1, ListNode* l2) {
+        ListNode* dummy = new ListNode(0), *res = dummy;
+        int carry = 0;
+        while(l1 || l2) {
+            int val1 = 0, val2 = 0;
+            if(l1) val1 = l1->val;
+            if(l2) val2 = l2->val;
+            int sum = val1 + val2 + carry;
+            carry = sum / 10;
+            ListNode* newNode = new ListNode(sum % 10);
+            res->next = newNode;
+            res = res->next;
+            if(l1) l1 = l1->next;
+            if(l2) l2 = l2->next;
+        }
+        if(carry) res->next = new ListNode(1);
+        return reverse(dummy->next);
+    }
+    
+    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
+        l1 = reverse(l1), l2 = reverse(l2);
+        return addTwoNumbersI(l1, l2);
+    }
+};
