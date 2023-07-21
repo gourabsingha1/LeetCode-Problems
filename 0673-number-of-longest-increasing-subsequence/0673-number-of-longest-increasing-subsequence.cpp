3f323b169705a671ab5491b778658333b79e2f25
Gourab Singha <110812339+gourabsingha1@users.noreply.github.com>
2023-07-21 12:02:08 +0530

    Time: 252 ms (20.35%), Space: 13.1 MB (66.65%) - LeetHub

--- 0673-number-of-longest-increasing-subsequence/0673-number-of-longest-increasing-subsequence.cpp
+++ 0673-number-of-longest-increasing-subsequence/0673-number-of-longest-increasing-subsequence.cpp
@@ -1,35 +1,30 @@
 class Solution {
 public:
-    int findNumberOfLIS(vector<int> &nums) {
-        const int n = nums.size();
-        vector<int> lis(n, 1);
-        vector<int> count(n, 1);
-        int maxLen = 1;
-        for (int i = 1; i < n; i++)
+    int findNumberOfLIS(vector<int>& nums) {
+        int n = nums.size(), res = 0, lengthOfLIS = 1;
+        vector<int> dp(n, 1), cnt(n, 1);
+        for (int i = 0; i < n; i++)
         {
             for (int j = 0; j < i; j++)
             {
-                if (nums[i] > nums[j])
-                {
-                    if (lis[j] + 1 > lis[i])
-                    {
-                        lis[i] = lis[j] + 1;
-                        count[i] = count[j];
+                if(nums[j] < nums[i]){
+                    if(dp[i] == 1 + dp[j]) {
+                        cnt[i] += cnt[j];
                     }
-                    else if (lis[j] + 1 == lis[i])
-                    {
-                        count[i] += count[j];
+                    else if(dp[i] < 1 + dp[j]) {
+                        cnt[i] = cnt[j];
                     }
+                    dp[i] = max(dp[i], 1 + dp[j]);
                 }
             }
-            maxLen = max(maxLen, lis[i]);
+            lengthOfLIS = max(lengthOfLIS, dp[i]);
         }
-        int numOfLIS = 0;
         for (int i = 0; i < n; i++)
         {
-            if (lis[i] == maxLen)
-                numOfLIS += count[i];
+            if(dp[i] == lengthOfLIS) {
+                res += cnt[i];
+            }
         }
-        return numOfLIS;
+        return res;
     }
 };
