d8cb915c9a939a77c5da780cf24c0f1454871759
Gourab Singha <110812339+gourabsingha1@users.noreply.github.com>
2023-07-21 11:18:00 +0530

    Time: 445 ms (52.77%), Space: 10.4 MB (66.98%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -1,27 +1,17 @@
 class Solution {
 public:
-    int helper(int j, int i, vector<int>& nums, vector<vector<int>>& dp) {
-        if(j == nums.size()) {
-            return 1;
-        }
-        if(dp[i][j] != -1) {
-            return dp[i][j];
-        }
-
-        int take = 0;
-        if(nums[i] < nums[j]) {
-            take = 1 + helper(j + 1, j, nums, dp);
-        }
-        int notTake = helper(j + 1, i, nums, dp);
-        return dp[i][j] = max(take, notTake);
-    }
-
     int lengthOfLIS(vector<int>& nums) {
-        int res = 0, n = nums.size();
-        vector<vector<int>> dp(n, vector<int> (n, -1));
-        for (int i = 0; i < n; i++)
+        int n = nums.size(), res = 0;
+        vector<int> dp(n, 1);
+        for (int i = n - 1; i >= 0; i--)
         {
-            res = max(res, helper(i + 1, i, nums, dp));
+            for (int j = i + 1; j < n; j++)
+            {
+                if(nums[i] < nums[j]){
+                    dp[i] = max(dp[i], 1 + dp[j]);
+                }
+            }
+            res = max(res, dp[i]);
         }
         return res;
     }
