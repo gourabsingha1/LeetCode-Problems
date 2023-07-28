3caeebc770ebf1a91f9580795930ec9755a5c7e4
Gourab Singha <110812339+gourabsingha1@users.noreply.github.com>
2023-07-28 11:12:03 +0530

    Time: 7 ms (43.71%), Space: 8.5 MB (10.30%) - LeetHub

--- 0486-predict-the-winner/0486-predict-the-winner.cpp
+++ 0486-predict-the-winner/0486-predict-the-winner.cpp
@@ -1,25 +1,32 @@
 class Solution {
 public:
-    int helper(int i, int j, bool player1, vector<int>& nums) {
+    int helper(int i, int j, bool player1, vector<int>& nums, vector<vector<vector<int>>>& dp) {
         if(i > j) {
             return 0;
         }
+        if(dp[i][j][player1] != -1) {
+            return dp[i][j][player1];
+        }
 
         int res = 0;
+        // player1 wants to maximize its score
         if(player1) {
-            int takeLeft = nums[i] + helper(i + 1, j, !player1, nums);
-            int takeRight = nums[j] + helper(i, j - 1, !player1, nums);
+            int takeLeft = nums[i] + helper(i + 1, j, !player1, nums, dp);
+            int takeRight = nums[j] + helper(i, j - 1, !player1, nums, dp);
             res = max(takeLeft, takeRight);
         }
+        // player2 wants to minimize player1's score
         else {
-            int takeLeft = -nums[i] + helper(i + 1, j, !player1, nums);
-            int takeRight = -nums[j] + helper(i, j - 1, !player1, nums);
+            int takeLeft = -nums[i] + helper(i + 1, j, !player1, nums, dp);
+            int takeRight = -nums[j] + helper(i, j - 1, !player1, nums, dp);
             res = min(takeLeft, takeRight);
         }
-        return res;
+        return dp[i][j][player1] = res;
     }
     
     bool PredictTheWinner(vector<int>& nums) {
-        return helper(0, nums.size() - 1, 1, nums) >= 0;
+        int n = nums.size();
+        vector<vector<vector<int>>> dp(n, vector<vector<int>> (n, vector<int> (2, -1)));
+        return helper(0, n - 1, 1, nums, dp) >= 0;
     }
 };
