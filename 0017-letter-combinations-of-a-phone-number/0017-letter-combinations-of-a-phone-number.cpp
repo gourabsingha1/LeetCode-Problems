a2b6d219c3f81bd16147a43341d446f1ba6f52f2
Gourab Singha <110812339+gourabsingha1@users.noreply.github.com>
2023-08-03 10:36:38 +0530

    Time: 3 ms (37.56%), Space: 6.3 MB (97.44%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.cpp
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.cpp
@@ -1,29 +1,29 @@
 class Solution {
 public:
     vector<string> keypad = {"abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
+    
+    void helper(string& temp, int i, string& digits, vector<string>& res) {
+        if(i == digits.size()){
+            res.push_back(temp);
+            return;
+        }
+        
+        for(auto& ch : keypad[digits[i] - '2'])
+        {
+            temp.push_back(ch);
+            helper(temp, i + 1, digits, res);
+            temp.pop_back();
+        }
+    }
+    
     vector<string> letterCombinations(string digits) {
-        if(digits == ""){
+        if(!digits.size()) {
             return {};
         }
-        char ch = digits[0];
-        string rest = digits.substr(1);
-        vector<string> temp = {""};
-        if(rest.size()){
-            temp = letterCombinations(rest);
-        }
+        
         vector<string> res;
-        for(string &s : temp){
-            for (int i = 0; i < keypad[ch - '2'].size(); i++)
-            {
-                char c = keypad[ch - '2'][i];
-                res.push_back(c + s);
-            }
-        }
+        string temp;
+        helper(temp, 0, digits, res);
         return res;
     }
 };
-// 456
-// 4 + 56
-// 4 = "ghi" // ch
-// 56 = ["jm, jn, jo, km, kn,.."] // rest
-// res = ["gjm", "gjn", "gjo",..]
