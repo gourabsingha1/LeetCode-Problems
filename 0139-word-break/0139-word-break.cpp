23abc7e1ba8d9d69f81dc4b3700e0be0570124f6
Gourab Singha <110812339+gourabsingha1@users.noreply.github.com>
2023-08-04 09:36:50 +0530

    Time: 20 ms (39.61%), Space: 13.8 MB (30.85%) - LeetHub

--- 0139-word-break/0139-word-break.cpp
+++ 0139-word-break/0139-word-break.cpp
@@ -1,24 +1,28 @@
 class Solution {
 public:
-    // tabulation
+    bool helper(int n, string& s, unordered_map<string, bool>& dict, vector<int>& dp) {
+        if(n < 0) {
+            return 1;
+        }
+        if(dp[n] != -1) {
+            return dp[n];
+        }
+        
+        bool res = 0;
+        for (int i = n; i >= 0; i--)
+        {
+            if(dict[s.substr(i, n - i + 1)]) {
+                res |= helper(i - 1, s, dict, dp);
+            }
+        }
+        return dp[n] = res;
+    }
     bool wordBreak(string s, vector<string>& wordDict) {
-        int n = s.size();
         unordered_map<string, bool> dict;
-        vector<int> dp(n + 1, 0);
+        vector<int> dp(s.size(), -1);
         for(auto& s : wordDict) {
             dict[s] = 1;
         }
-        dp[0] = 1;
-        for (int i = 1; i <= n; i++)
-        {
-            for (int j = i - 1; j >= 0; j--)
-            {
-                if(dp[j] && dict[s.substr(j, i - j)]) {
-                    dp[i] = 1;
-                    break;
-                }
-            }
-        }
-        return dp[n];
+        return helper(s.size() - 1, s, dict, dp);
     }
 };
