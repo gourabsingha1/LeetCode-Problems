50b953411a51a99c90bb8d48a3dd51c2d24037a4
Gourab Singha <110812339+gourabsingha1@users.noreply.github.com>
2023-08-05 16:00:58 +0530

    Time: 13 ms (87.58%), Space: 16.6 MB (11.56%) - LeetHub

---
+++ 0095-unique-binary-search-trees-ii/0095-unique-binary-search-trees-ii.cpp
@@ -0,0 +1,29 @@
+// first loop: select the root node
+// second loop: recursively select the left tree
+// third loop: recursively select the right tree
+// add the root to res
+
+class Solution {
+public:
+    vector<TreeNode*> helper(int left, int right) {
+        if(left > right) {
+            return {NULL};
+        }
+
+        vector<TreeNode*> res;
+        for (int val = left; val <= right; val++)
+        {
+            for(auto& leftTree : helper(left, val - 1)) {
+                for(auto& rightTree : helper(val + 1, right)) {
+                    TreeNode* root = new TreeNode(val, leftTree, rightTree);
+                    res.push_back(root);
+                }
+            }
+        }
+        return res;
+    }
+
+    vector<TreeNode*> generateTrees(int n) {
+        return helper(1, n);
+    }
+};
