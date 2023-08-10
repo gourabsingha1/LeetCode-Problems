8a5dffeaec5543f4e3c255a19c6e07011d396f05
Gourab Singha <110812339+gourabsingha1@users.noreply.github.com>
2023-08-11 01:52:42 +0530

    Time: 2 ms (72.72%), Space: 11.1 MB (18.26%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
@@ -1,46 +1,29 @@
 class Solution {
 public:
-    // if nums is increasing, then k = nums.size()
-    int findK(vector<int>& nums) {
+    int search(vector<int>& nums, int target) {
         int low = 0, high = nums.size() - 1;
         while(low <= high) {
             int mid = (low + high) / 2;
-            if(nums[mid] >= nums[0]) {
-                low = mid + 1;
+            if(nums[mid] == target) {
+                return mid;
             }
-            else {
-                high = mid - 1;
+            else if(nums[low] <= nums[mid]) { // first half in order
+                if(nums[low] <= target && target <= nums[mid]) {
+                    high = mid - 1;
+                }
+                else {
+                    low = mid + 1;
+                }
+            }
+            else { // second half in order
+                if(nums[mid] <= target && target <= nums[high]) {
+                    low = mid + 1;
+                }
+                else {
+                    high = mid - 1;
+                }
             }
         }
-        return low;
-    }
-
-    int findTarget(int low, int high, vector<int>& nums, int target) {
-        while(low <= high) {
-            int mid = (low + high) / 2;
-            if(nums[mid] < target) {
-                low = mid + 1;
-            }
-            else {
-                high = mid - 1;
-            }
-        }
-        return low;
-    }
-
-    int search(vector<int>& nums, int target) {
-        int k = findK(nums), n = nums.size();
-        int index = 0;
-        if(nums[0] > target) {
-            index = findTarget(k, n - 1, nums, target);
-        }
-        else {
-            index = findTarget(0, k - 1, nums, target);
-        }
-        
-        if(index == n || nums[index] != target) {
-            return -1;
-        }
-        return index; 
+        return -1;
     }
 };
