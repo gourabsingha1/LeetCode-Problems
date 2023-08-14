b4491f30675453516e3aa566e8f9679671daf8d6
Gourab Singha <110812339+gourabsingha1@users.noreply.github.com>
2023-08-15 03:22:17 +0530

    Time: 227 ms (67.86%), Space: 15.4 MB (76.32%) - LeetHub

--- 0718-maximum-length-of-repeated-subarray/0718-maximum-length-of-repeated-subarray.cpp
+++ 0718-maximum-length-of-repeated-subarray/0718-maximum-length-of-repeated-subarray.cpp
@@ -1,26 +1,29 @@
 class Solution {
 public:
-    int findLength(vector<int>& nums1, vector<int>& nums2)
-    {
-        int n = nums1.size(), m = nums2.size();
-        int dp[n+1][m+1], res = 0;
-        for (int i = 0; i < n; i++) dp[i][0] = 0;
-        for (int i = 0; i < m; i++) dp[0][i] = 0;
-
-        for (int i = 1; i < n+1; i++)
-        {
-            for (int j = 1; j < m+1; j++)
-            {
-                int c1 = nums1[i-1], c2 = nums2[j-1];
-                if(c1 != c2){
-                    dp[i][j] = 0;
-                }
-                else{
-                    dp[i][j] = dp[i-1][j-1] + 1;
-                    res = max(res, dp[i][j]);
-                }
-            }
+    int dp[1000][1000], res = 0;
+    
+    int helper(int i, int j, vector<int>& nums1, vector<int>& nums2) {
+        if(i < 0 || j < 0) {
+            return 0;
         }
+        if(dp[i][j] != -1) {
+            return dp[i][j];
+        }
+
+        int take = 0;
+        if(nums1[i] == nums2[j]) {
+            take = 1 + helper(i - 1, j - 1, nums1, nums2);
+        }
+        int notTake1 = helper(i - 1, j, nums1, nums2);
+        int notTake2 = helper(i, j - 1, nums1, nums2);
+        res = max(res, take);
+        return dp[i][j] = take;
+    }
+    
+    int findLength(vector<int>& nums1, vector<int>& nums2) {
+        int n = nums1.size(), m = nums2.size();
+        memset(dp, -1, sizeof(dp));
+        helper(n - 1, m - 1, nums1, nums2);
         return res;
     }
 };
