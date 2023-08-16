5811e694517919d8d4b57ac471f4a6d1da48b698
Gourab Singha <110812339+gourabsingha1@users.noreply.github.com>
2023-08-16 08:55:36 +0530

    Time: 185 ms (98.09%), Space: 134.6 MB (89.46%) - LeetHub

--- 0239-sliding-window-maximum/0239-sliding-window-maximum.cpp
+++ 0239-sliding-window-maximum/0239-sliding-window-maximum.cpp
@@ -1,3 +1,5 @@
+// Use deque to store at most k elements in decreasing order
+
 class Solution {
 public:
     vector<int> maxSlidingWindow(vector<int>& nums, int k) {
@@ -11,7 +13,7 @@ public:
             dq.push_back(i);
             if(i + 1 >= k){
                 res.push_back(nums[dq.front()]);
-                if(i - dq.front() + 1 == k){
+                if(i + 1 - dq.front() == k){
                     dq.pop_front();
                 }
             }
