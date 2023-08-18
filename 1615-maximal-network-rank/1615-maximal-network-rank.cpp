91befbaac03d55d7f8624801b02d8e9ddf8342be
Gourab Singha <110812339+gourabsingha1@users.noreply.github.com>
2023-08-18 20:34:55 +0530

    Time: 53 ms (97.68%), Space: 30.5 MB (83.52%) - LeetHub

--- 1615-maximal-network-rank/1615-maximal-network-rank.cpp
+++ 1615-maximal-network-rank/1615-maximal-network-rank.cpp
@@ -2,25 +2,25 @@ class Solution {
 public:
     int maximalNetworkRank(int n, vector<vector<int>>& roads) {
         int res = 0;
-        vector<vector<int>> adj(n);
+        vector<int> adj[n];
         for (int i = 0; i < roads.size(); i++)
         {
             int u = roads[i][0], v = roads[i][1];
             adj[u].push_back(v);
             adj[v].push_back(u);
         }
-        for (int i = 0; i < n; i++)
+        for (int u = 0; u < n; u++)
         {
-            for (int j = i + 1; j < n; j++)
+            for (int v = u + 1; v < n; v++)
             {
-                int cnt = adj[i].size() + adj[j].size();
-                for(auto &v : adj[j]){
-                    if(v == i){
-                        cnt--;
+                int cur = adj[u].size() + adj[v].size();
+                for(auto& vNode : adj[v]){
+                    if(vNode == u){
+                        cur--;
                         break;
                     }
                 }
-                res = max(res, cnt);
+                res = max(res, cur);
             }
         }
         return res;
