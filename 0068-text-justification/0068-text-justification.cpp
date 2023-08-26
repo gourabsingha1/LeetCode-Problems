0648f005f34ac12ec262e56e57904c9cdb079dad
Gourab Singha <110812339+gourabsingha1@users.noreply.github.com>
2023-08-26 19:32:51 +0530

    Time: 0 ms (100.00%), Space: 7.7 MB (7.59%) - LeetHub

--- 0068-text-justification/0068-text-justification.cpp
+++ 0068-text-justification/0068-text-justification.cpp
@@ -1,18 +1,24 @@
+// keep a window of length <= maxWidth
+// if there are 17 spaces and we have 5 places, then the space distribution is 4 + 4 + 3 + 3 + 3
+
 class Solution {
 public:
     vector<string> fullJustify(vector<string>& words, int maxWidth) {
         vector<string> res;
         int i = 0, j = 0, n = words.size();
         while(i < n) {
+            // make the window
             int cntChar = 0, cntSpace = 0;
             j = i;
             while(i < n && cntChar + cntSpace + words[i].size() <= maxWidth) {
                 cntChar += words[i++].size(), cntSpace++;
             }
+            // last window
             if(i == n) {
                 break;
             }
 
+            // make space array
             cntSpace--;
             int x = maxWidth - cntChar, rem = 0;
             if(cntSpace) {
@@ -27,6 +33,7 @@ public:
                 spaces[rem] += " ";
             }
 
+            // push the window
             string cur;
             int ind = 0;
             while(j < i && cur.size() < maxWidth) {
@@ -35,12 +42,14 @@ public:
                     cur += spaces[ind++];
                 }
             }
+            // when theres only one word in a window, push space at end
             if(cntSpace == 0) {
                 cur += space;
             }
             res.push_back(cur);
         }
 
+        // last window
         string last;
         while(j < n) {
             last += words[j++] + " ";
