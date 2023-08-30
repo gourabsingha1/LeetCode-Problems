fe23b58020c51588068e99a2b6c151546553630f
Gourab Singha <110812339+gourabsingha1@users.noreply.github.com>
2023-08-30 15:29:22 +0530

    Time: 107 ms (53.15%), Space: 54.6 MB (42.04%) - LeetHub

--- 2366-minimum-replacements-to-sort-the-array/2366-minimum-replacements-to-sort-the-array.cpp
+++ 2366-minimum-replacements-to-sort-the-array/2366-minimum-replacements-to-sort-the-array.cpp
@@ -1,3 +1,11 @@
+// 3, 8, 3
+// nums[i] = 8, mi = 3
+// break 8 into 3 parts | (nums[i] + mi - 1) / mi = 3
+// add (8 - 1) / 3 = 2 to res | (nums[i] - 1) / mi = 2
+// low = 1, high = mi
+// for every mid, check if mid + mid + mid ... >= target (nums[i]) | mid * times >= target
+// i.e the lower_bound will be the current minimum (mi)
+
 class Solution {
 public:
     int lowerbound(int high, int times, int target) {
@@ -24,7 +32,7 @@ public:
         {
             res += (nums[i] - 1) / mi;
             int times = (nums[i] + mi - 1) / mi;
-            mi = max(1, lowerbound(mi, times, nums[i]));
+            mi = lowerbound(mi, times, nums[i]);
         }
         return res;
     }
