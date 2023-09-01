6533d53f47b73237677389fd92d6d321138cb12c
Gourab Singha <110812339+gourabsingha1@users.noreply.github.com>
2023-09-01 17:33:12 +0530

    Time: 8 ms (56.95%), Space: 8.1 MB (74.10%) - LeetHub

--- 0419-battleships-in-a-board/0419-battleships-in-a-board.cpp
+++ 0419-battleships-in-a-board/0419-battleships-in-a-board.cpp
@@ -1,28 +1,16 @@
+// iterating from top left to bottom right and checking only
+// up and left cell ensures weather it is a part of current battleship or not
+
 class Solution {
 public:
-    vector<int> dir = {1, 0, -1, 0, 1}; // right, down, left, up
-
-    void dfs(int x, int y, vector<vector<char>>& board){
-        board[x][y] = '.';
-        for (int i = 0; i < 4; i++)
-        {
-            int dx = x + dir[i], dy = y + dir[i + 1];
-            if(dx >= 0 && dy >= 0 && dx < board.size() && dy < board[0].size() && board[dx][dy] == 'X'){
-                dfs(dx, dy, board);
-            }
-        }
-    }
-
     int countBattleships(vector<vector<char>>& board) {
         int res = 0;
         for (int i = 0; i < board.size(); i++)
         {
             for (int j = 0; j < board[0].size(); j++)
             {
-                if(board[i][j] == 'X'){
-                    dfs(i, j, board);
-                    res++;
-                }
+                if(board[i][j] == '.' || i > 0 && board[i - 1][j] == 'X' || j > 0 && board[i][j - 1] == 'X') continue;
+                res++;
             }
         }
         return res;
