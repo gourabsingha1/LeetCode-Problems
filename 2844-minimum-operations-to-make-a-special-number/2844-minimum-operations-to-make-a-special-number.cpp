e19a97549edfca791257f43c50dfd1de009aa1eb
Gourab Singha <110812339+gourabsingha1@users.noreply.github.com>
2023-09-06 02:52:43 +0530

    Time: 4 ms (41.71%), Space: 6.3 MB (28.94%) - LeetHub

--- 2844-minimum-operations-to-make-a-special-number/2844-minimum-operations-to-make-a-special-number.cpp
+++ 2844-minimum-operations-to-make-a-special-number/2844-minimum-operations-to-make-a-special-number.cpp
@@ -1,25 +1,25 @@
 class Solution {
 public:
-    unordered_map<string, int> dp;
+    int dp[25][100];
 
     int helper(int rem, int i, string& num) {
         if(i == num.size()) {
             if(rem == 0) {
                 return 0;
             }
-            return -1e3;
+            return 1e3;
         }
-        string memo = to_string(rem) + "*" + to_string(i);
-        if(dp.find(memo) != dp.end()) {
-            return dp[memo];
+        if(dp[rem][i] != -1) {
+            return dp[rem][i];
         }
 
-        int take = 1 + helper((rem * 10 + num[i] - '0') % 25, i + 1, num);
-        int notTake = helper(rem, i + 1, num);
-        return dp[memo] = max(take, notTake);
+        int take = helper((rem * 10 + num[i] - '0') % 25, i + 1, num);
+        int notTake = 1 + helper(rem, i + 1, num);
+        return dp[rem][i] = min(take, notTake);
     }
 
     int minimumOperations(string num) {
-        return num.size() - helper(0, 0, num);
+        memset(dp, -1, sizeof(dp));
+        return helper(0, 0, num);
     }
 };
