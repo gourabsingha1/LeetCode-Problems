dd4ba5a07782937f4418dcf7a5d5262030da377c
Gourab Singha <110812339+gourabsingha1@users.noreply.github.com>
2023-09-05 15:02:54 +0530

    Added solution - LeetHub

--- Maximum Index - GFG/maximum-index.cpp
+++ Maximum Index - GFG/maximum-index.cpp
@@ -6,19 +6,27 @@ using namespace std;
 // } Driver Code Ends
 class Solution{
     public:
-    int maxIndexDiff(int A[], int N) 
+    int maxIndexDiff(int a[], int n) 
     { 
-        vector<pair<int, int>> numsInd;
-        for (int i = 0; i < N; i++)
+        vector<int> pre(n), suf(n);
+        pre[0] = a[0], suf[n - 1] = a[n - 1];
+        for (int i = 1; i < n; i++)
         {
-            numsInd.push_back({A[i], i});
+            pre[i] = min(a[i], pre[i - 1]);
         }
-        sort(numsInd.begin(), numsInd.end());
-        int res = 0, mn = 1e9;
-        for (int i = 0; i < N; i++)
+        for (int i = n - 2; i >= 0; i--)
         {
-            mn = min(mn, numsInd[i].second);
-            res = max(res, numsInd[i].second - mn);
+            suf[i] = max(a[i], suf[i + 1]);
+        }
+        int res = 0, i = 0, j = 0;
+        while(i < n && j < n) {
+            if(pre[i] <= suf[j]) {
+                res = max(res, j - i);
+                j++;
+            }
+            else {
+                i++;
+            }
         }
         return res;
     }
