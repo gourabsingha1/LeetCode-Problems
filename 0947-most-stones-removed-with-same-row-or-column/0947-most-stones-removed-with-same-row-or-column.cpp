3ee1db0586adda31b018694105426b876d93cdee
Gourab Singha <110812339+gourabsingha1@users.noreply.github.com>
2023-09-09 14:39:28 +0530

    Time: 223 ms (22.09%), Space: 14 MB (95.51%) - LeetHub

--- 0947-most-stones-removed-with-same-row-or-column/0947-most-stones-removed-with-same-row-or-column.cpp
+++ 0947-most-stones-removed-with-same-row-or-column/0947-most-stones-removed-with-same-row-or-column.cpp
@@ -1,60 +1,33 @@
 // we can only remove stones if they are connected
-// stones lying on same row forms a graph. similarly for columms
+// stones lying on same row are connected. similarly for columms
 // treat indices of stones as nodes of a graph
-// build the graph then return n - number of island
+// return n - number of island
 // because last node of each island cannot be removed
 
 class Solution {
 public:
     vector<int> dir = {1, 0, -1, 0, 1}; // right, down, left, up
 
-    void dfs(vector<bool>& vis, int u, vector<int> adj[]){
-        vis[u] = 1;
-        for(auto& v : adj[u]) {
-            if(vis[v]) continue;
-            dfs(vis, v, adj);
-        }
-    }
-
-    int numIslands(int n, vector<int> adj[]) {
-        int res = 0;
-        vector<bool> vis(n, 0);
-        for (int i = 0; i < n; i++)
+    void dfs(vector<bool>& vis, int ind, vector<vector<int>>& stones){
+        vis[ind] = 1;
+        for (int i = 0; i < stones.size(); i++)
         {
-            if(!vis[i]){
-                dfs(vis, i, adj);
-                res++;
+            if(!vis[i] && (stones[ind][0] == stones[i][0] || stones[ind][1] == stones[i][1])) {
+                dfs(vis, i, stones);
             }
         }
-        return res;
     }
 
     int removeStones(vector<vector<int>>& stones) {
-        int res = 0, n = stones.size();
-        unordered_map<int, vector<int>> rows, cols;
-        vector<int> adj[n];
+        int res = 0, n = stones.size(), cntIslands = 0;
+        vector<bool> vis(n, 0);
         for (int i = 0; i < n; i++)
         {
-            int x = stones[i][0], y = stones[i][1];
-            rows[x].push_back(i);
-            cols[y].push_back(i);
-        }
-        for(auto& [x, nodes] : rows) {
-            for (int i = 1; i < nodes.size(); i++)
-            {
-                int u = nodes[i - 1], v = nodes[i];
-                adj[u].push_back(v);
-                adj[v].push_back(u);
+            if(!vis[i]) {
+                dfs(vis, i, stones);
+                cntIslands++;
             }
         }
-        for(auto& [x, nodes] : cols) {
-            for (int i = 1; i < nodes.size(); i++)
-            {
-                int u = nodes[i - 1], v = nodes[i];
-                adj[u].push_back(v);
-                adj[v].push_back(u);
-            }
-        }
-        return n - numIslands(n, adj);
+        return n - cntIslands;
     }
 };
