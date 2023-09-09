432cd751166396814d708136df24bea82ccfe3ca
Gourab Singha <110812339+gourabsingha1@users.noreply.github.com>
2023-09-09 15:29:17 +0530

    Time: 14 ms (75.24%), Space: 11.5 MB (35.47%) - LeetHub

--- 0589-n-ary-tree-preorder-traversal/0589-n-ary-tree-preorder-traversal.cpp
+++ 0589-n-ary-tree-preorder-traversal/0589-n-ary-tree-preorder-traversal.cpp
@@ -1,21 +1,20 @@
 class Solution {
 public:
+    void dfs(vector<int>& res, Node* root) {
+        if(!root) {
+            return;
+        }
+        
+        res.push_back(root->val);
+        for (int i = 0; i < root->children.size(); i++)
+        {
+            dfs(res, root->children[i]);
+        }
+    }
+    
     vector<int> preorder(Node* root) {
         vector<int> res;
-        if(!root){
-            return res;
-        }
-        stack<Node*> st;
-        st.push(root);
-        while(!st.empty()){
-            root = st.top();
-            st.pop();
-            res.push_back(root->val);
-            for (int i = root->children.size()-1; i >= 0; i--)
-            {
-                st.push(root->children[i]);
-            }
-        }
+        dfs(res, root);
         return res;
     }
 };
