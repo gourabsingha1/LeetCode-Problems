22003f63665dd44ac73211528473c5133c77ebd7
Gourab Singha <110812339+gourabsingha1@users.noreply.github.com>
2023-09-16 02:24:37 +0530

    Time: 623 ms (45.88%), Space: 42.5 MB (77.75%) - LeetHub

--- 1584-min-cost-to-connect-all-points/1584-min-cost-to-connect-all-points.cpp
+++ 1584-min-cost-to-connect-all-points/1584-min-cost-to-connect-all-points.cpp
@@ -1,22 +1,22 @@
 class Solution {
 public:
     int minCostConnectPoints(vector<vector<int>>& points) {
-        int n = points.size(), connected = n, cost = 0;
+        int n = points.size(), cost = 0;
         vector<bool> vis(n);
         priority_queue <pair<int, int>, vector<pair<int, int>>, greater<>> pq;
         pq.push({0, 0}); // dist, node
-        while(connected--){
-            int prev = pq.top().first, u = pq.top().second;
-            vis[u] = 1;
+        while(n--){
+            auto [prev, j] = pq.top();
+            vis[j] = 1;
             while(pq.size() && vis[pq.top().second]){
                 pq.pop();
             }
             cost += prev;
-            for (int i = 0; i < n; i++)
+            for (int i = 0; i < points.size(); i++)
             {
-                int v = i, wt = abs(points[i][0] - points[u][0]) + abs(points[i][1] - points[u][1]);
-                if(!vis[v]){
-                    pq.push({wt, v});
+                int wt = abs(points[i][0] - points[j][0]) + abs(points[i][1] - points[j][1]);
+                if(!vis[i]){
+                    pq.push({wt, i});
                 }
             }
         }
