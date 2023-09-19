1541cc98a2dfabd2cace2c37aba8ccfb824441a9
Gourab Singha <110812339+gourabsingha1@users.noreply.github.com>
2023-09-20 03:31:12 +0530

    Time: 384 ms (33.59%), Space: 53.4 MB (32.27%) - LeetHub

---
+++ 1928-minimum-cost-to-reach-destination-in-time/1928-minimum-cost-to-reach-destination-in-time.cpp
@@ -0,0 +1,46 @@
+// keep track of minimum fees till a particular node from node 0
+// if current time is within the maxTime, add in the queue
+
+class Solution {
+public: 
+    int dijkstra(int n, int maxTime, vector<vector<int>> adj[], vector<int>& passingFees) {
+        vector<int> cost(n, 1e9), time(n, 1e9);
+        cost[0] = passingFees[0], time[0] = 0;
+        priority_queue<vector<int>, vector<vector<int>>, greater<>> pq;
+        pq.push({passingFees[0], 0, 0}); // cost, time, node
+        while(pq.size()) {
+            auto node = pq.top();
+            int prevFees = node[0], prevTime = node[1], u = node[2];
+            pq.pop();
+            for(auto& it : adj[u]) {
+                int v = it[0], curTime = it[1], curFees = passingFees[v];
+                if(prevTime + curTime <= maxTime) {
+                    if(cost[v] > prevFees + curFees) {
+                        cost[v] = prevFees + curFees;
+                        pq.push({cost[v], prevTime + curTime, v});
+                    }
+                    else if(time[v] > prevTime + curTime) {
+                        time[v] = prevTime + curTime;
+                        pq.push({prevFees + curFees, time[v], v});
+                    }
+                }
+            }
+        }
+        if(cost[n - 1] == 1e9) {
+            return -1;
+        }
+        return cost[n - 1];
+    }
+    
+    int minCost(int maxTime, vector<vector<int>>& edges, vector<int>& passingFees) {
+        int n = passingFees.size();
+        vector<vector<int>> adj[n];
+        for (int i = 0; i < edges.size(); i++)
+        {
+            int u = edges[i][0], v = edges[i][1], time = edges[i][2];
+            adj[u].push_back({v, time});
+            adj[v].push_back({u, time});
+        }
+        return dijkstra(n, maxTime, adj, passingFees);
+    }
+};
