3856bc9ccea061f9071a2a5ce4c383006d1d61d7
Gourab Singha <110812339+gourabsingha1@users.noreply.github.com>
2023-09-21 01:27:36 +0530

    Time: 10 ms (73.07%), Space: 20.1 MB (48.97%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.cpp
+++ 0042-trapping-rain-water/0042-trapping-rain-water.cpp
@@ -1,18 +1,21 @@
+// only way to understand this is by doing dry run
+
 class Solution {
 public:
     int trap(vector<int>& height) {
-        int n = height.size(), res = 0;
-        vector<int> leftBoundary(n), rightBoundary(n);
-        leftBoundary[0] = height[0], rightBoundary[n - 1] = height[n - 1];
-        for (int i = 1; i < n; i++)
-        {
-            leftBoundary[i] = max(leftBoundary[i - 1], height[i]);
-            rightBoundary[n - i - 1] = max(rightBoundary[n - i], height[n - i - 1]);
-        }
-
-        for (int i = 0; i < n; i++)
-        {
-            res += min(leftBoundary[i], rightBoundary[i]) - height[i];
+        int n = height.size(), res = 0, l = 0, r = n - 1;
+        int leftBoundary = 0, rightBoundary = 0;
+        while(l < r) {
+            leftBoundary = max(leftBoundary, height[l]);
+            rightBoundary = max(rightBoundary, height[r]);
+            if(leftBoundary < rightBoundary) {
+                res += (leftBoundary - height[l]);
+                l++;
+            }
+            else {
+                res += (rightBoundary - height[r]);
+                r--;
+            }
         }
         return res;
     }
