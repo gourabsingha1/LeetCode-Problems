e86316d1147bb8db8c87b803f7c90a6f79e40572
Gourab Singha <110812339+gourabsingha1@users.noreply.github.com>
2023-09-20 11:24:59 +0530

    Time: 111 ms (92.10%), Space: 98.8 MB (38.49%) - LeetHub

--- 1658-minimum-operations-to-reduce-x-to-zero/1658-minimum-operations-to-reduce-x-to-zero.cpp
+++ 1658-minimum-operations-to-reduce-x-to-zero/1658-minimum-operations-to-reduce-x-to-zero.cpp
@@ -5,17 +5,14 @@ class Solution {
 public:
     int minOperations(vector<int>& nums, int x) {
         int n = nums.size(), target = accumulate(nums.begin(), nums.end(), 0) - x, res = 1e9, cur = 0;
-        if(target < 0) {
-            return -1;
-        }
-        if(target == 0 && n == 1) {
-            return 1;
+        if(target == 0) {
+            return n;
         }
         
         for (int i = 0, j = 0; i < n; i++)
         {
             cur += nums[i];
-            while(j < n && cur >= target) {
+            while(j <= i && cur >= target) {
                 if(cur == target) {
                     res = min(res, n - (i - j + 1));
                 }
