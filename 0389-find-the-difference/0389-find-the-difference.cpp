2ce42392c93ec4c44c07f3731587e9ee9a933825
Gourab Singha <110812339+gourabsingha1@users.noreply.github.com>
2023-09-25 10:40:25 +0530

    Time: 0 ms (100.00%), Space: 6.9 MB (16.46%) - LeetHub

--- 0389-find-the-difference/0389-find-the-difference.cpp
+++ 0389-find-the-difference/0389-find-the-difference.cpp
@@ -2,8 +2,10 @@ class Solution {
 public:
     char findTheDifference(string s, string t) {
         char res = 0;
-        s += t;
         for(auto &ch : s){
+            res ^= ch;
+        }
+        for(auto &ch : t){
             res ^= ch;
         }
         return res;
