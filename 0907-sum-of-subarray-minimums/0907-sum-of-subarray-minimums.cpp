22c8b1639134395f4754ccebe438ab00a3f407bb
Gourab Singha <110812339+gourabsingha1@users.noreply.github.com>
2023-09-30 18:45:15 +0530

    Time: 80 ms (48.71%), Space: 46.3 MB (10.05%) - LeetHub

--- 0907-sum-of-subarray-minimums/0907-sum-of-subarray-minimums.cpp
+++ 0907-sum-of-subarray-minimums/0907-sum-of-subarray-minimums.cpp
@@ -1,32 +1,44 @@
 class Solution {
 public:
-    int sumSubarrayMins(vector<int>& arr) {
+    pair<vector<int>, vector<int>> numberOfElementsGreaterThanCur(vector<int>& arr) {
         int n = arr.size(), res = 0, M = 1e9 + 7;
         stack<int> minStackPrev, minStackNext;
-        vector<int> left(n), right(n);
+        vector<int> left(n), right(n); // number of elements which are >= current element
         for (int i = 0; i < n; i++)
         {
             while(minStackPrev.size() && arr[minStackPrev.top()] > arr[i]){
                 minStackPrev.pop();
             }
-            if(minStackPrev.size()) left[i] = i - minStackPrev.top();
-            else left[i] = i + 1;
+            if(minStackPrev.size()) {
+                left[i] = i - minStackPrev.top();
+            }
+            else {
+                left[i] = i + 1;
+            }
             minStackPrev.push(i);
 
             int j = n - 1 - i;
             while(minStackNext.size() && arr[minStackNext.top()] >= arr[j]){
                 minStackNext.pop();
             }
-            if(minStackNext.size()) right[j] = minStackNext.top() - j;
-            else right[j] = i + 1;
+            if(minStackNext.size()) {
+                right[j] = minStackNext.top() - j;
+            }
+            else {
+                right[j] = i + 1;
+            }
             minStackNext.push(j);
         }
+        return {left, right};
+    }
+    
+    int sumSubarrayMins(vector<int>& arr) {
+        int n = arr.size(), res = 0, M = 1e9 + 7;
+        auto [left, right] = numberOfElementsGreaterThanCur(arr);
         for (int i = 0; i < n; i++)
         {
             res = (res + 1LL * left[i] * right[i] * arr[i]) % M;
         }
-        // for(auto &x : left) cout<<x<<" "; cout<<endl;
-        // for(auto &x : right) cout<<x<<" "; cout<<endl;
         return res;
     }
 };
