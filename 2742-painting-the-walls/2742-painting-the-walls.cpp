9bab1c073964a01ccd6d906a1d133a165c82c3b9
Gourab Singha <110812339+gourabsingha1@users.noreply.github.com>
2023-10-14 18:00:28 +0530

    Time: 188 ms (29.64%), Space: 90.3 MB (73.95%) - LeetHub

--- 2742-painting-the-walls/2742-painting-the-walls.cpp
+++ 2742-painting-the-walls/2742-painting-the-walls.cpp
@@ -1,29 +1,29 @@
 // in time == n, all the walls will be painted
 // pick painters such that their time covers all n walls with minimum cost
-// cntTime + cntIndex >= cost.size()
+// cntTime >= cost.size()
 
 class Solution {
 public:
     int dp[500][500];
 
-    int helper(int cntTime, int cntIndex, int n, vector<int>& cost, vector<int>& time) {
-        if(cntTime + cntIndex >= cost.size()) {
+    int helper(int cntTime, int n, vector<int>& cost, vector<int>& time) {
+        if(cntTime >= cost.size()) {
             return 0;
         }
         if(n < 0) {
             return 1e9;
         }
-        if(dp[cntTime + cntIndex][n] != -1) {
-            return dp[cntTime + cntIndex][n];
+        if(dp[cntTime][n] != -1) {
+            return dp[cntTime][n];
         }
 
-        int take = cost[n] + helper(cntTime + time[n], cntIndex + 1, n - 1, cost, time);
-        int notTake = helper(cntTime, cntIndex, n - 1, cost, time);
-        return dp[cntTime + cntIndex][n] = min(take, notTake);
+        int take = cost[n] + helper(cntTime + time[n] + 1, n - 1, cost, time);
+        int notTake = helper(cntTime, n - 1, cost, time);
+        return dp[cntTime][n] = min(take, notTake);
     }
 
     int paintWalls(vector<int>& cost, vector<int>& time) {
         memset(dp, -1, sizeof(dp));
-        return helper(0, 0, cost.size() - 1, cost, time);
+        return helper(0, cost.size() - 1, cost, time);
     }
 };
