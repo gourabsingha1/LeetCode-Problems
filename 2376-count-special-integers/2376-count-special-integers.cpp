d0774bab4645ca4d596e0ac50aad69d9a84b3e08
Gourab Singha <110812339+gourabsingha1@users.noreply.github.com>
2023-10-20 02:30:44 +0530

    Time: 14 ms (41.46%), Space: 6.5 MB (26.33%) - LeetHub

---
+++ 2376-count-special-integers/2376-count-special-integers.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    // pos, tight, seen, found
+    int dp[10][2][1024][2];
+
+    int helper(string& s, int pos = 0, bool tight = 1, int seen = 0, bool found = 0) {
+        if(pos == s.size()) {
+            return !found;
+        }
+        if(dp[pos][tight][seen][found] != -1) {
+            return dp[pos][tight][seen][found];
+        }
+
+        int res = 0, ub = tight ? s[pos] - '0' : 9;
+        for (int i = 0; i <= ub; i++)
+        {
+            int mask = (1 << i);
+            if(seen == 0 && i == 0) { // leading zero
+                res += helper(s, pos + 1, tight & (i == ub), seen, found);
+            }
+            else {
+                res += helper(s, pos + 1, tight & (i == ub), seen | mask, found | (seen & mask));
+            }
+        }
+        return dp[pos][tight][seen][found] = res;
+    }
+
+    int countSpecialNumbers(int n) {
+        string s = to_string(n);
+        memset(dp, -1, sizeof(dp));
+        return helper(s) - 1;
+    }
+};
