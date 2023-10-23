7950a30c876dd96c7dfc8ad58a1b82009c798fff
Gourab Singha <110812339+gourabsingha1@users.noreply.github.com>
2023-10-23 13:13:31 +0530

    Time: 0 ms (100.00%), Space: 6.2 MB (47.70%) - LeetHub

--- 0342-power-of-four/0342-power-of-four.cpp
+++ 0342-power-of-four/0342-power-of-four.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
     bool isPowerOfFour(int n) {
-        return n > 0 && (n & (n - 1)) == 0 && int(log2(n)) % 2 == 0;
+        return n > 0 && (n & (n - 1)) == 0 && (n - 1) % 3 == 0;
     }
 };
