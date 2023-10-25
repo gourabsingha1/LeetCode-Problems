db94dceb0b029b3efd20b88e7166c3787bd7f5f4
Gourab Singha <110812339+gourabsingha1@users.noreply.github.com>
2023-10-25 19:54:03 +0530

    Time: 162 ms (10.32%), Space: 103.6 MB (35.03%) - LeetHub

--- 0739-daily-temperatures/0739-daily-temperatures.cpp
+++ 0739-daily-temperatures/0739-daily-temperatures.cpp
@@ -1,19 +1,22 @@
 class Solution {
 public:
-    vector<int> dailyTemperatures(vector<int>& temperatures) {
-        vector<int> st;
-        vector<int> res(temperatures.size(), 0);
-        for (int i = 0; i < temperatures.size(); i++)
+    // **** Monotonic Stack (Strictly) - O(N), O(N) ****
+    vector<int> nextGreaterElementIndex(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> res(n, 0);
+        stack<int> decStack;
+        for (int i = 0; i < n; i++)
         {
-            if(st.size() && temperatures[st.back()] < temperatures[i]){
-                res[st.back()] = i - st.back();
-                st.pop_back();
-                i--;
+            while(decStack.size() && nums[decStack.top()] < nums[i]){
+                res[decStack.top()] = i - decStack.top();
+                decStack.pop();
             }
-            else{
-                st.push_back(i);
-            }
+            decStack.push(i);
         }
         return res;
+    }
+    
+    vector<int> dailyTemperatures(vector<int>& temperatures) {
+        return nextGreaterElementIndex(temperatures);
     }
 };
