421d6a7c520fbdd89c7dcb0925474a1dd65e258e
Gourab Singha <110812339+gourabsingha1@users.noreply.github.com>
2023-11-24 18:17:53 +0530

    Time: 191 ms (64.28%), Space: 97.7 MB (52.07%) - LeetHub

--- 1424-diagonal-traverse-ii/1424-diagonal-traverse-ii.cpp
+++ 1424-diagonal-traverse-ii/1424-diagonal-traverse-ii.cpp
@@ -1,19 +1,26 @@
+// group i + j elements
+// which comes first in columnwise, will come first in result
+
 class Solution {
 public:
     vector<int> findDiagonalOrder(vector<vector<int>>& nums) {
         vector<int> res;
-        map<int, vector<int>> mp;
+        unordered_map<int, vector<int>> mp;
+        int n = 0;
         for (int i = 0; i < nums.size(); i++)
         {
             for (int j = 0; j < nums[i].size(); j++)
             {
                 mp[i + j].push_back(nums[i][j]);
+                n = max(n, i + j);
             }
         }
-        for(auto& [x, v] : mp) {
-            for (int i = v.size() - 1; i >= 0; i--)
+
+        for (int i = 0; i <= n; i++)
+        {
+            for (int j = mp[i].size() - 1; j >= 0; j--)
             {
-                res.push_back(v[i]);
+                res.push_back(mp[i][j]);
             }
         }
         return res;
