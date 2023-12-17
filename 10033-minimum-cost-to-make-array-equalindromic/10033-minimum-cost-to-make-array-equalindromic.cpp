f592c57142d6a40d13bbf5b06ba007a92998c1bc
Gourab Singha <110812339+gourabsingha1@users.noreply.github.com>
2023-12-17 13:52:29 +0530

    Time: 126 ms (46.15%), Space: 53 MB (46.15%) - LeetHub

--- 10033-minimum-cost-to-make-array-equalindromic/10033-minimum-cost-to-make-array-equalindromic.cpp
+++ 10033-minimum-cost-to-make-array-equalindromic/10033-minimum-cost-to-make-array-equalindromic.cpp
@@ -3,81 +3,41 @@
 vector<ll> pals;
 
 void findPals() {
-    // 1, 2 digits
     for (int i = 1; i < 10; i++)
     {
-        string s = to_string(i);
-        string p = s;
-        reverse(p.begin(), p.end());
+        // 1, 2 digits
+        string s = to_string(i), p = s;
         pals.push_back(stoll(s + p));
         s.pop_back();
         pals.push_back(stoll(s + p));
-    }
-    
-    // 3, 4 digits
-    for (int i = 1; i < 10; i++)
-    {
         for (int j = 0; j < 10; j++)
         {
-            string s = to_string(i) + to_string(j);
-            string p = s;
+            // 3, 4 digits
+            s = to_string(i) + to_string(j), p = s;
             reverse(p.begin(), p.end());
             pals.push_back(stoll(s + p));
             s.pop_back();
             pals.push_back(stoll(s + p));
-        }
-    }
-    
-    // 5, 6 digits
-    for (int i = 1; i < 10; i++)
-    {
-        for (int j = 0; j < 10; j++)
-        {
             for (int k = 0; k < 10; k++)
             {
-                string s = to_string(i) + to_string(j) + to_string(k);
-                string p = s;
+                // 5, 6 digits
+                s = to_string(i) + to_string(j) + to_string(k), p = s;
                 reverse(p.begin(), p.end());
                 pals.push_back(stoll(s + p));
                 s.pop_back();
                 pals.push_back(stoll(s + p));
-            }
-        }
-    }
-    
-    // 7, 8 digits
-    for (int i = 1; i < 10; i++)
-    {
-        for (int j = 0; j < 10; j++)
-        {
-            for (int k = 0; k < 10; k++)
-            {
                 for (int l = 0; l < 10; l++)
                 {
-                    string s = to_string(i) + to_string(j) + to_string(k) + to_string(l);
-                    string p = s;
+                    // 7, 8 digits
+                    s = to_string(i) + to_string(j) + to_string(k) + to_string(l), p = s;
                     reverse(p.begin(), p.end());
                     pals.push_back(stoll(s + p));
                     s.pop_back();
                     pals.push_back(stoll(s + p));
-                }
-            }
-        }
-    }
-    
-    // 9 digits
-    for (int i = 1; i < 10; i++)
-    {
-        for (int j = 0; j < 10; j++)
-        {
-            for (int k = 0; k < 10; k++)
-            {
-                for (int l = 0; l < 10; l++)
-                {
                     for (int m = 0; m < 10; m++)
                     {
-                        string s = to_string(i) + to_string(j) + to_string(k) + to_string(l) + to_string(m);
-                        string p = s;
+                        // 9 digit
+                        s = to_string(i) + to_string(j) + to_string(k) + to_string(l) + to_string(m), p = s;
                         reverse(p.begin(), p.end());
                         s.pop_back();
                         pals.push_back(stoll(s + p));
@@ -98,44 +58,7 @@ public:
     }
 
     long long minimumCost(vector<int>& nums) {
-        // sort(nums.begin(), nums.end());
-        // unordered_map<ll, int> mp;
-        // int n = nums.size();
-        // for (int i = 0; i < n; i++)
-        // {
-        //     mp[nums[i]] = i;
-        // }
-        // vector<int> pre(n), suf(n);
-        // pre[0] = nums[0];
-        // suf[n - 1] = nums[n - 1];
-        // for (int i = 1; i < n; i++)
-        // {
-        //     pre[i] = nums[i] + pre[i - 1];
-        //     suf[n - 1 - i] = nums[n - 1 - i] + suf[n - i];
-        // }
-
-        ll cost = 1e14;
-        // for(auto& x : pals) {
-        //     if(x > nums.back()) break;
-        //     auto ub = upper_bound(nums.begin(), nums.end(), x);
-        //     ub--;
-        //     int i = mp[*ub];
-        //     cout<<i<<' ';
-        //     if(i == 0 && *ub != nums[i]) break;
-        //     ll cur = (1LL * x * (i + 1) - pre[i]) + (suf[i] - 1LL * x * (n - i));
-        //     cost = min(cost, cur);
-        // }
-        // cout<<endl;
-        
-        
-        // for(auto& x : pals) {
-        //     ll cur = 0;
-        //     for(auto& c : nums) {
-        //         cur += abs(x - c);
-        //     }
-        //     cost = min(cost, cur);
-        // }
-        ll low = 0, high = pals.size() - 1;
+        ll cost = 1e14, low = 0, high = pals.size() - 1;
         while(low <= high) {
             ll mid = (low + high) / 2;
             ll cur = 0;
