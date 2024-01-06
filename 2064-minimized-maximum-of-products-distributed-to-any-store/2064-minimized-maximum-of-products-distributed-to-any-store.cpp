41f969da38b82bd7b2d527b58b54e1552b0bbc68
Gourab Singha <110812339+gourabsingha1@users.noreply.github.com>
2024-01-06 12:34:49 +0530

    Time: 131 ms (89.50%), Space: 84.8 MB (47.81%) - LeetHub

--- 2064-minimized-maximum-of-products-distributed-to-any-store/2064-minimized-maximum-of-products-distributed-to-any-store.cpp
+++ 2064-minimized-maximum-of-products-distributed-to-any-store/2064-minimized-maximum-of-products-distributed-to-any-store.cpp
@@ -1,19 +1,22 @@
+// binary search on answer
+// if mid is eligible, we wanna minimize it. so, high = mid - 1
+
 class Solution {
 public:
     int minimizedMaximum(int n, vector<int>& quantities) {
-        int l = 1, h = 1e5;
-        while(l <= h){
-            int m = (l+h)/2, sum = 0;
-            for(int &x : quantities){
-                sum += ((x+m-1)/m);
+        int low = 1, high = 1e5;
+        while(low <= high){
+            int mid = (low + high) / 2, sum = 0;
+            for(auto& x : quantities){
+                sum += (x + mid - 1) / mid;
             }
             if(sum <= n){
-                h = m - 1;
+                high = mid - 1;
             }
             else{
-                l = m + 1;
+                low = mid + 1;
             }
         }
-        return l;
+        return low;
     }
 };
