d981221092d6865b00f617a35cb2b834c5299db4
Gourab Singha <110812339+gourabsingha1@users.noreply.github.com>
2024-01-10 09:00:56 +0530

    Time: 443 ms (46.75%), Space: 182.1 MB (29.35%) - LeetHub

--- 2385-amount-of-time-for-binary-tree-to-be-infected/2385-amount-of-time-for-binary-tree-to-be-infected.cpp
+++ 2385-amount-of-time-for-binary-tree-to-be-infected/2385-amount-of-time-for-binary-tree-to-be-infected.cpp
@@ -1,28 +1,39 @@
+// convert tree to graph then simple bfs
+
 class Solution {
 public:
+    // **** Tree to Graph ****
     unordered_map<int, vector<int>> adj;
-    void dfs(TreeNode* parent, TreeNode* root) {
-        if(!root) {
-            return;
-        }
-        int u = parent->val, v = root->val;
-        if(u != v) {
-            adj[u].push_back(v);
-            adj[v].push_back(u);
-        }
-        dfs(root, root->left);
-        dfs(root, root->right);
+
+    void treeToGraph(TreeNode* root){
+        queue<TreeNode*> q;
+        q.push(root);
+        while(q.size()){
+            auto node = q.front();
+            q.pop();
+            if(node->left) {
+                adj[node->val].push_back(node->left->val);
+                adj[node->left->val].push_back(node->val);
+                q.push(node->left);
+            }
+            if(node->right) {
+                adj[node->val].push_back(node->right->val);
+                adj[node->right->val].push_back(node->val);
+                q.push(node->right);
+            }
+        }   
     }
+    
     int amountOfTime(TreeNode* root, int start) {
-        dfs(root, root);
+        treeToGraph(root);
         unordered_map<int, bool> vis;
         int level = -1;
         queue<int> q;
         q.push(start);
         vis[start] = 1;
         while(q.size()) {
-            int t = q.size();
-            while(t--) {
+            int size = q.size();
+            while(size--) {
                 int u = q.front();
                 q.pop();
                 for(auto& v : adj[u]) {
