129c91f827f4e723c97842a92474822b8852d72c
Gourab Singha <110812339+gourabsingha1@users.noreply.github.com>
2024-01-15 12:02:09 +0530

    Time: 411 ms (92.99%), Space: 164.7 MB (74.48%) - LeetHub

--- 2225-find-players-with-zero-or-one-losses/2225-find-players-with-zero-or-one-losses.cpp
+++ 2225-find-players-with-zero-or-one-losses/2225-find-players-with-zero-or-one-losses.cpp
@@ -1,34 +1,26 @@
+// brute force
+
 class Solution {
 public:
     vector<vector<int>> findWinners(vector<vector<int>>& matches) {
-        unordered_map<int,int> ml;
-        vector<int> answer0, answer1;
-        vector<vector<int>> ans;
-                priority_queue<int, vector<int>, greater<int>> pq0, pq1;
-        for (auto &x : matches){
-            ml[x[1]]++;
+        vector<int> zero, one;
+        unordered_map<int, int> lostFreq;
+        for(auto& match : matches) {
+            lostFreq[match[1]]++;
         }
-        for(auto &x : ml){
-            if(x.second==1){
-                pq1.push(x.first);
+        for(auto& [player, freq] : lostFreq) {
+            if(freq == 1) {
+                one.push_back(player);
             }
         }
-        for(auto &x : matches){
-            if(ml[x[0]]==0){
-                pq0.push(x[0]);
-                ml[x[0]] = 1;
+        for(auto& match : matches) {
+            if(!lostFreq[match[0]]) {
+                zero.push_back(match[0]);
+                lostFreq[match[0]] = 1;
             }
         }
-        while(!pq0.empty()){
-            answer0.push_back(pq0.top());
-            pq0.pop();
-        }
-        while(!pq1.empty()){
-            answer1.push_back(pq1.top());
-            pq1.pop();
-        }
-        ans.push_back(answer0);
-        ans.push_back(answer1);
-        return ans;
+        sort(zero.begin(), zero.end());
+        sort(one.begin(), one.end());
+        return {zero, one};
     }
 };
