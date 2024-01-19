f184dbad11a90554493551a177fbe49219fbe4b8
Gourab Singha <110812339+gourabsingha1@users.noreply.github.com>
2024-01-19 20:55:01 +0530

    Time: 5 ms (48.57%), Space: 7.3 MB (18.26%) - LeetHub

---
+++ 0052-n-queens-ii/0052-n-queens-ii.cpp
@@ -0,0 +1,53 @@
+// same as N-Queens
+// go from top to bottom placing queens if position is valid
+
+class Solution {
+public:
+    bool isValid(int i, int j, int n, vector<string>& board) {
+        // upper left
+        int r = i, c = j;
+        while(r >= 0 && c >= 0) {
+            if(board[r--][c--] == 'Q') {
+                return 0;
+            }
+        }
+        // up
+        r = i, c = j;
+        while(r >= 0) {
+            if(board[r--][c] == 'Q') {
+                return 0;
+            }
+        }
+        // upper right
+        r = i, c = j;
+        while(r >= 0 && c < n) {
+            if(board[r--][c++] == 'Q') {
+                return 0;
+            }
+        }
+        return 1;
+    }
+    
+    void helper(int i, int n, vector<string>& board, int& res) {
+        if(i == n) {
+            res++;
+            return;
+        }
+        
+        for (int j = 0; j < n; j++)
+        {
+            if(isValid(i, j, n, board)) {
+                board[i][j] = 'Q';
+                helper(i + 1, n, board, res);
+                board[i][j] = '.';
+            }
+        }
+    }
+    
+    int totalNQueens(int n) {
+        int res = 0;
+        vector<string> board(n, string(n, '.'));
+        helper(0, n, board, res);
+        return res;
+    }
+};
