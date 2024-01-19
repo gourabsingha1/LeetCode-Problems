2a4a9e8547e5de049a54afa0ecc96540b9b648f0
Gourab Singha <110812339+gourabsingha1@users.noreply.github.com>
2024-01-19 09:24:04 +0530

    Time: 15 ms (39.09%), Space: 10.5 MB (94.68%) - LeetHub

--- 0931-minimum-falling-path-sum/0931-minimum-falling-path-sum.cpp
+++ 0931-minimum-falling-path-sum/0931-minimum-falling-path-sum.cpp
@@ -1,16 +1,23 @@
+// for mat[i][j], store its val + min of top, topAdjLeft & topAdjRight
+
 class Solution {
 public:
     int minFallingPathSum(vector<vector<int>>& matrix) {
         int n = matrix.size(), m = matrix[0].size();
         for (int i = 1; i < n; i++)
         {
-            matrix[i][0] += min(matrix[i-1][0], matrix[i-1][1]);
-            matrix[i][m-1] += min(matrix[i-1][m-2], matrix[i-1][m-1]);
-            for (int j = 1; j < m-1; j++)
+            for (int j = 0; j < m; j++)
             {
-                matrix[i][j] += min({matrix[i-1][j-1], matrix[i-1][j], matrix[i-1][j+1]});
+                int mn = matrix[i - 1][j];
+                if(j > 0)  {
+                    mn = min(mn, matrix[i - 1][j - 1]);
+                }
+                if(j + 1 < m) {
+                    mn = min(mn, matrix[i - 1][j + 1]);
+                }
+                matrix[i][j] += mn;
             }
         }
-        return *min_element(matrix[n-1].begin(), matrix[n-1].end());
+        return *min_element(matrix[n - 1].begin(), matrix[n - 1].end());
     }
 };
