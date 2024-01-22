fe48b2af28650e5f56493c456e9e35d04579c43d
Gourab Singha <110812339+gourabsingha1@users.noreply.github.com>
2024-01-22 11:46:29 +0530

    Time: 19 ms (89.24%), Space: 23.6 MB (37.21%) - LeetHub

--- 0645-set-mismatch/0645-set-mismatch.cpp
+++ 0645-set-mismatch/0645-set-mismatch.cpp
@@ -1,22 +1,29 @@
+// since numbers are from 1 to n, treat numbers as indices
+// set nums[nums[i] - 1] to its negation
+// if in one scan, a neg number is found, then nums[i] is the repeated element
+// in another scan if nums[i] > 0, the missing element is i + 1
+// bacause that index wasn't found and hence marked in the first scan
+
 class Solution {
 public:
     vector<int> findErrorNums(vector<int>& nums) {
-        int a, b;
-        for (int i = 0; i < nums.size(); i++)
+        int a, b, n = nums.size();
+        for (int i = 0; i < n; i++)
         {
-            int x = abs(nums[i]) - 1;
-            if(nums[x] < 0){
-                a = x + 1;
+            int ind = abs(nums[i]) - 1;
+            if(nums[ind] > 0){
+                nums[ind] = -nums[ind];
             }
             else{
-                nums[x] = -nums[x];
+                a = ind + 1;
             }
         }
         
-        for (int i = 0; i < nums.size(); i++)
+        for (int i = 0; i < n; i++)
         {
             if(nums[i] > 0){
                 b = i + 1;
+                break;
             }
         }
         return {a, b};
