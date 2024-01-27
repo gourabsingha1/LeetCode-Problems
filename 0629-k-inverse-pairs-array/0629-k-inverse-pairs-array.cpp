1f85c436f4ac76e1b9745027ff0df8a9f0bde315
Gourab Singha <110812339+gourabsingha1@users.noreply.github.com>
2024-01-27 19:12:50 +0530

    Time: 21 ms (57.69%), Space: 16.7 MB (18.68%) - LeetHub

--- 0629-k-inverse-pairs-array/0629-k-inverse-pairs-array.cpp
+++ 0629-k-inverse-pairs-array/0629-k-inverse-pairs-array.cpp
@@ -1,24 +1,22 @@
-// Tabulation - O(N * N * K), O(K)
+// Tabulation + Sliding window - O(N * K), O(K)
 
 class Solution {
 public:
     int kInversePairs(int n, int k) {
         int M = 1e9 + 7;
         vector<int> prev(k + 1, 0);
+        prev[0] = 1;
         for (int N = 1; N <= n; N++)
         {
             vector<int> cur(k + 1, 0);
+            long total = 0;
             for (int K = 0; K <= k; K++)
             {
-                for (int i = 0; i <= min(K, N - 1); i++)
-                {
-                    if(K == 0) {
-                        cur[K] = 1;
-                    }
-                    else {
-                        cur[K] = (cur[K] + prev[K - i]) % M;
-                    }
+                total = (total + prev[K] + M) % M;
+                if(K >= N) {
+                    total = (total - prev[K - N] + M) % M;
                 }
+                cur[K] = total;
             }
             prev = cur;
         }
