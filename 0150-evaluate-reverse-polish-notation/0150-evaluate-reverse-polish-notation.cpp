85ba997a8328d430b9de8b430732dbe212b93d3d
Gourab Singha <110812339+gourabsingha1@users.noreply.github.com>
2024-01-30 08:48:29 +0530

    Time: 12 ms (34.50%), Space: 16.7 MB (5.26%) - LeetHub

--- 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.cpp
+++ 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.cpp
@@ -1,17 +1,28 @@
+// brute force stack
+
 class Solution {
 public:
-    stack<long long int> st;
-    void operation(string &s){
-        long long int x = st.top();
+    stack<long long> st;
+    
+    void operation(string& s) {
+        long long x = st.top();
         st.pop();
-        if(s == "+") st.top() += x;
-        else if(s == "-") st.top() -= x;
-        else if(s == "*") st.top() *= x;
-        else st.top() /= x;
+        if(s == "+") {
+            st.top() += x;
+        }
+        else if(s == "-") {
+            st.top() -= x;
+        }
+        else if(s == "*") {
+            st.top() *= x;
+        }
+        else {
+            st.top() /= x;
+        }
     }
 
     int evalRPN(vector<string>& tokens) {
-        for(string &s : tokens){
+        for(string& s : tokens) {
             if(s == "+" || s == "-" || s == "*" || s == "/"){
                 operation(s);
             }
