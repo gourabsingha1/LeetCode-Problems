9d542370d7e1e5a448c5e0a691506b1ac3231b4d
Gourab Singha <110812339+gourabsingha1@users.noreply.github.com>
2024-02-06 09:03:08 +0530

    Time: 35 ms (38.55%), Space: 23.4 MB (30.72%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.cpp
+++ 0049-group-anagrams/0049-group-anagrams.cpp
@@ -1,15 +1,17 @@
+// sort to have a unique group leader
+
 class Solution {
 public:
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
         vector<vector<string>> res;
-        unordered_map<string, vector<string>> m;
-        for(string &s : strs){
+        unordered_map<string, vector<string>> mp;
+        for(string& s : strs){
             string p = s;
             sort(p.begin(), p.end());
-            m[p].push_back(s);
+            mp[p].push_back(s);
         }
-        for(auto &x : m){
-            res.push_back(x.second);
+        for(auto& [p, s] : mp){
+            res.push_back(s);
         }
         return res;
     }
