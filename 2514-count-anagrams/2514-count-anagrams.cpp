b2e75d7f2cdb529b72a440cd4b0127428becb126
Gourab Singha <110812339+gourabsingha1@users.noreply.github.com>
2024-02-06 09:37:15 +0530

    Time: 37 ms (84.57%), Space: 22.7 MB (57.98%) - LeetHub

--- 2514-count-anagrams/2514-count-anagrams.cpp
+++ 2514-count-anagrams/2514-count-anagrams.cpp
@@ -1,10 +1,11 @@
 // res *= permutations of a multiset (word)
 // ifact is used to handle the case where the number of repeating characters is large
 
+static const int MAX_N = 1e5 + 1, M = 1e9 + 7;
+int fact[MAX_N], ifact[MAX_N];
+
 class Solution {
 public:
-    static const int MAX_N = 1e5 + 1, M = 1e9 + 7;
-    
     int exp(int a, int b) {
         int res = 1;
         while(b > 0) {
@@ -20,9 +21,7 @@ public:
     int mod_inv(int a, int m = M) {
         return exp(a, m - 2);
     }
-    
-    int fact[MAX_N], ifact[MAX_N];
-    
+        
     void fact_init() {
         fact[0] = fact[1] = ifact[0] = ifact[1] = 1;
         for (int i = 2; i < MAX_N; i++) {
@@ -31,8 +30,14 @@ public:
         }
     }
     
+    // to reduce time
+    Solution() {
+        if(fact[0] == 0) {
+            fact_init();
+        }
+    }
+    
     int countAnagrams(string s) {
-        fact_init();
         int res = 1, n = s.size();
         for (int i = 0; i < n; i++)
         {
