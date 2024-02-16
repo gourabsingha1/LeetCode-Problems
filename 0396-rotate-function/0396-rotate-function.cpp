fa07c875f4c51265f25a82f21c1c2ceb0aeba179
Gourab Singha <110812339+gourabsingha1@users.noreply.github.com>
2024-02-16 19:04:32 +0530

    Time: 90 ms (93.47%), Space: 98.2 MB (36.06%) - LeetHub

--- 0396-rotate-function/0396-rotate-function.cpp
+++ 0396-rotate-function/0396-rotate-function.cpp
@@ -1,21 +1,24 @@
 // put k = 0, 1, 2 and find the formula
 // F(1) = F(0) + sum - n * nums[n - 1]
 // F(2) = F(1) + sum - n * nums[n - 2]
+// space optimization
 
 class Solution {
 public:
     int maxRotateFunction(vector<int>& nums) {
-        int n = nums.size(), sum = 0;
-        vector<int> F(n);
+        int n = nums.size(), sum = 0, prev = 0, cur = 0;
         for (int i = 0; i < n; i++)
         {
-            F[0] += i * nums[i];
+            prev += i * nums[i];
             sum += nums[i];
         }
+        int res = prev;
         for (int i = 1; i < n; i++)
         {
-            F[i] = F[i - 1] + sum - n * nums[n - i];
+            cur = prev + sum - n * nums[n - i];
+            res = max(res, cur);
+            prev = cur;
         }
-        return *max_element(F.begin(), F.end());
+        return res;
     }
 };
