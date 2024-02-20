64de3735717136c8d4be2de09928077fa491eac5
Gourab Singha <110812339+gourabsingha1@users.noreply.github.com>
2024-02-20 12:48:18 +0530

    Time: 18 ms (56.26%), Space: 24.4 MB (12.72%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.cpp
+++ 0036-valid-sudoku/0036-valid-sudoku.cpp
@@ -1,3 +1,5 @@
+// an element should not appear more than once in a row/col/3x3
+
 class Solution {
 public:
     bool isValidSudoku(vector<vector<char>>& board) {
@@ -6,25 +8,21 @@ public:
             for(int j = 0; j < 9; j++){
                 // row & col
                 if(board[i][j] != '.'){
-                    mrow[board[i][j]]++;
-                    if(mrow[board[i][j]] > 1){
+                    if(mrow[board[i][j]]++){
                         return 0;
                     }
                 }
                 if(board[j][i] != '.'){
-                    mcol[board[j][i]]++;
-                    if(mcol[board[j][i]] > 1){
+                    if(mcol[board[j][i]]++){
                         return 0;
                     }
                 }
 
-                // 3x3
-                int x = (i/3)*3 + (j/3), y = (i%3)*3 + j%3;
+                // 3 x 3
+                int x = (i / 3) * 3 + (j / 3), y = (i % 3) * 3 + j % 3;
                 // cout<<"("<<x<<","<<y<<") ";
-
                 if(board[x][y] != '.'){
-                    m3x3[board[x][y]]++;
-                    if(m3x3[board[x][y]] > 1){
+                    if(m3x3[board[x][y]]++){
                         return 0;
                     }
                 }
