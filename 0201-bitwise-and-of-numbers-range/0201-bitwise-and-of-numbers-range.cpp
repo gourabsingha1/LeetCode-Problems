a336d500c5057acde0e8931496afc0a9c15eebb2
Gourab Singha <110812339+gourabsingha1@users.noreply.github.com>
2024-02-21 09:31:32 +0530

    Time: 91 ms (7.33%), Space: 8.8 MB (46.79%) - LeetHub

--- 0201-bitwise-and-of-numbers-range/0201-bitwise-and-of-numbers-range.cpp
+++ 0201-bitwise-and-of-numbers-range/0201-bitwise-and-of-numbers-range.cpp
@@ -1,9 +1,15 @@
+// AND is always 0 if log2(left) != log2(right)
+
 class Solution {
 public:
     int rangeBitwiseAnd(int left, int right) {
-        if(!left || !right) return 0;
-        int res = left, l = log2(left), r = log2(right);
+        if(!left || !right) {
+            return 0;
+        }
+        
+        int l = log2(left), r = log2(right);
         if(l == r){
+            int res = left;
             for (long i = left; i <= right; i++)
             {
                 res &= i;
