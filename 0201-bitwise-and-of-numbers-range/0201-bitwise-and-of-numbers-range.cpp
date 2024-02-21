2e5a7afeb231a437917e7eae7254a0a0281301eb
Gourab Singha <110812339+gourabsingha1@users.noreply.github.com>
2024-02-21 09:37:51 +0530

    Time: 6 ms (66.15%), Space: 8.8 MB (26.31%) - LeetHub

--- 0201-bitwise-and-of-numbers-range/0201-bitwise-and-of-numbers-range.cpp
+++ 0201-bitwise-and-of-numbers-range/0201-bitwise-and-of-numbers-range.cpp
@@ -1,21 +1,16 @@
-// AND is always 0 if log2(left) != log2(right)
+// (a & b) <= min(a, b)
+// for left = 4, right = 14
+// 14 & 13 = 12
+// 12 & 11 = 8
+// 8 & 7 = 0
+// total 3 iterations
 
 class Solution {
 public:
     int rangeBitwiseAnd(int left, int right) {
-        if(!left || !right) {
-            return 0;
+        while(left < right) {
+            right = right & (right - 1);
         }
-        
-        int l = log2(left), r = log2(right);
-        if(l == r){
-            int res = left;
-            for (long i = left; i <= right; i++)
-            {
-                res &= i;
-            }
-            return res;
-        }
-        return 0;
+        return right;
     }
 };
