61c7b232cc171557a63fa6a2de8c0775b54fe1a1
Gourab Singha <110812339+gourabsingha1@users.noreply.github.com>
2024-02-29 08:25:49 +0530

    Time: 179 ms (95.28%), Space: 149.1 MB (89.27%) - LeetHub

--- 1609-even-odd-tree/1609-even-odd-tree.cpp
+++ 1609-even-odd-tree/1609-even-odd-tree.cpp
@@ -1,35 +1,32 @@
 class Solution {
 public:
-    bool levelOrder(TreeNode *root){
+    bool isEvenOddTree(TreeNode* root) {
         queue<TreeNode*> q;
         q.push(root);
         int level = 0;
         while(q.size()){
-            int n = q.size();
-            int prev = INT_MIN;
-            if(level&1){
-                prev = INT_MAX;
-            }
+            int n = q.size(), prev;
             for (int i = 0; i < n; i++)
             {
                 TreeNode *node = q.front();
                 q.pop();
-                if(!(level&1) && !(node->val&1 && prev < node->val) || level&1 && !(!(node->val&1) && prev > node->val)){
+                if(level % 2 == 0 && (i == 0 && node->val % 2 == 0 || i > 0 && !(prev % 2 && node->val % 2 && prev < node->val))) {
                     return 0;
                 }
-                prev = node->val;
+                if(level % 2 && (i == 0 && node->val % 2 || i > 0 && !(prev % 2 == 0 && node->val % 2 == 0 && prev > node->val))) {
+                    return 0;
+                }
+                
                 if(node->left){
                     q.push(node->left);
                 }
                 if(node->right){
                     q.push(node->right);
                 }
+                prev = node->val;
             }
             level++;
         }
         return 1;
-    }
-    bool isEvenOddTree(TreeNode* root) {
-        return levelOrder(root);
     }
 };
